37bf8ccb169aa57b365fd3a9e4d05da6f77444f6
lvxiaoyi <lvxiaoyi@juzix.io>
2018-10-22 17:35:02 +0800

    type BlockSignature增加SignHash

--- core/cbfttypes/type.go
+++ core/cbfttypes/type.go
@@ -2,23 +2,24 @@ package cbfttypes
 
 import (
 	"Platon-go/common"
-	"math/big"
 	"Platon-go/core/state"
 	"Platon-go/core/types"
+	"math/big"
 )
 
 // modify by platon
 // Block's Signature info
 type BlockSignature struct {
-	Hash        common.Hash
-	Number      *big.Int
-	Signature   *common.BlockConfirmSign
+	SignHash  common.Hash //签名hash，header[0:32]
+	Hash      common.Hash //块hash，header[:]
+	Number    *big.Int
+	Signature *common.BlockConfirmSign
 }
 
 // modify by platon
 type CbftResult struct {
-	Block       		*types.Block
-	Receipts    		types.Receipts
-	State       		*state.StateDB
-	BlockConfirmSigns 	[]*common.BlockConfirmSign
+	Block             *types.Block
+	Receipts          types.Receipts
+	State             *state.StateDB
+	BlockConfirmSigns []*common.BlockConfirmSign
 }
