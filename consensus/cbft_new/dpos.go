b4bb2f36a0a8f0a008503288bf021dbca9375d8f
lvxiaoyi <lvxiaoyi@juzix.io>
2018-10-29 16:54:41 +0800

    重构cbft

---
+++ consensus/cbft_new/dpos.go
@@ -0,0 +1,90 @@
+package cbft_new
+
+import (
+	"Platon-go/common"
+	"Platon-go/core"
+	"Platon-go/crypto"
+	"Platon-go/log"
+	"Platon-go/p2p/discover"
+	"bytes"
+)
+
+type dpos struct {
+	primaryNodeList   []discover.Node
+	chain             *core.BlockChain
+	lastCycleBlockNum uint64
+	startTimeOfEpoch  int64 // 一轮共识开始时间，通常是上一轮共识结束时最后一个区块的出块时间；如果是第一轮，则从1970.1.1.0.0.0.0开始。单位：毫秒
+
+}
+
+func newDpos(initialNodes []discover.Node) *dpos {
+	dpos := &dpos{
+		primaryNodeList:   initialNodes,
+		lastCycleBlockNum: 0,
+	}
+	return dpos
+}
+
+func (d *dpos) IsPrimary(addr common.Address) bool {
+	// 判断当前节点是否是共识节点
+	for _, node := range d.primaryNodeList {
+		pub, err := node.ID.Pubkey()
+		if err != nil || pub == nil {
+			log.Error("nodeID.ID.Pubkey error!")
+		}
+		address := crypto.PubkeyToAddress(*pub)
+		return bytes.Equal(address[:], addr[:])
+	}
+	return false
+}
+
+func (d *dpos) NodeIndex(nodeID discover.NodeID) int64 {
+	for idx, node := range d.primaryNodeList {
+		if node.ID == nodeID {
+			return int64(idx)
+		}
+	}
+	return int64(-1)
+}
+
+func (d *dpos) LastCycleBlockNum() uint64 {
+	// 获取最后一轮共识结束时的区块高度
+	return d.lastCycleBlockNum
+}
+
+func (d *dpos) SetLastCycleBlockNum(blockNumber uint64) {
+	// 设置最后一轮共识结束时的区块高度
+	d.lastCycleBlockNum = blockNumber
+}
+
+// modify by platon
+// 返回当前共识节点地址列表
+/*func (b *dpos) ConsensusNodes() []discover.Node {
+	return b.primaryNodeList
+}
+*/
+// 判断某个节点是否本轮或上一轮选举共识节点
+/*func (b *dpos) CheckConsensusNode(id discover.NodeID) bool {
+	nodes := b.ConsensusNodes()
+	for _, node := range nodes {
+		if node.ID == id {
+			return true
+		}
+	}
+	return false
+}*/
+
+// 判断当前节点是否本轮或上一轮选举共识节点
+/*func (b *dpos) IsConsensusNode() (bool, error) {
+	return true, nil
+}
+*/
+
+func (d *dpos) StartTimeOfEpoch() int64 {
+	return d.startTimeOfEpoch
+}
+
+func (d *dpos) SetStartTimeOfEpoch(startTimeOfEpoch int64) {
+	// 设置最后一轮共识结束时的区块高度
+	d.startTimeOfEpoch = startTimeOfEpoch
+}
