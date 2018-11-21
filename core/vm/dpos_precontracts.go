78328fe826909745bd375acdaa60cd03bb2709ad
caiguopei <caiguopei@juzix.io>
2018-11-21 19:34:19 +0800

    增加候选池接口使用说明文档 && 修改候选池合约接口参数

--- core/vm/dpos_precontracts.go
+++ core/vm/dpos_precontracts.go
@@ -198,14 +198,10 @@ func (c *candidateContract) CandidateDeposit(nodeId discover.NodeID, owner commo
 }
 
 //申请退回质押金
-func (c *candidateContract) CandidateApplyWithdraw(params [][]byte) ([]byte, error)  {
+func (c *candidateContract) CandidateApplyWithdraw(nodeId discover.NodeID, withdraw *big.Int) ([]byte, error)  {
 
-	if len(params)!=1 {
-		return nil, ErrParamsLen
-	}
-	nodeId := hex.EncodeToString(params[0])
 	from := c.contract.caller.Address().Hex()
-	fmt.Println("CandidateApplyWithdraw==> nodeId: ", nodeId, " from: ", from)
+	fmt.Println("CandidateApplyWithdraw==> nodeId: ", nodeId, " from: ", from, " withdraw: ", withdraw)
 
 	//校验from和owner是否一致
 	//调用接口生成退款记录
@@ -215,16 +211,18 @@ func (c *candidateContract) CandidateApplyWithdraw(params [][]byte) ([]byte, err
 }
 
 //质押金提现
-func (c *candidateContract) CandidateWithdraw(params [][]byte) ([]byte, error)  {
-
-	if len(params)!=1 {
-		return nil, ErrParamsLen
-	}
-	nodeId := hex.EncodeToString(params[0])
+func (c *candidateContract) CandidateWithdraw(nodeId discover.NodeID) ([]byte, error)  {
 
 	//调用接口退款，判断返回值
 
 	fmt.Println("CandidateWithdraw==> nodeId: ", nodeId)
+	return nil, nil
+}
+
+//获取已申请的退款记录
+func (c *candidateContract) CandidateWithdrawInfos(nodeId discover.NodeID)([]byte, error){
+
+	fmt.Println("CandidateWithdrawInfos==> nodeId: ", nodeId)
 	return nil, nil
 }
 
