c6176182177bf1539e5d9b8f8add17765f3682f8
Gavin <meiyan532484710@163.com>
2018-11-27 17:42:18 +0800

    修复，节点重新启动是， 设置 三轮见证节点dposround 空指针问题 Signed-off-by: Gavin <meiyan532484710@163.com>

--- consensus/cbft/dpos.go
+++ consensus/cbft/dpos.go
@@ -280,12 +280,14 @@ func (d *dpos) SetCandidatePool(blockChain *core.BlockChain) {
 			if len(curArr) != 0 {
 				d.current.nodes = convertNodeID(curArr)
 			}
-
-			d.next.start = big.NewInt(int64(BaseSwitchWitness*(round+1)) + 1)
-			d.next.end = new(big.Int).Add(d.next.start, big.NewInt(int64(BaseSwitchWitness-1)))
-			if len(nextArr) != 0 {
-				d.next.nodes = convertNodeID(nextArr)
+			if nil != d.next {
+				d.next.start = big.NewInt(int64(BaseSwitchWitness*(round+1)) + 1)
+				d.next.end = new(big.Int).Add(d.next.start, big.NewInt(int64(BaseSwitchWitness-1)))
+				if len(nextArr) != 0 {
+					d.next.nodes = convertNodeID(nextArr)
+				}
 			}
+
 			d.lock.Unlock()
 		}
 	}
