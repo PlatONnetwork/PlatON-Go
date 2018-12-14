b881d5a4fdf38e2aea10a277d6ae9c2f21dad131
caiguopei <caiguopei@juzix.io>
2018-12-14 19:11:08 +0800

    票ID列表缓存调试

--- core/ppos/ticketids_cache.go
+++ core/ppos/ticketids_cache.go
@@ -3,6 +3,7 @@ package pposm
 import (
 	"Platon-go/p2p/discover"
 	"Platon-go/common"
+	"bytes"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -14,6 +15,9 @@ import (
 
 var (
 	ErrFaile = errors.New("fail")
+	ErrNotfindFromblockNumber = errors.New("Not find tickets from block number")
+	ErrNotfindFromblockHash = errors.New("Not find tickets from block hash")
+	ErrNotfindFromnodeId = errors.New("Not find tickets from node id")
 )
 
 var ticketidsCache *NumBlocks
@@ -23,7 +27,7 @@ func newTicketIdsCache() (*NumBlocks, error)  {
 	//read from leveldb
 	ticketidsCache = &NumBlocks{}
 
-	//test
+	//read from leveldb
 	fname := ".//pb1.bin"
 	in, err := ioutil.ReadFile(fname)
 	if err != nil {
@@ -61,37 +65,80 @@ func (nb *NumBlocks) Put(blocknumber *big.Int, blockhash common.Hash, nodeId dis
 	for _, v := range tIds{
 		ticketIds.TicketId = append(ticketIds.TicketId, v.Bytes())
 	}
-
-	return ErrFaile
+	return nil
 }
 
-func (nb *NumBlocks) Del(ticketIds []common.Hash) error {
+func (nb *NumBlocks) Del(blocknumber *big.Int, blockhash common.Hash, nodeId discover.NodeID, tIds []common.Hash) error {
 
+	blockNodes, ok := nb.NBlocks[blocknumber.String()]	//to -> func check(){...}
+	if !ok {
+		return ErrNotfindFromblockNumber
+	}
+	nodeTicketIds, ok := blockNodes.BNodes[blockhash.String()]
+	if !ok {
+		return ErrNotfindFromblockHash
+	}
+	ticketIds, ok := nodeTicketIds.NTickets[nodeId.String()]
+	if !ok {
+		return ErrNotfindFromnodeId
+	}
 
+	//对比下list储存的时候删除快，还是map储存的时候删除快
+	//for i:=
 
-
-	return ErrFaile
+	for _, tin := range tIds {
+		for i, tcache := range ticketIds.TicketId {
+			if cmp:=bytes.Equal(tin.Bytes(), tcache); cmp{
+				ticketIds.TicketId = append(ticketIds.TicketId[:i], ticketIds.TicketId[i+1:]...)
+				break
+			}
+		}
+	}
+	return nil
 }
 
 func (nb *NumBlocks) Get(blocknumber *big.Int, blockhash common.Hash, nodeId discover.NodeID)([]common.Hash, error) {
 
-
-
-	return []common.Hash{}, ErrFaile
+	blockNodes, ok := nb.NBlocks[blocknumber.String()]
+	if !ok {
+		return nil, ErrNotfindFromblockNumber
+	}
+	nodeTicketIds, ok := blockNodes.BNodes[blockhash.String()]
+	if !ok {
+		return nil, ErrNotfindFromblockHash
+	}
+	ticketIds, ok := nodeTicketIds.NTickets[nodeId.String()]
+	if !ok {
+		return nil, ErrNotfindFromnodeId
+	}
+	ret := make([]common.Hash, 0)
+	for _, v := range ticketIds.TicketId{
+		ret = append(ret, common.BytesToHash(v))
+	}
+	return ret, nil
 }
 
-func (nb *NumBlocks) Hash(blockhash common.Hash) common.Hash {
+func (nb *NumBlocks) Hash(blocknumber *big.Int, blockhash common.Hash) (common.Hash, error) {
+	ret := common.Hash{}
+	blockNodes, ok := nb.NBlocks[blocknumber.String()]
+	if !ok {
+		return ret, ErrNotfindFromblockNumber
+	}
+	nodeTicketIds, ok := blockNodes.BNodes[blockhash.String()]
+	if !ok {
+		return ret, ErrNotfindFromblockHash
+	}
+	fmt.Println(nodeTicketIds)
+	// ret = sha3(nodeTicketIds.NTickets)
 
-
-
-	return common.Hash{}
+	return ret, nil
 }
 
-func (nb *NumBlocks) TCount(blockhash common.Hash, nodeId discover.NodeID) *big.Int {
+func (nb *NumBlocks) TCount(blocknumber *big.Int, blockhash common.Hash, nodeId discover.NodeID)(*big.Int, error) {
 
 
-	
-	return big.NewInt(0)
+
+	return big.NewInt(0), nil
 }
 
 func (nb *NumBlocks) Commit() error {
