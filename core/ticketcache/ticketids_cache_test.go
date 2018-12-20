9ab11f1cfaa5441f812f27d98fe339cd729a88c1
caiguopei <caiguopei@juzix.io>
2018-12-20 15:13:00 +0800

    票池缓存单元测试

--- core/ticketcache/ticketids_cache_test.go
+++ core/ticketcache/ticketids_cache_test.go
@@ -1,32 +1,35 @@
 package ticketcache
 
 import (
-	_ "Platon-go/common/hexutil"
-	_ "github.com/syndtr/goleveldb/leveldb"
+	"Platon-go/common"
+	"Platon-go/ethdb"
+	"fmt"
+	"math/big"
 	"testing"
 )
 
-func Test(t *testing.T) {
+func Test_All(t *testing.T)  {
+	ldb, err := ethdb.NewLDBDatabase("./data/platon/chaindata", 0, 0)
+	if err!=nil {
+		t.Errorf("NewLDBDatabase faile")
+	}
+	defer ldb.Close()
 
-	//filename := ".//testDb"
-	//db, err := leveldb.OpenFile(filename, nil)
-	//fmt.Println("open err: ", err.Error())
-	//if err==nil {
-	//	cache, err :=db.Get(ticketPoolCacheKey, nil)
-	//	fmt.Println("get cache: ", hexutil.Encode(cache), " err: ", err.Error())
-	//	//err = db.Put(ticketPoolCacheKey, []byte("aabbccdd"), nil);
-	//	//fmt.Println("set cache err: ", err.Error())
-	//}
+	number := big.NewInt(0)
+	bkhash := common.HexToHash("0xe58643cfb17fc50b8579784e134d36bb4021ed70f9baac84190d9260dc005a10")
+	tc := NewTicketIdsCache(ldb)
 
+	mapCache := tc.GetNodeTicketsMap(number, bkhash)
+	fmt.Println("change befor: ", mapCache)
+	mapCache["nodeid1"] = []common.Hash{common.HexToHash("0x8959adf8343c5d256feb9524b027815a18e32a379de8ecc84b515b07be405c12"), common.HexToHash("0xc3b9798e430fe0b647cc91d57141799c2455fff66c485dd1d7f9595363bdb0d0")}
+	fmt.Println("change after: ", mapCache)
 
-	//instance, _ := newTicketIdsCache()
-	//blocknumber := big.NewInt(600)
-	//blockhash := common.HexToHash("0x5678901234567890123456789012345678901234567890123456789012345678")
-	//nodeId := discover.MustHexID("0x01234567890121345678901123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345")
-	//tIds := make([]common.Hash, 0)
-	//for i:=0; i<10 ; i++ {
-	//	tIds = append(tIds, common.HexToHash("0xff789012345678901234567890123456789012345678901234567890123456ff"))
-	//}
-	//instance.Put(blocknumber, blockhash, nodeId, tIds)
-	//instance.Commit()
+	numberchild := big.NewInt(1)
+	bkhashchild := common.HexToHash("0xf148e650a37cc218268d8cb91972814b63f1f02fc87f28c24e0ed15d3ad4aca5")
+	tc.Submit2Cache(numberchild, bkhashchild, mapCache)
+
+	chash, err:= tc.Hash(number, bkhash)
+	fmt.Println("hash: ", chash.Hex())
+	tc.Commit(ldb)
+
 }
