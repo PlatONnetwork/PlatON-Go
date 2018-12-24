2891e81e99296000e3ab0edaf5e0dc8e17a7a6da
liuzhiqiang <waxs2006>
2018-12-24 18:25:39 +0800

    合并0.2.0版本cbft

--- consensus/cbft/rotating.go
+++ consensus/cbft/rotating.go
@@ -6,10 +6,10 @@ import (
 
 type rotating struct {
 	ppos         *ppos
-	rotaList     []common.Address // 本轮循环出块节点顺序列表
-	startTime    int64            // 本轮循环开始时间戳，单位毫秒
-	endTime      int64            // 本轮循环结束时间戳，单位毫秒
-	timeInterval int64            // 每个节点出块时间，单位毫秒
+	rotaList     []common.Address // This round of cyclic block node order list
+	startTime    int64            // The current cycle start timestamp, in milliseconds
+	endTime      int64            // The current cycle end timestamp, in milliseconds
+	timeInterval int64            // Block time per unit, in milliseconds
 }
 
 func newRotating(ppos *ppos, timeInterval int64) *rotating {
@@ -21,13 +21,13 @@ func newRotating(ppos *ppos, timeInterval int64) *rotating {
 }
 
 func sort() {
-	// 新一轮共识的排序函数
-	// xor(上一轮的最后区块hash + 节点公钥地址)
+	// New round of consensus sorting function
+	// xor(Last block last block hash + node public key address)
 }
 
 func (r *rotating) IsRotating(common.Address) bool {
-	// 判断当前节点是否轮值出块
-	// 根据共识排序以及时间窗口
+	// Determine whether the current node is out of order
+	// Sort by consensus and time window
 	return false
 }
 
