da265855be418a5c4072c59b0d84e1c4da006bce
chenfeixiang <mokeyc@163.com>
2019-01-07 13:13:46 +0800

    update

--- core/vm/ppos_precontracts.go
+++ core/vm/ppos_precontracts.go
@@ -113,15 +113,15 @@ func (c *CandidateContract) Run(input []byte) ([]byte, error) {
 	}
 	funcValue := command[byteutil.BytesToString(source[1])]
 	// validate transaction type
-	var TxType = map[uint64]interface{}{
-		1001: c.CandidateDeposit,
-		1002: c.CandidateApplyWithdraw,
-		1003: c.CandidateWithdraw,
-		1004: c.SetCandidateExtra,
+	var txTypeMap = map[string]uint64{
+		"CandidateDeposit":       1001,
+		"CandidateApplyWithdraw": 1002,
+		"CandidateWithdraw":      1003,
+		"SetCandidateExtra":      1004,
 	}
-	if txFunc, ok := TxType[binary.BigEndian.Uint64(source[0])]; ok {
-		if reflect.TypeOf(txFunc) != reflect.TypeOf(funcValue) {
-			log.Error("Failed to Run==> ", "ErrTxType: ", ErrTxType.Error())
+	if txType, ok := txTypeMap[byteutil.BytesToString(source[1])]; ok {
+		if txType != binary.BigEndian.Uint64(source[0]) {
+			log.Error("Failed to execute==> ", "ErrTxType: ", ErrTxType.Error())
 			return nil, ErrTxType
 		}
 	}
