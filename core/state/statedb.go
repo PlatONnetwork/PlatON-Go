eb3017d75825fe1bf77a1a5ebc06e8791052b36b
yangzhou <yangzhou@juzix.io>
2019-01-15 22:59:37 -0800

    modify statedb add blank

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -355,6 +355,7 @@ func getKeyValue(address common.Address, key []byte, value []byte) (string, comm
 	//if value != nil && !bytes.Equal(value,[]byte{}){
 	buffer.Reset()
 	buffer.WriteString(string(value))
+
 	valueKey := common.Hash{}
 	keccak := sha3.NewKeccak256()
 	keccak.Write(value)
