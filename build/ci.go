7471f60ac1808454c8b11567739d718c03a07281
zhangrongjin <zhangrongjin@juzix.net>
2019-01-25 11:01:18 +0800

    Merge remote-tracking branch 'remotes/origin/pangu_0.1.1_dev' into 0.3.1 vc

--- build/ci.go
+++ build/ci.go
@@ -219,6 +219,7 @@ func doInstall(cmdline []string) {
 		mpc     = flag.String("mpc", "off", "Switch of mpc , on for compiling MPC, off for without compiling")
 		gcflags = flag.String("gcflags", "", "Turn off compiler code optimization and function inlining")
 		vc      = flag.String("vc", "off", "Switch of vc , on for compiling VC, off for without compiling")
+		mv      = flag.String("mv", "off", "Switch of mv , on for compilingMPC and VC, off for without compiling")
 	)
 	flag.CommandLine.Parse(cmdline)
 	env := build.Env()
@@ -255,6 +256,9 @@ func doInstall(cmdline []string) {
 		}
 		if *vc == "on" {
 			goinstall.Args = append(goinstall.Args, "-tags=vcon")
+		}
+		if *mv == "on" {
+			goinstall.Args = append(goinstall.Args, "-tags=mpcon vcon")
 		}
 		goinstall.Args = append(goinstall.Args, packages...)
 		build.MustRun(goinstall)
