5667681afff8396ab5bb6c8e961ebd7efa9cbe57
luowei <luowei@juzix.net>
2019-03-12 15:35:00 +0800

    修改票池存储

--- core/types/ticket.go
+++ core/types/ticket.go
@@ -17,11 +17,28 @@ type Ticket struct {
 	// current block height number when purchasing tickets
 	BlockNumber 	*big.Int
 	// The number of remaining tickets
-	remaining		uint64
+	Remaining		uint64
 }
 
 func (t *Ticket) CalcEpoch(blockNumber *big.Int) *big.Int {
 	result := new(big.Int).SetUint64(0)
 	result.Sub(blockNumber, t.BlockNumber)
 	return result
+}
+
+func (t *Ticket) SubRemaining() {
+	if t.Remaining > 0 {
+		t.Remaining--
+	}
+}
+
+func (t *Ticket) DeepCopy() *Ticket {
+	ticket := &Ticket{
+		t.Owner,
+		new(big.Int).SetUint64(t.Deposit.Uint64()),
+		t.CandidateId,
+		new(big.Int).SetUint64(t.BlockNumber.Uint64()),
+		t.Remaining,
+	}
+	return ticket
 }
