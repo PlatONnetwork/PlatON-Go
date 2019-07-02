e5b801209905388b4cc4c9817e0ce1e777846717
GavinXu520 <meiyan532484710@163.com>
2019-07-02 17:44:13 +0800

    Signed-off-by: GavinXu520 <meiyan532484710@163.com>

--- core/vm/staking_contract_test.go
+++ core/vm/staking_contract_test.go
@@ -3,6 +3,7 @@ package vm
 import (
 	"bytes"
 	"fmt"
+	"github.com/PlatONnetwork/PlatON-Go/common"
 	"github.com/PlatONnetwork/PlatON-Go/common/hexutil"
 	"github.com/PlatONnetwork/PlatON-Go/rlp"
 	"testing"
@@ -13,9 +14,13 @@ func TestRLP_encode (t *testing.T) {
 	var params [][]byte
 	params = make([][]byte, 0)
 
-	fnType, err := rlp.EncodeToBytes(1102)
+	fnType, err := rlp.EncodeToBytes(uint16(1102))
 	if nil != err {
 		fmt.Println("fnType err", err)
+	}else {
+		var num uint16
+		rlp.DecodeBytes(fnType, &num)
+		fmt.Println("num is ", num)
 	}
 	params = append(params, fnType)
 
@@ -32,16 +37,17 @@ func TestRLP_encode (t *testing.T) {
 
 func TestRLP_2 (t *testing.T) {
 
-	/*var GetVerifiersList [][]byte
-	GetVerifiersList = make([][]byte, 0)
-	GetVerifiersList = append(GetVerifiersList, byteutil.Uint64ToBytes(0xf1))
-	GetVerifiersList = append(GetVerifiersList, []byte("GetVerifiersList"))
-	bufGetVerifiersList := new(bytes.Buffer)
-	err := rlp.Encode(bufGetVerifiersList, GetVerifiersList)
+	var params [][]byte
+	params = make([][]byte, 0)
+	params = append(params, common.Uint64ToBytes(1102))
+	params = append(params, []byte("GetVerifiersList"))
+	buf := new(bytes.Buffer)
+	err := rlp.Encode(buf, params)
 	if err != nil {
 		fmt.Println(err)
 		t.Errorf("GetVerifiersList encode rlp data fail")
 	} else {
-		fmt.Println("GetVerifiersList data rlp: ", hexutil.Encode(bufGetVerifiersList.Bytes()))
-	}*/
+		fmt.Println("GetVerifiersList data rlp: ", hexutil.Encode(buf.Bytes()))
+	}
+
 }
