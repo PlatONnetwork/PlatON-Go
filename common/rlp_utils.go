c863900a330ed777cfc54a76c1981e3b21103d19
lvxiaoyi <lvxiaoyi@juzix.net>
2019-07-03 11:51:21 +0800

    add rlp_utils.go

---
+++ common/rlp_utils.go
@@ -0,0 +1,20 @@
+package common
+
+import "github.com/PlatONnetwork/PlatON-Go/rlp"
+
+func MustRlpEncode(val interface{}) []byte {
+	bytes, err := rlp.EncodeToBytes(val)
+	if err != nil {
+		panic(err)
+	}
+	return bytes
+}
+
+
+func MustRlpDecode(bytes []byte, val interface{}) {
+	err := rlp.DecodeBytes(bytes, val)
+	if err != nil {
+		panic(err)
+	}
+}
+
