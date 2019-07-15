246c80307e1b1e4e214bec23145a67c3f0cf4b5c
GavinXu520 <meiyan532484710@163.com>
2019-07-15 15:51:56 +0800

    Signed-off-by: GavinXu520 <meiyan532484710@163.com>

--- x/plugin/staking_plugin.go
+++ x/plugin/staking_plugin.go
@@ -35,7 +35,7 @@ var (
 	DelegateVonNotEnough       = common.NewBizError("The von of delegate is not enough")
 	WithdrewDelegateVonCalcErr = common.NewBizError("withdrew delegate von calculate err")
 	ParamsErr                  = common.NewBizError("the fn params err")
-	ProcessVersionErr          = common.NewBizError("The version of the relates node's process is too low")
+
 	BlockNumberDisordered      = common.NewBizError("The blockNumber is disordered")
 	VonAmountNotRight          = common.NewBizError("The amount of von is not right")
 	CandidateNotExist          = common.NewBizError("The candidate is not exist")
@@ -234,30 +234,7 @@ func (sk *StakingPlugin) GetCandidateInfoByIrr(addr common.Address) (*staking.Ca
 }
 
 func (sk *StakingPlugin) CreateCandidate(state xcom.StateDB, blockHash common.Hash, blockNumber,
-amount *big.Int, processVersion uint32, typ uint16, addr common.Address, can *staking.Candidate) error {
-
-	// Query current active version
-	curr_version := govp.GetActiveVersion(state)
-
-	currVersion := xutil.CalcVersion(curr_version)
-	inputVersion := xutil.CalcVersion(processVersion)
-
-	var isDeclareVersion bool
-
-	// Compare version
-	// Just like that:
-	// eg: 2.1.x == 2.1.x; 2.1.x > 2.0.x
-	if inputVersion < currVersion {
-
-		log.Error("Failed to CreateCandidate on stakingPlugin: input version  less than current valid version",
-			"input Version", xutil.ProcessVerion2Str(processVersion), "current Large Version",
-			xutil.ProcessVerion2Str(curr_version))
-
-		return ProcessVersionErr
-	} else if inputVersion > currVersion {
-		isDeclareVersion = true
-	}
-	can.ProcessVersion = currVersion
+amount *big.Int, typ uint16, addr common.Address, can *staking.Candidate) error {
 
 	// from account free von
 	if typ == FreeOrigin {
@@ -297,14 +274,7 @@ amount *big.Int, processVersion uint32, typ uint16, addr common.Address, can *st
 		return err
 	}
 
-	if isDeclareVersion {
-		// Declare new Version
-		err := govp.DeclareVersion(can.StakingAddress, can.NodeId, processVersion, blockHash, blockNumber.Uint64(), state)
-		if nil != err {
-			log.Error("Call CreateCandidate with govplugin DelareVersion failed",
-				"blockNumber", blockNumber.Uint64(), "blockHash", blockHash.Hex(),"err", err)
-		}
-	}
+
 	// todo test
 	//canJson, _ := json.Marshal(can)
 	//fmt.Println("Created the can:", string(canJson))
@@ -2226,7 +2196,7 @@ func (sk *StakingPlugin) ProposalPassedNotify(blockHash common.Hash, blockNumber
 	processVersion uint32) error {
 
 	log.Debug("Call ProposalPassedNotify to promote candidate processVersion", "blockNumber", blockNumber,
-		"blockHash", blockHash.Hex(), "version", processVersion, "nodeId num", len(nodeIds))
+		"blockHash", blockHash.Hex(), "version", processVersion, "nodeIdQueueSize", len(nodeIds))
 
 	for _, nodeId := range nodeIds {
 
