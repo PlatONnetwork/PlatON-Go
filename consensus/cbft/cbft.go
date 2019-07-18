087c5020241c12d512adb006619d4b506b2da2bb
TraceBundy <yangzhou221@gmail.com>
2019-07-18 16:54:02 +0800

    Syncblock (#73)
    * add process function
    
    * add qc block protocol
    
    * add todo
    
    * add fetcher
    
    * add fetcher test
    
    * remove annotion
    
    * add fecher logical
    
    * add fetcher
    
    * add fetcher
    
    * change msginfo struct
    
    * change update

--- consensus/cbft/cbft.go
+++ consensus/cbft/cbft.go
@@ -39,16 +39,17 @@ type Config struct {
 }
 
 type Cbft struct {
-	config     Config
-	eventMux   *event.TypeMux
-	closeOnce  sync.Once
-	exitCh     chan struct{}
-	txPool     consensus.TxPoolReset
-	blockChain consensus.ChainReader
-	peerMsgCh  chan *ctypes.MsgInfo
-	syncMsgCh  chan *ctypes.MsgInfo
-	evPool     evidence.EvidencePool
-	log        log.Logger
+	config           Config
+	eventMux         *event.TypeMux
+	closeOnce        sync.Once
+	exitCh           chan struct{}
+	txPool           consensus.TxPoolReset
+	blockChain       consensus.ChainReader
+	blockCacheWriter consensus.BlockCacheWriter
+	peerMsgCh        chan *ctypes.MsgInfo
+	syncMsgCh        chan *ctypes.MsgInfo
+	evPool           evidence.EvidencePool
+	log              log.Logger
 
 	// Async call channel
 	asyncCallCh chan func()
@@ -57,8 +58,6 @@ type Cbft struct {
 	// Control the current view state
 	state cstate.ViewState
 
-	// Execution block function
-	execute consensus.Executor
 	// Block asyncExecutor, the block responsible for executing the current view
 	asyncExecutor executor.AsyncBlockExecutor
 
@@ -99,10 +98,10 @@ func New(sysConfig *params.CbftConfig, optConfig *OptionsConfig, eventMux *event
 	return cbft
 }
 
-func (cbft *Cbft) Start(chain consensus.ChainReader, executorFn consensus.Executor, txPool consensus.TxPoolReset, agency consensus.Agency) error {
+func (cbft *Cbft) Start(chain consensus.ChainReader, blockCacheWriter consensus.BlockCacheWriter, txPool consensus.TxPoolReset, agency consensus.Agency) error {
 	cbft.blockChain = chain
 	cbft.txPool = txPool
-	cbft.asyncExecutor = executor.NewAsyncExecutor(executorFn)
+	cbft.asyncExecutor = executor.NewAsyncExecutor(blockCacheWriter.Execute)
 	cbft.validatorPool = validator.NewValidatorPool(agency, chain.CurrentHeader().Number.Uint64(), cbft.config.sys.NodeID)
 
 	//Initialize block tree
@@ -380,8 +379,8 @@ func (cbft *Cbft) Close() error {
 		}
 		close(cbft.exitCh)
 	})
-	if cbft.executor != nil{
-		cbft.executor.Stop()
+	if cbft.asyncExecutor != nil {
+		cbft.asyncExecutor.Stop()
 	}
 	return nil
 }
@@ -504,4 +503,3 @@ func (cbft *Cbft) commitBlock(block *types.Block, qc *ctypes.QuorumCert) {
 		SyncState: nil,
 	})
 }
-
