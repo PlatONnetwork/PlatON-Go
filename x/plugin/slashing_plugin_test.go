0beea333fa6598a423f7b8027cc6164a781a1d38
luowei <luowei@juzix.net>
2019-07-24 20:09:31 +0800

    update unit test

--- x/plugin/slashing_plugin_test.go
+++ x/plugin/slashing_plugin_test.go
@@ -362,7 +362,7 @@ func TestSlashingPlugin_Slash(t *testing.T) {
 		return
 	}
 	err = si.Slash(evidence, common.ZeroHash, blockNumber.Uint64(), stateDB, common.HexToAddress("0x120b77ab712589ebd42d69003893ef962cc52800"))
-	assert.NotNil(t, err)
+	assert.Nil(t, err)
 	data = `{
           "duplicate_prepare": [
             {
@@ -393,7 +393,7 @@ func TestSlashingPlugin_Slash(t *testing.T) {
 		return
 	}
 	err = si.Slash(evidence, common.ZeroHash, blockNumber.Uint64(), stateDB, common.HexToAddress("0x120b77ab712589ebd42d69003893ef962cc52800"))
-	assert.NotNil(t, err)
+	assert.Nil(t, err)
 }
 
 func TestSlashingPlugin_CheckMutiSign(t *testing.T) {
