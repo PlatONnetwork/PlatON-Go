c74c7370feb43fce9f3f4d7ee311d09dcdc9e0d2
wutao <1102252651@qq.com>
2019-07-26 17:31:08 +0800

    reset rlp encode test

--- rlp/encode_test.go
+++ rlp/encode_test.go
@@ -19,6 +19,7 @@ package rlp
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
@@ -287,60 +288,42 @@ func boolToBytes(val bool) []byte {
 
 func TestDecodeEncode(t *testing.T) {
 	///////////////////////////////////////////////////////////////////////////////
-	// nodeId, _ := hex.DecodeString("e152be5f5f0167250592a12a197ab19b215c5295d5eb0bb1133673dc8607530db1bfa5415b2ec5e94113f2fce0c4a60e697d5d703a29609b197b836b020446c7")
-	// owner, _ := hex.DecodeString("4FED1fC4144c223aE3C1553be203cDFcbD38C581")
-	//
-	// var source [][]byte
-	// source = make([][]byte, 0)
-	// source = append(source, uint64ToBytes(0xf1))
-	// source = append(source, []byte("CandidateDeposit"))
-	// source = append(source, nodeId)
-	// source = append(source, owner)
-	// source = append(source, uint64ToBytes(500)) //10000
-	// source = append(source, []byte("127.0.0.1"))
-	// source = append(source, []byte("7890"))
-	// source = append(source, []byte("extra data"))
-	//
-	// //rlp Encode
-	// buffer := new(bytes.Buffer)
-	// err := Encode(buffer, source)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	t.Errorf("fail")
-	// } else {
-	// 	fmt.Println("encode_hex_string: ", hexutil.Encode(buffer.Bytes()))
-	// 	fmt.Println("encode_bytes: ", buffer.Bytes())
-	// }
-	//
-	// //rlp Decode
-	// ptr := new(interface{})
-	// if err := Decode(bytes.NewReader(buffer.Bytes()), &ptr); err != nil {
-	// 	fmt.Println(err)
-	// 	t.Errorf("fail")
-	// } else {
-	// 	deref := reflect.ValueOf(ptr).Elem().Interface()
-	// 	for i, v := range deref.([]interface{}) {
-	// 		fmt.Println(i, ": ", hex.EncodeToString(v.([]byte)))
-	// 	}
-	// }
+	nodeId, _ := hex.DecodeString("e152be5f5f0167250592a12a197ab19b215c5295d5eb0bb1133673dc8607530db1bfa5415b2ec5e94113f2fce0c4a60e697d5d703a29609b197b836b020446c7")
+	owner, _ := hex.DecodeString("4FED1fC4144c223aE3C1553be203cDFcbD38C581")
 
-	data := []byte{206, 136, 27, 193, 109, 103, 78, 200, 0, 0, 128, 128, 194, 51, 50}
+	var source [][]byte
+	source = make([][]byte, 0)
+	source = append(source, uint64ToBytes(0xf1))
+	source = append(source, []byte("CandidateDeposit"))
+	source = append(source, nodeId)
+	source = append(source, owner)
+	source = append(source, uint64ToBytes(500)) //10000
+	source = append(source, []byte("127.0.0.1"))
+	source = append(source, []byte("7890"))
+	source = append(source, []byte("extra data"))
 
-	var result RestrictingInfo
-
-	err := Decode(bytes.NewBuffer(data), &result)
+	// rlp Encode
+	buffer := new(bytes.Buffer)
+	err := Encode(buffer, source)
 	if err != nil {
-		t.Fatal(err.Error())
+		fmt.Println(err)
+		t.Errorf("fail")
 	} else {
-		t.Log(result)
+		fmt.Println("encode_hex_string: ", hexutil.Encode(buffer.Bytes()))
+		fmt.Println("encode_bytes: ", buffer.Bytes())
 	}
-}
 
-type RestrictingInfo struct {
-	Balance     *big.Int // Balance representation all locked amount
-	Debt        *big.Int // Debt representation will released amount.
-	DebtSymbol  bool     // Debt is owed to release in the past while symbol is true, else Debt can be used instead of release
-	ReleaseList []uint64 // ReleaseList representation which epoch will release restricting
+	// rlp Decode
+	ptr := new(interface{})
+	if err := Decode(bytes.NewReader(buffer.Bytes()), &ptr); err != nil {
+		fmt.Println(err)
+		t.Errorf("fail")
+	} else {
+		deref := reflect.ValueOf(ptr).Elem().Interface()
+		for i, v := range deref.([]interface{}) {
+			fmt.Println(i, ": ", hex.EncodeToString(v.([]byte)))
+		}
+	}
 }
 
 func TestEncodeF03(t *testing.T) {
