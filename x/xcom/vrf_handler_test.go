6a285fddde39db2708229bd7b31b79f80ef527eb
luowei <luowei@juzix.net>
2019-07-29 14:44:25 +0800

    Update unit test error handling

--- x/xcom/vrf_handler_test.go
+++ x/xcom/vrf_handler_test.go
@@ -33,27 +33,24 @@ func TestVrfHandler_StorageLoad(t *testing.T) {
 	hash := common.ZeroHash
 	for i := 0; i < int(EpochValidatorNum())+10; i++ {
 		if err := vh.db.NewBlock(blockNumber, phash, common.ZeroHash); nil != err {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		pi, err := vh.GenerateNonce(blockNumber, phash)
 		if nil != err {
-			t.Error(err)
-			return
+			t.Fatal(err)
 		}
 		if err := vh.Storage(blockNumber, phash, common.ZeroHash, vrf.ProofToHash(pi)); nil != err {
-			t.Error(err)
-			return
+			t.Fatal(err)
 		}
 		hash = common.BytesToHash([]byte(strconv.Itoa(i)))
 		phash = hash
 		if err := vh.db.Flush(hash, blockNumber); nil != err {
-			t.Error(err)
-			return
+			t.Fatal(err)
 		}
 		blockNumber.Add(blockNumber, common.Big1)
 	}
 	if value, err := vh.Load(phash); nil != err {
-		t.Error(err)
+		t.Fatal(err)
 	} else {
 		assert.Equal(t, len(value), int(EpochValidatorNum()))
 	}
@@ -67,19 +64,19 @@ func TestVrfHandler_Verify(t *testing.T) {
 	blockNumber := new(big.Int).SetUint64(1)
 	hash := common.BytesToHash([]byte("h1"))
 	if value, err := vh.GenerateNonce(blockNumber, common.Hash{}); nil != err {
-		t.Error(err)
+		t.Fatal(err)
 	} else {
 		if err := vh.VerifyVrf(&sk.PublicKey, blockNumber, hash, common.ZeroHash, value); nil != err {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		pri, err := crypto.GenerateKey()
 		if err != nil {
-			panic(err)
+			t.Fatal(err)
 		}
 		vh.SetPrivateKey(pri)
 		nonce, err := vh.GenerateNonce(blockNumber, common.Hash{})
 		if nil != err {
-			t.Error(err)
+			t.Fatal(err)
 		}
 		err = vh.VerifyVrf(&sk.PublicKey, blockNumber, hash, common.ZeroHash, nonce)
 		assert.Equal(t, ErrInvalidVrfProve, err)
