3564c0df195f6a5299d59bae7a0ada8b17ff9545
Joey <bglmmz@hotmail.com>
2019-08-21 19:33:23 +0800

    add unit test

--- x/xcom/crypto_handler_test.go
+++ x/xcom/crypto_handler_test.go
@@ -14,8 +14,11 @@ import (
 )
 
 var (
-	priKey = crypto.HexMustToECDSA("8e1477549bea04b97ea15911e2e9b3041b7a9921f80bd6ddbe4c2b080473de22")
-	nodeID = discover.MustHexID("3e7864716b671c4de0dc2d7fd86215e0dcb8419e66430a770294eb2f37b714a07b6a3493055bb2d733dee9bfcc995e1c8e7885f338a69bf6c28930f3cf341819")
+	//priKey = crypto.HexMustToECDSA("8e1477549bea04b97ea15911e2e9b3041b7a9921f80bd6ddbe4c2b080473de22")
+	priKey = crypto.HexMustToECDSA("8c56e4a0d8bb1f82b94231d535c499fdcbf6e06221acf669d5a964f5bb974903")
+
+	//nodeID = discover.MustHexID("3e7864716b671c4de0dc2d7fd86215e0dcb8419e66430a770294eb2f37b714a07b6a3493055bb2d733dee9bfcc995e1c8e7885f338a69bf6c28930f3cf341819")
+	nodeID = discover.MustHexID("3a06953a2d5d45b29167bef58208f1287225bdd2591260af29ae1300aeed362e9b548369dfc1659abbef403c9b3b07a8a194040e966acd6e5b6d55aa2df7c1d8")
 )
 
 func initChandlerHandler() {
@@ -42,7 +45,7 @@ func Test_Decode(t *testing.T) {
 	initChandlerHandler()
 	log.Root().SetHandler(log.CallerFileHandler(log.LvlFilterHandler(log.Lvl(6), log.StreamHandler(os.Stderr, log.TerminalFormat(true)))))
 
-	versionSign, err := hex.DecodeString("b195655dd28594ead36cd9291a9b09b29630e188a3dff96ed7af145167ae86e648698f8b2dcc89a6f73018b7a69549b97bda6c61d10fbbd46d8a70b867b3be2b00")
+	versionSign, err := hex.DecodeString("e9626170e0c02d86c329193af7d2138813596f7bfdad16608015f92873a40b70641130f04f76cb506de059462472f9f0041bdb37e9be18303e2bbab6b6e61d0401")
 	if err != nil {
 		log.Error("Decode hex String", "err", err)
 		return
