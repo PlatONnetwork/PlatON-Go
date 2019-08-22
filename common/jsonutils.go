1919950cce4e36b50e413f656b7bd02ab1e9c464
Joey <bglmmz@hotmail.com>
2019-08-22 11:47:13 +0800

    add jsonutils

---
+++ common/jsonutils.go
@@ -0,0 +1,24 @@
+package common
+
+import (
+	"encoding/json"
+
+	"github.com/PlatONnetwork/PlatON-Go/log"
+)
+
+func MustToString(obj interface{}) string {
+	if obj == nil {
+		return ""
+	}
+	bs, err := json.Marshal(obj)
+	log.Error("cannot marshal object", "err", err)
+	return string(bs)
+}
+
+func MustToObject(bs []byte, objRefer *interface{}) {
+	if len(bs) == 0 {
+		return
+	}
+	err := json.Unmarshal(bs, objRefer)
+	log.Error("cannot unmarshal to object", "err", err)
+}
