63ba21404d731fd70322cf07baff028a28f6e327
liuzhiqiang <waxs2006>
2019-08-30 10:35:46 +0800

    safety_rules unit test

--- consensus/cbft/rules/safety_rules_test.go
+++ consensus/cbft/rules/safety_rules_test.go
@@ -3,8 +3,11 @@ package rules
 import (
 	"fmt"
 	"math/big"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/PlatONnetwork/PlatON-Go/consensus/cbft/utils"
 
 	"github.com/PlatONnetwork/PlatON-Go/common"
 	"github.com/PlatONnetwork/PlatON-Go/consensus/cbft/protocols"
@@ -13,6 +16,27 @@ import (
 	"github.com/PlatONnetwork/PlatON-Go/core/types"
 	"github.com/PlatONnetwork/PlatON-Go/params"
 	"github.com/stretchr/testify/assert"
+)
+
+const (
+	// prepareBlock
+	overIndexLimit       = "blockIndex higher than amount"
+	existIndex           = "blockIndex already exists"
+	firstBlockNotQCChild = "the first index block is not contiguous by local highestQC or highestLock"
+	notExistPreIndex     = "previous index block not exists"
+	diffPreIndexBlock    = "non contiguous index block"
+
+	viewNumberTooLow = "viewNumber too low"
+	needChangeView   = "need change view"
+	needFetchBlock   = "epoch higher than local"
+
+	// prepareVote
+	overVoteIndexLimit = "voteIndex higher than amount"
+	existVote          = "prepare vote has exist"
+	noExistPrepare     = "current index block not existed"
+	viewNumberTooHigh  = "viewNumber higher than local"
+	epochTooLow        = "epoch too low"
+	epochTooHigh       = "new epoch, need fetch blocks"
 )
 
 func NewBlock(parent common.Hash, number uint64) *types.Block {
@@ -73,56 +97,82 @@ func newEpochViewNumberState(epoch, viewNumber uint64, amount uint32) (*state.Vi
 	return viewState, blockTree
 }
 
-func testBaseSafetyRulesPrepareBlockRules(t *testing.T, viewState *state.ViewState, blockTree *ctypes.BlockTree, rules SafetyRules, amount uint32) {
-	type testCase struct {
-		err          error
-		common       bool
-		fetch        bool
-		newView      bool
-		fetchPrepare bool
-		pb           *protocols.PrepareBlock
-	}
+type testCase struct {
+	err          error
+	common       bool
+	fetch        bool
+	newView      bool
+	fetchPrepare bool
+	pb           *protocols.PrepareBlock
+	pv           *protocols.PrepareVote
+	vc           *protocols.ViewChange
+}
 
-	newPrepareBlock := func(epoch, viewNumber uint64, block *types.Block, blockIndex uint32) *protocols.PrepareBlock {
-		return &protocols.PrepareBlock{
-			Epoch:      epoch,
-			ViewNumber: viewNumber,
-			Block:      block,
-			BlockIndex: blockIndex,
-		}
-	}
-
-	qcBlock := viewState.HighestQCBlock()
-	tests := []testCase{
-		{newCommonError(""), true, false, false, false, newPrepareBlock(Epoch, ViewNumber, qcBlock, 1)},
-		{newCommonError(""), false, false, false, false, newPrepareBlock(Epoch, ViewNumber, qcBlock, amount+1)},
-		{newFetchError(""), false, true, false, false, newPrepareBlock(Epoch+1, ViewNumber, qcBlock, 1)},
-		{newFetchError(""), false, true, false, false, newPrepareBlock(Epoch, ViewNumber+1, qcBlock, 1)},
-		{newFetchError(""), false, true, false, false, newPrepareBlock(Epoch, ViewNumber+1, NewBlock(qcBlock.Hash(), qcBlock.NumberU64()), 0)},
-		{newViewError(""), false, false, true, false, newPrepareBlock(Epoch+1, ViewNumber, NewBlock(qcBlock.Hash(), qcBlock.NumberU64()+1), 0)},
-	}
+func invokePrepareBlockRules(t *testing.T, rules SafetyRules, tests []testCase) {
 	for i, c := range tests {
 		if c.err == nil {
 			assert.Nil(t, rules.PrepareBlockRules(c.pb), fmt.Sprintf("case:%d failed", i))
 		} else {
 			err := rules.PrepareBlockRules(c.pb)
 			assert.NotNil(t, err, fmt.Sprintf("case:%d failed", i), err)
+			assert.Equal(t, c.common, err.Common(), fmt.Sprintf("case:%d failed %s", i, c.pb.String()), err)
 			assert.Equal(t, c.fetch, err.Fetch(), fmt.Sprintf("case:%d failed %s", i, c.pb.String()), err)
-			assert.Equal(t, c.newView, err.NewView(), fmt.Sprintf("case:%d failed", i), err)
+			assert.Equal(t, c.newView, err.NewView(), fmt.Sprintf("case:%d failed %s", i, c.pb.String()), err)
+			assert.Equal(t, c.fetchPrepare, err.FetchPrepare(), fmt.Sprintf("case:%d failed %s", i, c.pb.String()), err)
+			assert.True(t, strings.HasPrefix(err.Error(), c.err.Error()))
 		}
 	}
 }
 
-func testBaseSafetyRulesPrepareVoteRules(t *testing.T, viewState *state.ViewState, blockTree *ctypes.BlockTree, rules SafetyRules, amount uint32) {
-	type testCase struct {
-		err          error
-		common       bool
-		fetch        bool
-		newView      bool
-		fetchPrepare bool
-		pb           *protocols.PrepareVote
+func testBaseSafetyRulesPrepareBlockRules(t *testing.T, viewState *state.ViewState, blockTree *ctypes.BlockTree, rules SafetyRules, amount uint32) {
+	newPrepareBlock := func(epoch, viewNumber uint64, parentHash common.Hash, blockNumber uint64, blockIndex uint32, viewChangeQC *ctypes.ViewChangeQC) *protocols.PrepareBlock {
+		return &protocols.PrepareBlock{
+			Epoch:        epoch,
+			ViewNumber:   viewNumber,
+			Block:        NewBlock(parentHash, blockNumber),
+			BlockIndex:   blockIndex,
+			ViewChangeQC: viewChangeQC,
+		}
+	}
+	newViewChangeQC := func(epoch, viewNumber uint64, blockHash common.Hash, blockNumber uint64) *ctypes.ViewChangeQC {
+		return &ctypes.ViewChangeQC{
+			QCs: []*ctypes.ViewChangeQuorumCert{
+				{
+					Epoch:       epoch,
+					ViewNumber:  viewNumber,
+					BlockHash:   blockHash,
+					BlockNumber: blockNumber,
+				},
+			},
+		}
 	}
 
+	qcBlock := viewState.HighestQCBlock()
+	lockBlock := viewState.HighestLockBlock()
+	commitBlock := viewState.HighestCommitBlock()
+	nextIndex := viewState.NextViewBlockIndex()
+	tests := []testCase{
+		{newCommonError(overIndexLimit), true, false, false, false, newPrepareBlock(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64()+1, amount+1, nil), nil, nil},
+		{newCommonError(existIndex), true, false, false, false, newPrepareBlock(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64()+1, 1, nil), nil, nil},
+		{newCommonError(notExistPreIndex), false, false, false, true, newPrepareBlock(Epoch, ViewNumber, lockBlock.Hash(), lockBlock.NumberU64()+1, nextIndex+1, nil), nil, nil},
+		{newCommonError(diffPreIndexBlock), true, false, false, false, newPrepareBlock(Epoch, ViewNumber, lockBlock.Hash(), lockBlock.NumberU64()+1, nextIndex, nil), nil, nil},
+		{nil, false, false, false, false, newPrepareBlock(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), nextIndex, nil), nil, nil},
+	}
+	invokePrepareBlockRules(t, rules, tests)
+	// change the view
+	viewState.ResetView(Epoch, ViewNumber+1)
+	tests = []testCase{
+		{newCommonError(firstBlockNotQCChild), true, false, false, false, newPrepareBlock(Epoch, ViewNumber+1, commitBlock.Hash(), commitBlock.NumberU64()+1, 0, nil), nil, nil},
+		{newCommonError(viewNumberTooLow), true, false, false, false, newPrepareBlock(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64()+1, 1, nil), nil, nil},
+		{newViewError(needChangeView), false, false, true, false, newPrepareBlock(Epoch, ViewNumber+2, qcBlock.Hash(), qcBlock.NumberU64()+1, 0, newViewChangeQC(Epoch, ViewNumber+1, qcBlock.Hash(), qcBlock.NumberU64())), nil, nil},
+		{newFetchError(needFetchBlock), false, true, false, false, newPrepareBlock(Epoch+1, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64()+1, 1, nil), nil, nil},
+	}
+	invokePrepareBlockRules(t, rules, tests)
+	// change the view
+	viewState.ResetView(Epoch, ViewNumber)
+}
+
+func testBaseSafetyRulesPrepareVoteRules(t *testing.T, viewState *state.ViewState, blockTree *ctypes.BlockTree, rules SafetyRules, amount uint32) {
 	newPrepareVote := func(epoch, viewNumber uint64, hash common.Hash, number uint64, index uint32) *protocols.PrepareVote {
 		return &protocols.PrepareVote{
 			Epoch:       epoch,
@@ -134,32 +184,32 @@ func testBaseSafetyRulesPrepareVoteRules(t *testing.T, viewState *state.ViewStat
 	}
 
 	qcBlock := viewState.HighestQCBlock()
+	pv := newPrepareVote(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), amount-1)
+	viewState.AddPrepareVote(0, pv)
 
 	tests := []testCase{
-		{newCommonError(""), true, false, false, false, newPrepareVote(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), amount+1)},
-		{newFetchError(""), false, true, false, false, newPrepareVote(Epoch, ViewNumber+1, qcBlock.Hash(), qcBlock.NumberU64(), amount+1)},
-		{newFetchError(""), false, true, false, false, newPrepareVote(Epoch+1, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), 1)},
-		{newCommonError(""), false, false, false, false, newPrepareVote(Epoch-1, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), 1)},
+		{newCommonError(overVoteIndexLimit), true, false, false, false, nil, newPrepareVote(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), amount+1), nil},
+		{newFetchError(existVote), true, false, false, false, nil, pv, nil},
+		{newFetchError(noExistPrepare), false, false, false, true, nil, newPrepareVote(Epoch, ViewNumber, common.BytesToHash(utils.Rand32Bytes(32)), qcBlock.NumberU64()+1, amount-2), nil},
+		{newCommonError(viewNumberTooLow), true, false, false, false, nil, newPrepareVote(Epoch, ViewNumber-1, qcBlock.Hash(), qcBlock.NumberU64(), 0), nil},
+		{newCommonError(viewNumberTooHigh), false, true, false, false, nil, newPrepareVote(Epoch, ViewNumber+1, qcBlock.Hash(), qcBlock.NumberU64()+1, 0), nil},
 	}
 	for i, c := range tests {
 		if c.err == nil {
-			assert.Nil(t, rules.PrepareVoteRules(c.pb), fmt.Sprintf("case:%d failed", i))
+			assert.Nil(t, rules.PrepareVoteRules(c.pv), fmt.Sprintf("case:%d failed", i))
 		} else {
-			err := rules.PrepareVoteRules(c.pb)
-			assert.NotNil(t, err, fmt.Sprintf("case:%d failed", i))
-			assert.Equal(t, c.fetch, err.Fetch(), fmt.Sprintf("case:%d failed", i))
-			assert.Equal(t, c.newView, err.NewView(), fmt.Sprintf("case:%d failed", i))
+			err := rules.PrepareVoteRules(c.pv)
+			assert.NotNil(t, err, fmt.Sprintf("case:%d failed", i), err)
+			assert.Equal(t, c.common, err.Common(), fmt.Sprintf("case:%d failed %s", i, c.pv.String()), err)
+			assert.Equal(t, c.fetch, err.Fetch(), fmt.Sprintf("case:%d failed %s", i, c.pv.String()), err)
+			assert.Equal(t, c.newView, err.NewView(), fmt.Sprintf("case:%d failed %s", i, c.pv.String()), err)
+			assert.Equal(t, c.fetchPrepare, err.FetchPrepare(), fmt.Sprintf("case:%d failed %s", i, c.pv.String()), err)
+			assert.True(t, strings.HasPrefix(err.Error(), c.err.Error()))
 		}
 	}
 }
 
 func testBaseSafetyRulesViewChangeRules(t *testing.T, viewState *state.ViewState, blockTree *ctypes.BlockTree, rules SafetyRules, amount uint32) {
-	type testCase struct {
-		err     error
-		fetch   bool
-		newView bool
-		pb      *protocols.ViewChange
-	}
 	newViewChange := func(epoch, viewNumber uint64, hash common.Hash, number uint64, index uint32) *protocols.ViewChange {
 		return &protocols.ViewChange{
 			Epoch:       epoch,
@@ -172,29 +222,31 @@ func testBaseSafetyRulesViewChangeRules(t *testing.T, viewState *state.ViewState
 	qcBlock := viewState.HighestQCBlock()
 
 	tests := []testCase{
-		{nil, false, false, newViewChange(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), amount+1)},
-		{newError(""), true, false, newViewChange(Epoch, ViewNumber+1, qcBlock.Hash(), qcBlock.NumberU64(), amount+1)},
-		{newError(""), true, false, newViewChange(Epoch+1, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), 1)},
-		{newError(""), false, false, newViewChange(Epoch-1, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), 1)},
+		{nil, false, false, false, false, nil, nil, newViewChange(Epoch, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), amount+1)},
+		{newError(viewNumberTooHigh), false, true, false, false, nil, nil, newViewChange(Epoch, ViewNumber+1, qcBlock.Hash(), qcBlock.NumberU64(), amount+1)},
+		{newError(epochTooHigh), false, true, false, false, nil, nil, newViewChange(Epoch+1, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), 1)},
+		{newError(epochTooLow), true, false, false, false, nil, nil, newViewChange(Epoch-1, ViewNumber, qcBlock.Hash(), qcBlock.NumberU64(), 1)},
 	}
 	for i, c := range tests {
 		if c.err == nil {
-			assert.Nil(t, rules.ViewChangeRules(c.pb), fmt.Sprintf("case:%d failed", i))
+			assert.Nil(t, rules.ViewChangeRules(c.vc), fmt.Sprintf("case:%d failed", i))
 		} else {
-			err := rules.ViewChangeRules(c.pb)
-			assert.NotNil(t, err, fmt.Sprintf("case:%d failed", i))
-			assert.NotNil(t, c.newView, err.NewView(), fmt.Sprintf("case:%d failed", i))
-			assert.NotNil(t, c.fetch, err.Fetch(), fmt.Sprintf("case:%d failed", i))
+			err := rules.ViewChangeRules(c.vc)
+			assert.NotNil(t, err, fmt.Sprintf("case:%d failed", i), err)
+			assert.Equal(t, c.common, err.Common(), fmt.Sprintf("case:%d failed %s", i, c.vc.String()), err)
+			assert.Equal(t, c.fetch, err.Fetch(), fmt.Sprintf("case:%d failed %s", i, c.vc.String()), err)
+			assert.Equal(t, c.newView, err.NewView(), fmt.Sprintf("case:%d failed %s", i, c.vc.String()), err)
+			assert.Equal(t, c.fetchPrepare, err.FetchPrepare(), fmt.Sprintf("case:%d failed %s", i, c.vc.String()), err)
+			assert.True(t, strings.HasPrefix(err.Error(), c.err.Error()))
 		}
 	}
 }
 
 func TestSafetyError(t *testing.T) {
-	viewState, blockTree := newEpochViewNumberState(Epoch, ViewNumber, 10)
+	viewState, blockTree := newEpochViewNumberState(Epoch, ViewNumber, 6)
 	amount := uint32(10)
 	rules := NewSafetyRules(viewState, blockTree, &ctypes.Config{Sys: &params.CbftConfig{Amount: amount}})
 	testBaseSafetyRulesPrepareBlockRules(t, viewState, blockTree, rules, amount)
 	testBaseSafetyRulesPrepareVoteRules(t, viewState, blockTree, rules, amount)
-
 	testBaseSafetyRulesViewChangeRules(t, viewState, blockTree, rules, amount)
 }
