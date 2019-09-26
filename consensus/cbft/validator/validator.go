2c143d02926d2a1bc98474ff44e2a10fc6e77639
mowind <wjinwen.1988@gmail.com>
2019-09-26 21:26:08 +0800

    fix debug log

--- consensus/cbft/validator/validator.go
+++ consensus/cbft/validator/validator.go
@@ -577,7 +577,7 @@ func (vp *ValidatorPool) VerifyAggSigByBA(epoch uint64, vSet *utils.BitArray, ms
 		return err
 	}
 	if !sig.Verify(&pub, string(msg)) {
-		log.Debug("Verify signature fail", "epoch", "vSet", vSet.String(), "msg", hex.EncodeToString(msg), "signature", hex.EncodeToString(signature), "nodeList", nodeList, "validators", validators.String())
+		log.Debug("Verify signature fail", "epoch", epoch, "vSet", vSet.String(), "msg", hex.EncodeToString(msg), "signature", hex.EncodeToString(signature), "nodeList", nodeList, "validators", validators.String())
 		return errors.New("bls verifies signature fail")
 	}
 	return nil
