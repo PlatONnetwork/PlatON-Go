e74fbaa9776bec40caeb9d3af42535916652d16d
mowind <wjinwen.1988@gmail.com>
2019-09-29 15:54:40 +0800

    notify RemoveValidatorEvent all nodes

--- consensus/cbft/validator/validator.go
+++ consensus/cbft/validator/validator.go
@@ -365,9 +365,12 @@ func (vp *ValidatorPool) Update(blockNumber uint64, epoch uint64, eventMux *even
 				eventMux.Post(cbfttypes.AddValidatorEvent{NodeID: nodeID})
 				log.Trace("Post AddValidatorEvent", "nodeID", nodeID.String())
 			}
+		} else {
+			for _, nodeID := range vp.prevValidators.NodeList() {
+				eventMux.Post(cbfttypes.RemoveValidatorEvent{NodeID: nodeID})
+				log.Trace("Post RemoveValidatorEvent", "nodeID", nodeID.String())
+			}
 		}
-
-		// We are still not a consensus node, just update validator list.
 	}
 
 	return nil
