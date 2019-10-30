84b2a3e91ec4c221d78e486e9553716f528e9113
luo-dahui <934803813@qq.com>
2019-10-30 21:06:34 +0800

    Modify the id of seed node and initial verification nodeid,blskey and chainid

--- core/genesis.go
+++ core/genesis.go
@@ -291,7 +291,6 @@ func (g *Genesis) ToBlock(db ethdb.Database, sdb snapshotdb.DB) *types.Block {
 		genesisIssuance = genesisIssuance.Add(genesisIssuance, account.Balance)
 	}
 	log.Debug("genesisIssuance", "amount", genesisIssuance)
-
 	// Store genesis version into governance data
 	if err := genesisPluginState(g, statedb, genesisIssuance, params.GenesisVersion); nil != err {
 		panic("Failed to Store xxPlugin genesis statedb: " + err.Error())
@@ -381,7 +380,7 @@ func GenesisBlockForTesting(db ethdb.Database, addr common.Address, balance *big
 func DefaultGenesisBlock() *Genesis {
 
 	// TODO this should change
-	generalAddr := common.HexToAddress("0x9bbac0df99f269af1473fd384cb0970b95311001")
+	generalAddr := common.HexToAddress("0x5437959B69eD1014cf6Aa8B4a2c77e7Ba2341955")
 	generalBalance, _ := new(big.Int).SetString("9718188019000000000000000000", 10)
 
 	rewardMgrPoolIssue, _ := new(big.Int).SetString("200000000000000000000000000", 10)
