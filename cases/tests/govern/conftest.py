f805140ebdde2269a833440540f0c715974df60f
lipingD <zhouliping@juzix.net>
2019-11-01 18:14:09 +0800

    add conftest fixture candidate_has_proposal

--- cases/tests/govern/conftest.py
+++ cases/tests/govern/conftest.py
@@ -68,3 +68,32 @@ def new_node_has_proposal(global_test_env, client_new_node_obj, client_verifier_
                           pip_obj.node.staking_address, transaction_cfg=pip_obj.cfg.transaction_cfg)
     assert result.get('Code') == 0
     return client_new_node_obj.pip
+
+@pytest.fixture()
+def candidate_has_proposal(global_test_env, client_candidate_obj, client_verifier_obj, client_list_obj):
+    pip_obj = client_verifier_obj.pip
+    if pip_obj.is_exist_effective_proposal():
+        proposalinfo = pip_obj.get_effect_proposal_info_of_vote()
+        log.info('升级提案信息为{}'.format(proposalinfo))
+        if proposalinfo.get('EndVotingBlock') - pip_obj.node.block_number < 2 * pip_obj.economic.consensus_size:
+            global_test_env.deploy_all()
+            normal_node_obj_list = global_test_env.normal_node_list
+            for normal_node_obj in normal_node_obj_list:
+                client_obj = get_client_obj(normal_node_obj.node_id, client_list_obj)
+                address, _ = pip_obj.economic.account.generate_account(pip_obj.node.web3, 10 ** 18 * 10000000)
+                log.info('对节点{}进行质押操作'.format(normal_node_obj.node_id))
+                result = client_obj.staking.create_staking(0, address, address)
+                log.info('节点{}质押结果为{}'.format(normal_node_obj.node_id, result))
+                assert result.get('Code') == 0
+            pip_obj.economic.wait_settlement_blocknum(pip_obj.node)
+            node_id_list = pip_obj.get_candidate_list_not_verifier()
+            if not node_id_list:
+                raise Exception('获取候选人失败')
+            client_candidate_obj = get_client_obj(node_id_list[0], client_list_obj)
+        else:
+            return client_candidate_obj.pip
+    result = pip_obj.submitVersion(pip_obj.node.node_id, str(time.time()), pip_obj.cfg.version5, 5,
+                          pip_obj.node.staking_address, transaction_cfg=pip_obj.cfg.transaction_cfg)
+    log.info('发起升级提案结果为{}'.format(result))
+    assert result.get('Code') == 0
+    return client_candidate_obj.pip
