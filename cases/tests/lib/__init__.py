ea3473c2c4c726ae69c1107f651e854bfdb916fa
shinnng <lewsion@gmail.com>
2019-11-01 16:44:03 +0800

    add delete cases

--- cases/tests/lib/__init__.py
+++
@@ -1,9 +0,0 @@
-from .economic import Economic
-from .config import PipConfig, StakingConfig, EconomicConfig
-from .pip import Pip
-from .delegate import Delegate
-from .genesis import Genesis
-from .staking import Staking
-from .duplicate_sign import DuplicateSign
-from .restricting import Restricting
-from .utils import *
--- cases/tests/ppos_2/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Author: wuyiqin
-@Date: 2019/10/30 15:42
-@Description:
-"""
