4e5ecc43a9f3383cb2af59d89b4a9f2bb5640389
awake <hietel366435@163.com>
2019-11-01 17:46:44 +0800

    add test_start_mini_node

--- cases/tests/chain/test_chain_deploy.py
+++ cases/tests/chain/test_chain_deploy.py
@@ -30,3 +30,16 @@ def test_start_all_node(global_test_env):
     log.info("部署{}节点".format(len(global_test_env.consensus_node_config_list)))
     global_test_env.deploy_all()
     global_test_env.check_block()
+
+
+@allure.title("启动共识节点2f+1开始出块")
+@pytest.mark.P0
+def test_start_mini_node(global_test_env):
+    """
+    用例id:51
+    测试启动共识节点达到最低共识节点数量时，开始出块
+    """
+    num = int(2 * global_test_env.max_byzantium + 1)
+    log.info("部署{}个节点".format(num))
+    global_test_env.deploy_nodes(global_test_env.consensus_node_list[0:num], genesis_file=global_test_env.cfg.genesis_tmp)
+    global_test_env.check_block(multiple=num, node_list=global_test_env.consensus_node_list[0:num])
