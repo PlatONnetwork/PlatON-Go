7a188f4353d373feb7fde9a05fd762de796dcd46
lipingD <zhouliping@juzix.net>
2019-11-05 20:37:34 +0800

    add testcase test_DE_CA_032

--- cases/tests/govern/test_declare_version.py
+++ cases/tests/govern/test_declare_version.py
@@ -894,3 +894,15 @@ class TestNoProposalCA:
 
         result = wrong_verison_declare(pip_obj, pip_obj.chain_version)
         assert_code(result, 302024)
+
+    def test_DE_CA_032(self, proposal_ca_pipobj_list, client_verifier_obj):
+        pip_obj = proposal_ca_pipobj_list[0]
+
+        result = replace_version_declare(pip_obj, pip_obj.cfg.PLATON_NEW_BIN0, pip_obj.cfg.version0)
+        assert_code(result, 0)
+
+        result = wrong_verisonsign_declare(pip_obj, client_verifier_obj.pip)
+        assert_code(result, 302024)
+
+        result = wrong_verison_declare(pip_obj)
+        assert_code(result, 302024)
