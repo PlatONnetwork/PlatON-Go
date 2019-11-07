56c303e95b11b9f6a9bbb06118b5ec524110d08a
lifapan <lifapan@juzix.io>
2019-11-07 13:43:46 +0800

    add case /tests/ppos/test_governance_penalty_parameters.py/test_PIP_PVF_007

--- cases/tests/ppos/test_governance_penalty_parameters.py
+++ cases/tests/ppos/test_governance_penalty_parameters.py
@@ -265,9 +265,9 @@ def test_PIP_PVF_006(new_genesis_env, client_con_list_obj, reset_environment):
     # view Parameter value before treatment
     slash_blocks1 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'MaxEvidenceAge')
     # create Parametric proposal
-    param_governance_verify(client_con_list_obj[0], 'Slashing', 'MaxEvidenceAge', '0', False)
+    param_governance_verify(client_con_list_obj[0], 'Slashing', 'MaxEvidenceAge', '1', False)
     # view Parameter value before treatment again
-    slash_blocks2 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'SlashBlocksReward')
+    slash_blocks2 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'MaxEvidenceAge')
     assert slash_blocks2 == slash_blocks1, "ErrMsg:Parameter value after treatment {}".format(slash_blocks2)
     report_address, _ = client_con_list_obj[0].economic.account.generate_account(client_con_list_obj[0].node.web3, client_con_list_obj[0].node.web3.toWei(1000, 'ether'))
     # Verify changed parameters
@@ -279,3 +279,43 @@ def test_PIP_PVF_006(new_genesis_env, client_con_list_obj, reset_environment):
     log.info("Report information: {}".format(report_information))
     result = client_con_list_obj[0].duplicatesign.reportDuplicateSign(1, report_information, report_address)
     assert_code(result, 0)
+
+
+@pytest.mark.P1
+def test_PIP_PVF_007(new_genesis_env, client_con_list_obj, reset_environment):
+    """
+    治理修改区块双签-证据有效期处于未生效期
+    :param new_genesis_env:
+    :param client_con_list_obj:
+    :param reset_environment:
+    :return:
+    """
+    # Change configuration parameters
+    genesis = from_dict(data_class=Genesis, data=new_genesis_env.genesis_config)
+    genesis.EconomicModel.Staking.UnStakeFreezeDuration = 3
+    genesis.EconomicModel.Slashing.MaxEvidenceAge = 2
+    new_file = new_genesis_env.cfg.env_tmp + "/genesis.json"
+    genesis.to_file(new_file)
+    new_genesis_env.deploy_all(new_file)
+    # view Parameter value before treatment
+    slash_blocks1 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'MaxEvidenceAge')
+    # create Parametric proposal
+    param_governance_verify_before_endblock(client_con_list_obj[0], 'Slashing', 'MaxEvidenceAge', '1')
+    # view Parameter value before treatment again
+    slash_blocks2 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'SlashBlocksReward')
+    assert slash_blocks2 == slash_blocks1, "ErrMsg:Parameter value after treatment {}".format(slash_blocks2)
+    report_address, _ = client_con_list_obj[0].economic.account.generate_account(client_con_list_obj[0].node.web3,
+                                                                                 client_con_list_obj[0].node.web3.toWei(
+                                                                                     1000, 'ether'))
+    # Verify changed parameters
+    current_block = client_con_list_obj[0].node.eth.blockNumber
+    log.info("Current block: {}".format(current_block))
+    # Report prepareblock signature
+    report_information = mock_duplicate_sign(1, client_con_list_obj[0].node.nodekey,
+                                             client_con_list_obj[0].node.blsprikey,
+                                             current_block)
+    log.info("Report information: {}".format(report_information))
+    result = client_con_list_obj[0].duplicatesign.reportDuplicateSign(1, report_information, report_address)
+    assert_code(result, 0)
+
+
