e050cfcfc617f399a56438ebe1e123074667cb87
lifapan <lifapan@juzix.io>
2019-11-07 17:25:38 +0800

    add case /tests/ppos/test_governance_penalty_parameters.py/test_PIP_PVF_018

--- cases/tests/ppos/test_governance_penalty_parameters.py
+++ cases/tests/ppos/test_governance_penalty_parameters.py
@@ -704,7 +704,7 @@ def test_PIP_PVF_016(client_con_list_obj, reset_environment):
     incentive_pool_account2 = client_con_list_obj[1].node.eth.getBalance(EconomicConfig.INCENTIVEPOOL_ADDRESS)
     # asster amount reward
     assert report_amount1 + proportion_reward - report_amount2 < client_con_list_obj[1].node.web3.toWei(1, 'ether'), "ErrMsg:report amount {}".format(report_amount2)
-    assert incentive_pool_account2 == incentive_pool_account1 + incentive_pool_reward, "ErrMsg:Incentive pool account {}".format(
+    assert incentive_pool_account2 == incentive_pool_account1 + incentive_pool_reward + (report_amount1 + proportion_reward - report_amount2), "ErrMsg:Incentive pool account {}".format(
         incentive_pool_account2)
 
 
@@ -723,10 +723,6 @@ def test_PIP_PVF_017(client_con_list_obj, reset_environment):
     report_address, _ = client_con_list_obj[0].economic.account.generate_account(client_con_list_obj[0].node.web3,
                                                                                  client_con_list_obj[0].node.web3.toWei(
                                                                                      1000, 'ether'))
-    # view report amount
-    report_amount1 = client_con_list_obj[0].node.eth.getBalance(report_address)
-    # view Incentive pool account
-    incentive_pool_account1 = client_con_list_obj[0].node.eth.getBalance(EconomicConfig.INCENTIVEPOOL_ADDRESS)
     # view Parameter value before treatment
     report_reward1 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'DuplicateSignReportReward')
     # create Parametric proposal
@@ -734,7 +730,10 @@ def test_PIP_PVF_017(client_con_list_obj, reset_environment):
     # view Parameter value after treatment
     report_reward2 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'DuplicateSignReportReward')
     assert report_reward1 == report_reward2, "ErrMsg:Parameter value after treatment {}".format(report_reward2)
-
+    # view report amount
+    report_amount1 = client_con_list_obj[0].node.eth.getBalance(report_address)
+    # view Incentive pool account
+    incentive_pool_account1 = client_con_list_obj[0].node.eth.getBalance(EconomicConfig.INCENTIVEPOOL_ADDRESS)
     # Verify changed parameters
     current_block = client_con_list_obj[0].node.eth.blockNumber
     log.info("Current block height: {}".format(current_block))
@@ -754,5 +753,54 @@ def test_PIP_PVF_017(client_con_list_obj, reset_environment):
     incentive_pool_account2 = client_con_list_obj[1].node.eth.getBalance(EconomicConfig.INCENTIVEPOOL_ADDRESS)
     # asster amount reward
     assert report_amount1 + proportion_reward - report_amount2 < client_con_list_obj[1].node.web3.toWei(1, 'ether'), "ErrMsg:report amount {}".format(report_amount2)
-    assert incentive_pool_account2 == incentive_pool_account1 + incentive_pool_reward, "ErrMsg:Incentive pool account {}".format(
+    assert incentive_pool_account2 == incentive_pool_account1 + incentive_pool_reward + (report_amount1 + proportion_reward - report_amount2), "ErrMsg:Incentive pool account {}".format(
+        incentive_pool_account2)
+
+
+@pytest.mark.P1
+def test_PIP_PVF_018(client_con_list_obj, reset_environment):
+    """
+    治理修改区块双签-举报奖励比例处于已生效期
+    :param client_con_list_obj:
+    :param reset_environment:
+    :return:
+    """
+    # view Pledge amount
+    candidate_info1 = client_con_list_obj[1].ppos.getCandidateInfo(client_con_list_obj[0].node.node_id)
+    pledge_amount1 = candidate_info1['Ret']['Released']
+    # create report account
+    report_address, _ = client_con_list_obj[0].economic.account.generate_account(client_con_list_obj[0].node.web3,
+                                                                                 client_con_list_obj[0].node.web3.toWei(
+                                                                                     1000, 'ether'))
+    # view Parameter value before treatment
+    report_reward1 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'DuplicateSignReportReward')
+    # create Parametric proposal
+    param_governance_verify(client_con_list_obj[0], 'Slashing', 'DuplicateSignReportReward', '60')
+    # view Parameter value after treatment
+    report_reward2 = get_governable_parameter_value(client_con_list_obj[0], 'Slashing', 'DuplicateSignReportReward')
+    assert report_reward1 == report_reward2, "ErrMsg:Parameter value after treatment {}".format(report_reward2)
+    # view report amount
+    report_amount1 = client_con_list_obj[0].node.eth.getBalance(report_address)
+    # view Incentive pool account
+    incentive_pool_account1 = client_con_list_obj[0].node.eth.getBalance(EconomicConfig.INCENTIVEPOOL_ADDRESS)
+    # Verify changed parameters
+    current_block = client_con_list_obj[0].node.eth.blockNumber
+    log.info("Current block height: {}".format(current_block))
+    # Report1 prepareblock signature
+    report_information = mock_duplicate_sign(1, client_con_list_obj[0].node.nodekey,
+                                             client_con_list_obj[0].node.blsprikey,
+                                             current_block)
+    log.info("Report information: {}".format(report_information))
+    result = client_con_list_obj[1].duplicatesign.reportDuplicateSign(1, report_information, report_address)
+    assert_code(result, 0)
+    # view Pledge amount after punishment
+    proportion_reward, incentive_pool_reward = client_con_list_obj[1].economic.get_report_reward(pledge_amount1)
+    log.info("Whistleblower benefits：{} Incentive pool income：{}".format(proportion_reward, incentive_pool_reward))
+    # view report amount
+    report_amount2 = client_con_list_obj[1].node.eth.getBalance(report_address)
+    # view Incentive pool account
+    incentive_pool_account2 = client_con_list_obj[1].node.eth.getBalance(EconomicConfig.INCENTIVEPOOL_ADDRESS)
+    # asster amount reward
+    assert report_amount1 + proportion_reward - report_amount2 < client_con_list_obj[1].node.web3.toWei(1, 'ether'), "ErrMsg:report amount {}".format(report_amount2)
+    assert incentive_pool_account2 == incentive_pool_account1 + incentive_pool_reward + (report_amount1 + proportion_reward - report_amount2), "ErrMsg:Incentive pool account {}".format(
         incentive_pool_account2)
