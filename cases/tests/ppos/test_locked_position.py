241771fe3e3c1335c3b243fbbc82cce12b870873
lifapan <lifapan@juzix.io>
2019-11-11 20:15:54 +0800

    add case /tests/ppos/test_locked_position.py/test_LS_CSV_012

--- cases/tests/ppos/test_locked_position.py
+++ cases/tests/ppos/test_locked_position.py
@@ -1994,7 +1994,7 @@ def test_LS_CSV_011(client_new_node_obj):
     address1, _ = economic.account.generate_account(node.web3, von_amount(economic.create_staking_limit, 3))
     # create Restricting Plan
     amount = von_amount(economic.create_staking_limit, 2)
-    plan = [{'Epoch': 1, 'Amount': amount}]
+    plan = [{'Epoch': 2, 'Amount': amount}]
     result = client.restricting.createRestrictingPlan(address1, plan, address1)
     assert_code(result, 0)
     # create staking
@@ -2005,13 +2005,50 @@ def test_LS_CSV_011(client_new_node_obj):
     # Additional pledge
     result = client.staking.increase_staking(1, address1)
     assert_code(result, 0)
+    balance = node.eth.getBalance(address1)
+    log.info("Account address: {} balance: {}".format(address1, balance))
     # Application for return of pledge
     result = client.staking.withdrew_staking(address1)
     assert_code(result, 0)
-    balance = node.eth.getBalance(address1)
-    log.info("Account address: {} balance: {}".format(address1, balance))
-    # Waiting for the end of the 2 settlement period
-    economic.wait_settlement_blocknum(node, 2)
     balance1 = node.eth.getBalance(address1)
     log.info("Account address: {} balance: {}".format(address1, balance1))
-    assert balance1 == balance + economic.create_staking_limit, "errMsg: Account address: {} balance: {}".format(address1, balance1)
+    # Waiting for the end of the 2 settlement period
+    economic.wait_settlement_blocknum(node, 2)
+    balance2 = node.eth.getBalance(address1)
+    log.info("Account address: {} balance: {}".format(address1, balance2))
+    assert balance2 - balance1 > economic.create_staking_limit, "errMsg: Account address: {} balance: {}".format(address1, balance2)
+
+
+@pytest.mark.P1
+def test_LS_CSV_012(client_new_node_obj):
+    """
+    锁仓账户退回质押金中，申请委托节点
+    :param client_new_node_obj:
+    :return:
+    """
+    client = client_new_node_obj
+    economic = client.economic
+    node = client.node
+    # create restricting plan staking
+    address1 = restricting_plan_verification_pledge(client, economic, node)
+    # create staking
+    result = client.staking.create_staking(1, address1, address1)
+    assert_code(result, 0)
+    # create account
+    address2, _ = economic.account.generate_account(node.web3, node.web3.toWei(1000, 'ether'))
+    # Waiting for the end of the settlement period
+    economic.wait_settlement_blocknum(node)
+    # Application for return of pledge
+    result = client.staking.withdrew_staking(address1)
+    assert_code(result, 0)
+    # create Restricting Plan
+    amount = von_amount(economic.delegate_limit, 10)
+    plan = [{'Epoch': 1, 'Amount': amount}]
+    result = client.restricting.createRestrictingPlan(address2, plan, address2)
+    assert_code(result, 0)
+    # Free amount Additional pledge
+    result = client.delegate.delegate(0, address2)
+    assert_code(result, 301107)
+    # Restricting amount Additional pledge
+    result = client.delegate.delegate(1, address2)
+    assert_code(result, 301107)
