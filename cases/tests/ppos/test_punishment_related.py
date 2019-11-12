39e846d062f726e3506735cb0b2c5bc50afcbe10
lifapan <lifapan@juzix.io>
2019-11-12 18:26:27 +0800

    add case /tests/ppos/test_governance_penalty_parameters.py/test_VP_PV_018

--- cases/tests/ppos/test_punishment_related.py
+++ cases/tests/ppos/test_punishment_related.py
@@ -404,3 +404,24 @@ def test_VP_PV_017(client_con_list_obj):
     result = client.duplicatesign.reportDuplicateSign(1, jsondata, report_address)
     assert_code(result, 303000)
 
+
+@pytest.mark.P1
+def test_VP_PV_018(client_con_list_obj):
+    """
+    举报双签-blsPubKey不一致举报双签
+    :param client_con_list_obj:
+    :return:
+    """
+    client = client_con_list_obj[0]
+    economic = client.economic
+    node = client.node
+    # create report address
+    report_address, _ = economic.account.generate_account(node.web3, node.web3.toWei(1000, 'ether'))
+    # Obtain information of report evidence
+    report_information, current_block = obtaining_evidence_information(economic, node)
+    # Modification of evidence
+    jsondata = update_param_by_dict(report_information, 'prepareA', 'validateNode', 'blsPubKey', client_con_list_obj[1].node.blspubkey)
+    log.info("Evidence information: {}".format(jsondata))
+    # Report verifier Duplicate Sign
+    result = client.duplicatesign.reportDuplicateSign(1, jsondata, report_address)
+    assert_code(result, 303000)
