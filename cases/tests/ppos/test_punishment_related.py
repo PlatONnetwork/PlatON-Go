ca3ce9cbb897af4f19e79b452e97621d5af64cce
lifapan <lifapan@juzix.io>
2019-11-12 19:10:42 +0800

    add case /tests/ppos/test_governance_penalty_parameters.py/test_VP_PV_024

--- cases/tests/ppos/test_punishment_related.py
+++ cases/tests/ppos/test_punishment_related.py
@@ -578,3 +578,29 @@ def test_VP_PV_023(client_con_list_obj):
     # Report verifier Duplicate Sign
     result = client.duplicatesign.reportDuplicateSign(1, jsondata, report_address)
     assert_code(result, 303000)
+
+
+@pytest.mark.P1
+def test_VP_PV_024(client_con_list_obj):
+    """
+    举报双签-伪造合法signature情况下伪造blockNumber
+    :param client_con_list_obj:
+    :return:
+    """
+    client = client_con_list_obj[0]
+    economic = client.economic
+    node = client.node
+    # create report address
+    report_address, _ = economic.account.generate_account(node.web3, node.web3.toWei(1000, 'ether'))
+    # Obtain information of report evidence
+    report_information, current_block = obtaining_evidence_information(economic, node)
+    # Obtain evidence of violation
+    report_information1 = mock_duplicate_sign(1, node.nodekey, node.blsprikey, current_block + 1)
+    log.info("Report information: {}".format(report_information))
+    # Modification of evidence
+    evidence_parameter = get_param_by_dict(report_information1, 'prepareB', 'signature')
+    jsondata = update_param_by_dict(report_information, 'prepareA', 'signature', None, evidence_parameter)
+    log.info("Evidence information: {}".format(jsondata))
+    # Report verifier Duplicate Sign
+    result = client.duplicatesign.reportDuplicateSign(1, jsondata, report_address)
+    assert_code(result, 303000)
