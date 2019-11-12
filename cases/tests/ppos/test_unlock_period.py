0a0e9326d98a540ab56f2a4dacce8efb3615ad2e
lifapan <lifapan@juzix.io>
2019-11-12 15:04:54 +0800

    add case /tests/ppos/test_locked_position.py/test_UP_FV_016

--- cases/tests/ppos/test_unlock_period.py
+++ cases/tests/ppos/test_unlock_period.py
@@ -34,7 +34,7 @@ def restricting_plan_validation_staking(client, economic, node):
     address1 = restricting_plan_validation_release(client, economic, node)
     # create staking
     staking_amount = economic.create_staking_limit
-    result = client.staking.create_staking(1, address1, address1, amount=staking_amount)
+    result = client.staking.create_staking(address1, address1, amount=staking_amount)
     assert_code(result, 0)
     return address1
 
@@ -606,7 +606,7 @@ def test_UP_FV_014(client_new_node_obj, reset_environment):
             info = candidate_info['Ret']
             staking_blocknum = info['StakingBlockNum']
             # withdrew delegate
-            result = client.delegate.withdrew_delegate(staking_blocknum, report_address, amount=delegate_amount)
+            result = client.delegate.withdrew_delegate(staking_blocknum, report_address)
             assert_code(result, 0)
             break
         else:
@@ -644,3 +644,37 @@ def test_UP_FV_015(client_new_node_obj):
     # view restricting plan
     restricting_info = client.ppos.getRestrictingInfo(address1)
     log.info("restricting plan informtion: {}".format(restricting_info))
+
+
+@pytest.mark.P2
+def test_UP_FV_016(client_new_node_obj):
+    """
+    自由资金质押，锁仓再增持
+    :param client_new_node_obj:
+    :return:
+    """
+    client = client_new_node_obj
+    economic = client.economic
+    node = client.node
+    # create account
+    amount1 = von_amount(economic.create_staking_limit, 2)
+    amount2 = von_amount(economic.create_staking_limit, 1)
+    address1, address2 = create_account_amount(client, amount1, amount2)
+    # create Restricting Plan
+    delegate_amount = von_amount(economic.add_staking_limit, 10)
+    plan = [{'Epoch': 3, 'Amount': delegate_amount}]
+    result = client.restricting.createRestrictingPlan(address1, plan, address1)
+    assert_code(result, 0)
+    # create staking
+    result = client.staking.create_staking(0, address1, address1)
+    assert_code(result, 0)
+    # Apply for additional pledge
+    result = client.staking.increase_staking(1, address1)
+    assert_code(result, 0)
+    # Waiting for the end of the settlement period
+    economic.wait_settlement_blocknum(node)
+    # Apply for additional pledge
+    result = client.staking.increase_staking(1, address1)
+    assert_code(result, 0)
+
+
