4924b4c8dec643d3ce48e1a4a2cbca30632261ba
lifapan <lifapan@juzix.io>
2019-11-13 10:41:52 +0800

    add case /tests/ppos/test_general_punishment.py/test_VP_GPFV_006

--- cases/tests/ppos/test_general_punishment.py
+++ cases/tests/ppos/test_general_punishment.py
@@ -24,7 +24,7 @@ def information_before_slash_blocks(client_obj, node):
 
 
 @pytest.mark.P0
-def test_VP_GPFV_003(client_new_node_obj_list):
+def test_VP_GPFV_003(client_new_node_obj_list, reset_environment):
     """
     低出快率最高处罚标准
     :param client_new_node_obj_list:
@@ -66,7 +66,7 @@ def test_VP_GPFV_003(client_new_node_obj_list):
 
 
 @pytest.mark.P0
-def test_VP_GPFV_004(client_new_node_obj_list):
+def test_VP_GPFV_004(client_new_node_obj_list, reset_environment):
     """
     锁仓质押被惩罚最高处罚标准
     :param client_new_node_obj_list:
@@ -113,7 +113,7 @@ def test_VP_GPFV_004(client_new_node_obj_list):
 
 
 @pytest.mark.P2
-def test_VP_GPFV_005(client_new_node_obj_list):
+def test_VP_GPFV_005(client_new_node_obj_list, reset_environment):
     """
     锁仓增持/委托后被惩罚
     :param client_new_node_obj_list:
@@ -130,13 +130,13 @@ def test_VP_GPFV_005(client_new_node_obj_list):
     # create account
     address2, _ = economic.account.generate_account(node.web3, von_amount(economic.create_staking_limit, 1))
     # create Restricting Plan
-    amount = von_amount(economic.create_staking_limit, 2)
-    plan = [{'Epoch': 1, 'Amount': amount}]
+    amount1 = von_amount(economic.create_staking_limit, 2)
+    plan = [{'Epoch': 1, 'Amount': amount1}]
     result = client1.restricting.createRestrictingPlan(address1, plan, address1)
     assert_code(result, 0)
     # create Restricting Plan
-    amount = von_amount(economic.create_staking_limit, 1)
-    plan = [{'Epoch': 1, 'Amount': amount}]
+    amount2 = von_amount(economic.delegate_limit, 100)
+    plan = [{'Epoch': 1, 'Amount': amount2}]
     result = client1.restricting.createRestrictingPlan(address2, plan, address2)
     assert_code(result, 0)
     # create staking
@@ -170,3 +170,55 @@ def test_VP_GPFV_005(client_new_node_obj_list):
             pledge_amount2)
     else:
         assert pledge_amount2 == 0, "ErrMsg:Consensus Amount of pledge {}".format(pledge_amount2)
+
+
+@pytest.mark.P2
+def test_VP_GPFV_006(client_new_node_obj_list, reset_environment):
+    """
+    自由金额增持/委托后被惩罚
+    :param client_new_node_obj_list:
+    :return:
+    """
+    client1 = client_new_node_obj_list[0]
+    log.info("Current connection node1: {}".format(client1))
+    client2 = client_new_node_obj_list[1]
+    log.info("Current connection node2: {}".format(client2))
+    economic = client1.economic
+    node = client1.node
+    # create account
+    address1, _ = economic.account.generate_account(node.web3, von_amount(economic.create_staking_limit, 3))
+    # create account
+    address2, _ = economic.account.generate_account(node.web3, von_amount(economic.create_staking_limit, 1))
+    # create staking
+    result = client1.staking.create_staking(0, address1, address1)
+    assert_code(result, 0)
+    # increase staking
+    result = client1.staking.increase_staking(0, address1)
+    assert_code(result, 0)
+    # Additional pledge
+    result = client1.delegate.delegate(0, address2, amount=von_amount(economic.delegate_limit, 100))
+    assert_code(result, 0)
+    # Wait for the settlement round to end
+    economic.wait_settlement_blocknum(node)
+    # get pledge amount1 and block reward
+    pledge_amount1, block_reward, slash_blocks = information_before_slash_blocks(client1, node)
+    log.info("Current block height: {}".format(client1.node.eth.blockNumber))
+    # stop node
+    client1.node.stop()
+    # Waiting for a settlement round
+    client2.economic.wait_consensus_blocknum(client2.node, 2)
+    log.info("Current block height: {}".format(client2.node.eth.blockNumber))
+    # view verifier list
+    verifier_list = client2.ppos.getVerifierList()
+    log.info("verifier_list: {}".format(verifier_list))
+    candidate_info = client2.ppos.getCandidateInfo(client1.node.node_id)
+    log.info("Pledge node information： {}".format(candidate_info))
+    pledge_amount2 = candidate_info['Ret']['Released']
+    punishment_amonut = int(Decimal(str(block_reward)) * Decimal(str(slash_blocks)))
+    if punishment_amonut < pledge_amount1:
+        assert pledge_amount2 == pledge_amount1 - punishment_amonut, "ErrMsg:Consensus Amount of pledge {}".format(
+            pledge_amount2)
+    else:
+        assert pledge_amount2 == 0, "ErrMsg:Consensus Amount of pledge {}".format(pledge_amount2)
+
+
