776fa07102be2f85f92111c6d3a0452538935364
bglmmz <bglmmz@hotmail.com>
2019-11-18 17:37:06 +0800

    modify the govern parameter's (maxBlockGasLimit) description.

--- x/gov/gov_params.go
+++ x/gov/gov_params.go
@@ -219,7 +219,7 @@ func initParam() []*GovernParam {
 		About Block module
 		*/
 		{
-			ParamItem:  &ParamItem{ModuleBlock, KeyMaxBlockGasLimit, fmt.Sprintf("maximum gas limit per block, range：(%d, %s)", int(params.TxGas*200), xcom.PositiveInfinity)},
+			ParamItem:  &ParamItem{ModuleBlock, KeyMaxBlockGasLimit, fmt.Sprintf("maximum gas limit per block, range：(%d, %d)", int(params.GenesisGasLimit), int(params.MaxGasCeil))},
 			ParamValue: &ParamValue{"", strconv.Itoa(int(params.DefaultMinerGasCeil)), 0},
 			ParamVerifier: func(blockNumber uint64, blockHash common.Hash, value string) error {
 
