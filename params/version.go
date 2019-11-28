7b74b1d21483c0e61bb10bc6775188c4d4b54edd
gavin <meiyan532484710@163.com>
2019-11-28 16:50:16 +0800

    Signed-off-by: gavin <meiyan532484710@163.com>

--- params/version.go
+++ params/version.go
@@ -21,6 +21,7 @@ import (
 )
 
 const (
+
 	//These versions are meaning the current code version.
 	VersionMajor = 0          // Major version component of the current release
 	VersionMinor = 7          // Minor version component of the current release
