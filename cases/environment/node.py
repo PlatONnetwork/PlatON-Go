fedd4c4f2e2fecd4ec02d726b69af9e09650c39a
awake <hietel366435@163.com>
2019-12-05 17:30:12 +0800

    add --db.nogc

--- cases/environment/node.py
+++ cases/environment/node.py
@@ -387,8 +387,8 @@ class Node:
             if self.fail_point:
                 go_fail_point = " GO_FAILPOINTS='{}' ".format(self.fail_point)
             cmd = "{} --identity platon --datadir".format(self.remote_bin_file)
-            cmd = cmd + " --db.nogc"
             cmd = cmd + " {} --port ".format(self.remote_data_dir) + self.p2p_port
+            cmd = cmd + " --db.nogc"
             cmd = cmd + " --gcmode archive --nodekey " + self.remote_nodekey_file
             cmd = cmd + " --cbft.blskey " + self.remote_blskey_file
             cmd = cmd + " --config " + self.remote_config_file
