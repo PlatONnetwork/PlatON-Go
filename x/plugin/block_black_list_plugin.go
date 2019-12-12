cc05e1a014ce1338fb520d8ecd7c3b71d2ec3853
clearly <910372762@qq.com>
2019-12-12 11:35:14 +0800

    add rool back

---
+++ x/plugin/block_black_list_plugin.go
@@ -0,0 +1,49 @@
+package plugin
+
+import (
+	"fmt"
+
+	"github.com/PlatONnetwork/PlatON-Go/params"
+
+	"github.com/PlatONnetwork/PlatON-Go/common"
+	"github.com/PlatONnetwork/PlatON-Go/core/types"
+	"github.com/PlatONnetwork/PlatON-Go/p2p/discover"
+	"github.com/PlatONnetwork/PlatON-Go/x/xcom"
+)
+
+const (
+	FORKHASH    = "0xe34b4b2cfc9df86b0b0ae331d7ffa09e2172bb8ba56b07db8f77b9ba7e8f899e"
+	FORKNUM     = 695
+	FORKVERSION = uint32(params.VersionMajor<<16|params.VersionMinor<<8|params.VersionPatch) + 1
+)
+
+var BlockBlackListERROR = fmt.Errorf("the block is exist in BlackList,hash:%v", FORKHASH)
+
+type BlockBlackListPlugin struct {
+	list []common.Hash
+}
+
+func NewBlockBlackListPlugin() *BlockBlackListPlugin {
+	blackhash := common.HexToHash(FORKHASH)
+	bl := new(BlockBlackListPlugin)
+	bl.list = make([]common.Hash, 0)
+	bl.list = append(bl.list, blackhash)
+	return bl
+}
+
+func (b *BlockBlackListPlugin) BeginBlock(blockHash common.Hash, header *types.Header, state xcom.StateDB) error {
+	for _, value := range b.list {
+		if blockHash == value {
+			return BlockBlackListERROR
+		}
+	}
+	return nil
+}
+
+func (b *BlockBlackListPlugin) EndBlock(blockHash common.Hash, header *types.Header, state xcom.StateDB) error {
+	return nil
+}
+
+func (b *BlockBlackListPlugin) Confirmed(nodeId discover.NodeID, block *types.Block) error {
+	return nil
+}
--- x/plugin/reward_plugin.go
+++ x/plugin/reward_plugin.go
@@ -48,7 +48,7 @@ const (
 	AfterFoundationYearFoundRewardRate     = 50
 	IncreaseIssue                          = 40
 	RewardPoolIncreaseRate                 = 80 // 80% of fixed-issued tokens are allocated to reward pool each year
-	VERSIONTrueActive                      = 510000
+	VERSIONTrueActive                      = 500
 )
 
 var (
