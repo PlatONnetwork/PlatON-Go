0f18ecda6794f07ae0a62db6794e2dc44d110b29
luo-dahui <934803813@qq.com>
2019-12-19 19:53:26 +0800

    modify ChainId

--- params/config.go
+++ params/config.go
@@ -101,7 +101,7 @@ var (
 
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:     big.NewInt(97),
+		ChainID:     big.NewInt(95),
 		EmptyBlock:  "on",
 		EIP155Block: big.NewInt(1),
 		Cbft: &CbftConfig{
