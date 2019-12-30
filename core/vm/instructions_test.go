246c2d6efff439649f61db776e6905350ad00a7a
zjsunzone <zjsunzone@gmail.com>
2019-12-30 20:02:48 +0800

    add TestOpSlt for unit test

--- core/vm/instructions_test.go
+++ core/vm/instructions_test.go
@@ -333,6 +333,20 @@ func TestOpGt(t *testing.T) {
 	testTwoOperandOp(t, tests, opGt)
 }
 
+func TestOpSlt(t *testing.T) {
+	v := func(v int64) string {
+		b := new(big.Int).SetInt64(v)
+		return common.Bytes2Hex(b.Bytes())
+	}
+	tests := []twoOperandTest{
+		{v(2), v(1), v(1)},
+		{v(0), v(0), v(0)},
+		{v(1), v(2), v(0)},
+		{v(-2), v(0), v(1)},
+	}
+	testTwoOperandOp(t, tests, opSlt)
+}
+
 func opBenchmark(bench *testing.B, op func(pc *uint64, interpreter *EVMInterpreter, contract *Contract, memory *Memory, stack *Stack) ([]byte, error), args ...string) {
 	var (
 		env            = NewEVM(Context{}, nil, params.TestChainConfig, Config{})
