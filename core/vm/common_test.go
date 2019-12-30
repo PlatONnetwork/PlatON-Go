889f62d8d089cae47b53972daab71525a280e485
zjsunzone <zjsunzone@gmail.com>
2019-12-30 13:49:32 +0800

    add test of TestCalcMemSize for common_test.go

--- core/vm/common_test.go
+++ core/vm/common_test.go
@@ -15,3 +15,32 @@
 // along with the PlatON-Go library. If not, see <http://www.gnu.org/licenses/>.
 
 package vm
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func buildBigInt(v int64) *big.Int {
+	return new(big.Int).SetInt64(v)
+}
+
+func TestCalcMemSize(t *testing.T) {
+	testCases := []struct {
+		off  *big.Int
+		l    *big.Int
+		want *big.Int
+	}{
+		{off: buildBigInt(10), l: buildBigInt(10), want: buildBigInt(20)},
+		{off: buildBigInt(0), l: buildBigInt(0), want: buildBigInt(0)},
+		{off: buildBigInt(1), l: buildBigInt(-1), want: buildBigInt(0)},
+		{off: buildBigInt(10), l: buildBigInt(-10), want: buildBigInt(0)},
+		{off: buildBigInt(3), l: buildBigInt(4), want: buildBigInt(7)},
+	}
+	for _, v := range testCases {
+		res := calcMemSize(v.off, v.l)
+		assert.Equal(t, v.want.Uint64(), res.Uint64())
+	}
+}
