1fe0b352e027c7e29a2f759b2d5f537f69798a09
zjsunzone <zjsunzone@gmail.com>
2019-12-30 19:38:45 +0800

    add TestOpExp for unit test

--- core/vm/instructions_test.go
+++ core/vm/instructions_test.go
@@ -277,6 +277,20 @@ func TestOpSmod(t *testing.T) {
 	testTwoOperandOp(t, tests, opSmod)
 }
 
+func TestOpExp(t *testing.T) {
+	v := func(v int64) string {
+		b := new(big.Int).SetInt64(v)
+		return common.Bytes2Hex(b.Bytes())
+	}
+	tests := []twoOperandTest{
+		{v(2), v(10), v(100)},
+		{v(10), v(2), v(1024)},
+		{v(100), v(1), v(1)},
+		{v(2), v(0), v(0)},
+	}
+	testTwoOperandOp(t, tests, opExp)
+}
+
 func opBenchmark(bench *testing.B, op func(pc *uint64, interpreter *EVMInterpreter, contract *Contract, memory *Memory, stack *Stack) ([]byte, error), args ...string) {
 	var (
 		env            = NewEVM(Context{}, nil, params.TestChainConfig, Config{})
