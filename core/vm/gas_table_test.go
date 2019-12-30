1a9e43297874a01b0939c3624eb7a0a0431aa7ca
zjsunzone <zjsunzone@gmail.com>
2019-12-30 11:14:37 +0800

    add test of TestGasRevert in gas_table_test.go

--- core/vm/gas_table_test.go
+++ core/vm/gas_table_test.go
@@ -334,3 +334,21 @@ func TestGasReturn(t *testing.T) {
 		t.Error("not expected error")
 	}
 }
+
+func TestGasRevert(t *testing.T) {
+	gasTable := params.GasTableConstantinople
+	stack := newstack()
+	stateDB, _, _ := newChainState()
+
+	stack.push(new(big.Int).SetUint64(100))
+	stack.push(new(big.Int).SetUint64(100))
+	stack.push(new(big.Int).SetUint64(100))
+	stack.push(new(big.Int).SetUint64(100))
+	gas, err := gasRevert(gasTable, &EVM{StateDB: stateDB}, &Contract{Gas: 1000}, stack, NewMemory(), 1024)
+	if gas != 98 {
+		t.Errorf("Expected: 98, got %d", gas)
+	}
+	if err != nil {
+		t.Error("not expected error")
+	}
+}
