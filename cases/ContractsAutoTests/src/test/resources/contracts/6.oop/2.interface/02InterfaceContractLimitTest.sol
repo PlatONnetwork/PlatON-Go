dc188e2a2546e00b384f12e3d4f7545ee3ff184b
qudong <1013650096@qq.com>
2019-12-31 19:58:27 +0800

    提交接口及继承合约

--- cases/ContractsAutoTests/src/test/resources/contracts/6.oop/2.interface/02InterfaceContractLimitTest.sol
+++ cases/ContractsAutoTests/src/test/resources/contracts/6.oop/2.interface/02InterfaceContractLimitTest.sol
@@ -1,45 +1,34 @@
 pragma solidity 0.5.13;
 
-
 /**
- *
- * 
  * @author qudong
  * @dev 2019/12/23
  * 
- *测试合约接口功能点
- *
- *接口(interface)简述：接口类似于抽象合约，但是其不能实现任何函数。
- *
- *
+ * 测试合约接口功能点
+ * 接口(interface)简述：接口类似于抽象合约，但是其不能实现任何函数。
  *-----------------  测试点   ------------------------------
- *1、接口限制点测试(5.0以后版本)
- *1)、 可以声明结构体
- *2)、 可以声明枚举
- *
- *2、接口被继承情况测试
- *1)、 普通合约继承单/多个接口
- *2)、 接口无法继承其他合约或接口
+ * 1、接口限制点测试(5.0以后版本)
+ *    1)、 可以声明结构体
+ *    2)、 可以声明枚举
  */
-
-
-
 interface InterfaceContractEnum {
 
    /**
     * 验证：1、接口可以声明枚举
     *-----------------------------
-    *验证结果：*/
-
+    * 验证结果：接口中可以正常定义枚举
+    */
    enum FreshJuiceSize {
         SMALL,
         MEDIUM, 
         LARGE 
    }
+
+   function getDefaultChoice() external returns (uint);
 }
 
 
-contract  InterfaceContractTestEnum is InterfaceContractEnum {
+contract  InterfaceContractEnumTest is InterfaceContractEnum {
 
     FreshJuiceSize choice;
      //默认饮料类型
@@ -53,40 +42,46 @@ contract  InterfaceContractTestEnum is InterfaceContractEnum {
       return choice;
    }
    
-   function getDefaultChoice() public view returns (uint) {
+   function getDefaultChoice() external returns (uint) {
       return uint(defaultChoice);
    }
 }
 
 
-/*interface InterfaceContractStruct {
+interface InterfaceContractStruct {
 
-   *//**
+   /**
     * 验证：2、接口可以声明结构体
     *-----------------------------
-    *验证结果：
-    *//*
+    * 验证结果：接口中可以定义结构体
+    */
    struct Book {
         int bookID;
         string title;
         string describe;
    }
-}*/
 
-/*contract  InterfaceContractTestStruct is InterfaceContractStruct {
+   function getDefaultBookID() external returns (int);
+}
+
+contract  InterfaceContractStructTest is InterfaceContractStruct {
 
     Book book;
-
     function setBook() public {
-
          book = Book(1,"PlatON","PlatON Describe");
     }
 
     function getBookID() public view  returns (int) {
-         
           return book.bookID;
     }
-}*/
+
+    function getDefaultBookID() external returns (int) {
+       if(book.bookID == 0){
+           return 0;
+       }
+       return book.bookID;
+    }
+}
 
 
 
