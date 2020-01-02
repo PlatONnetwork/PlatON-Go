e4e75f7b238e97bba7ec7389f89c5fb7146a08bf
qudong <1013650096@qq.com>
2020-01-03 00:54:34 +0800

    提交基本类型脚本

--- cases/ContractsAutoTests/src/test/java/data_type/BasicDataType/BasicDataTypeContract02_Test.java
+++ cases/ContractsAutoTests/src/test/java/data_type/BasicDataType/BasicDataTypeContract02_Test.java
@@ -44,20 +44,56 @@ public class BasicDataTypeContract02_Test extends ContractPrepareTest {
         }
 
         //调用合约方法
-        //1、验证：地址类型(Address)--当前合约的余额
+
+        //1、验证：address类型，转账给指定地址
         try {
-            BigInteger expectValue = new BigInteger("0");
-            //执行getCurrentBalance()
-            BigInteger actualValue = basicDataTypeContract2.getCurrentBalance().send();
-            collector.logStepPass("BasicDataTypeContract 执行getCurrentBalance() successfully actualValue:" + actualValue);
-            //collector.assertEqual(actualValue,expectValue, "checkout  execute success.");
+            BigInteger moneyValue1 = new BigInteger("100");
+            BigInteger moneyValue2 = new BigInteger("100");
+            int expectValue = 200;
+
+            String myContractAddress = "0x8a9B36694F1eeeb500c84A19bB34137B05162EC9";
+
+            //1)、查询地址余额
+            BigInteger contractBalance = basicDataTypeContract2.getBalance(myContractAddress).send();
+            collector.logStepPass("BasicDataTypeContract  执行 1)、查询地址余额 getBalance() successfully.contractBalance:" + contractBalance);
+
+            //2）、查询当前转账人账户余额getBalance()
+            BigInteger currentTransferUserBalance = basicDataTypeContract2.getBalance(walletAddress).send();
+            collector.logStepPass("BasicDataTypeContract 执行 2)、转账人账户余额getBalance() successfully.currentTransferUserBalance: " + currentTransferUserBalance);
+
+            //3)、给当前地址第一次转账
+            TransactionReceipt transactionReceipt = basicDataTypeContract2.goTransfer(myContractAddress,moneyValue1).send();
+            collector.logStepPass("BasicDataTypeContract 执行 3)、给当前地址转账  goTransfer() successfully hash:" + transactionReceipt.getTransactionHash());
+
+            //4)、给当前地址第二次转账
+            TransactionReceipt transactionReceipt1 = basicDataTypeContract2.goSend(myContractAddress,moneyValue2).send();
+            collector.logStepPass("BasicDataTypeContract 执行 4)、给当前地址转账  goSend() successfully hash:" + transactionReceipt1.getTransactionHash());
+
+            //5)、查询转账完毕，地址余额
+            BigInteger currentContractBalance = basicDataTypeContract2.getBalance(myContractAddress).send();
+            collector.logStepPass("BasicDataTypeContract 执行 5)、查询转账完毕，地址余额 getBalance() successfully currentContractBalance:" + currentContractBalance);
+            int actualValue = currentContractBalance.intValue() - contractBalance.intValue();
+
+            collector.assertEqual(actualValue,expectValue, "checkout execute success.");
+
         } catch (Exception e) {
             collector.logStepFail("BasicDataTypeContract Calling Method fail.", e.toString());
             e.printStackTrace();
         }
 
+        //2、验证：address类型，获取当前合约余额
+        try {
+            BigInteger expectValue = new BigInteger("0");
+            //执行getCurrentBalance()
+            BigInteger actualValue = basicDataTypeContract2.getCurrentBalance().send();
+            collector.logStepPass("BasicDataTypeContract 执行查询当前合约中余额 getCurrentBalance() successfully contractBalance:" + actualValue);
+            collector.assertEqual(actualValue,expectValue, "checkout  execute success.");
+        } catch (Exception e) {
+            collector.logStepFail("BasicDataTypeContract Calling Method fail.", e.toString());
+            e.printStackTrace();
+        }
 
-        //1、验证：字符串字面常量，进行赋值
+        //3、验证：字符串字面常量，进行赋值
         try {
             String expectValue = "hello";
             //执行getCurrentBalance()
@@ -69,7 +105,7 @@ public class BasicDataTypeContract02_Test extends ContractPrepareTest {
             e.printStackTrace();
         }
 
-        //1、验证：字符串是特殊的动态长度字节数组，获取长度
+        //4、验证：字符串是特殊的动态长度字节数组，获取长度
         try {
             BigInteger expectValue = new BigInteger("5");
             //执行getCurrentBalance()
@@ -81,7 +117,7 @@ public class BasicDataTypeContract02_Test extends ContractPrepareTest {
             e.printStackTrace();
         }
 
-        //1、验证：字符串是特殊的动态长度字节数组，进行转换
+        //5、验证：字符串是特殊的动态长度字节数组，进行转换
         try {
             String expectValue = "aello";
             //赋值执行setStr1()
@@ -94,7 +130,7 @@ public class BasicDataTypeContract02_Test extends ContractPrepareTest {
         }
 
 
-        //1、验证：十六进制字面常量，定义赋值取值
+        //6、验证：十六进制字面常量，定义赋值取值
         try {
             //执行getHexLiteral()
             String expectValue = "c8";
@@ -130,7 +166,7 @@ public class BasicDataTypeContract02_Test extends ContractPrepareTest {
             e.printStackTrace();
         }
 
-        //验证：枚举类型
+        //7、验证：枚举类型
         try {
             BigInteger expectValue1 = new BigInteger("1");
             BigInteger expectValue2 = new BigInteger("3");
@@ -149,7 +185,27 @@ public class BasicDataTypeContract02_Test extends ContractPrepareTest {
             e.printStackTrace();
         }
 
+        //8、验证：有理数常量
+        try {
+            BigInteger expectValue1 = new BigInteger("3");
+            BigInteger expectValue2 = new BigInteger("20000000000");
+            BigInteger expectValue3 = new BigInteger("3000000000000000");
+            //执行getValue()
+            Tuple3<BigInteger, BigInteger, BigInteger> tuple3 = basicDataTypeContract2.getValue().send();
+            BigInteger actualValue1 = tuple3.getValue1();
+            BigInteger actualValue2 = tuple3.getValue2();
+            BigInteger actualValue3 = tuple3.getValue3();
 
+            collector.logStepPass("BasicDataTypeContract  验证有理数常量执行 getValue() successfully actualValue1:" + actualValue1 +
+                                  ",  actualValue2: " + actualValue2 + ",  actualValue3:" + actualValue3) ;
+            collector.assertEqual(actualValue1,expectValue1, "checkout  execute success.");
+            collector.assertEqual(actualValue2,expectValue2, "checkout  execute success.");
+            collector.assertEqual(actualValue3,expectValue3, "checkout  execute success.");
+
+        } catch (Exception e) {
+            collector.logStepFail("BasicDataTypeContract Calling Method fail.", e.toString());
+            e.printStackTrace();
+        }
 
     }
 }
