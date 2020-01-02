a1e66f1d41b7cca4d26423e7b1d9c38058dd6521
zjsunzone <zjsunzone@gmail.com>
2020-01-02 15:32:40 +0800

    update TestGasDelegateCall for unit test of gas_table_test.go

--- core/vm/gas_table_test.go
+++ core/vm/gas_table_test.go
@@ -577,10 +577,10 @@ func TestGasDelegateCall(t *testing.T) {
 		expected   uint64
 		isNil      bool
 	}{
-		{elements: []*big.Int{uint2BigInt(100), uint2BigInt(100), uint2BigInt(100), uint2BigInt(100)}, expected: 9800, memorySize: 0, isNil: true},
-		{elements: []*big.Int{uint2BigInt(100), uint2BigInt(100), uint2BigInt(100), uint2BigInt(100)}, expected: 9898, memorySize: 1024, isNil: true},
+		{elements: []*big.Int{uint2BigInt(100), uint2BigInt(100), uint2BigInt(100), uint2BigInt(100)}, expected: 800, memorySize: 0, isNil: true},
+		{elements: []*big.Int{uint2BigInt(100), uint2BigInt(100), uint2BigInt(100), uint2BigInt(100)}, expected: 898, memorySize: 1024, isNil: true},
 		{elements: []*big.Int{uint2BigInt(100), uint2BigInt(100), uint2BigInt(100), uint2BigInt(100)}, expected: 0, memorySize: 0xffffffffe1, isNil: false},
-		{elements: []*big.Int{uint2BigInt(2), overUint, uint2BigInt(3), uint2BigInt(4)}, expected: 9802, memorySize: 1024, isNil: false},
+		{elements: []*big.Int{uint2BigInt(2), overUint, uint2BigInt(3), uint2BigInt(4)}, expected: 802, memorySize: 1024, isNil: false},
 	}
 	stateDB, _, _ := newChainState()
 	for i, v := range testCases {
@@ -594,21 +594,6 @@ func TestGasDelegateCall(t *testing.T) {
 		}
 	}
 
-	/*gasTable := params.GasTableConstantinople
-	stack := newstack()
-	stateDB, _, _ := newChainState()
-
-	stack.push(new(big.Int).SetUint64(100))
-	stack.push(new(big.Int).SetUint64(100))
-	stack.push(new(big.Int).SetUint64(100))
-	stack.push(new(big.Int).SetUint64(100))
-	gas, err := gasDelegateCall(gasTable, &EVM{StateDB: stateDB}, &Contract{Gas: 1000, self: &MockAddressRef{}}, stack, NewMemory(), 1024)
-	if gas != 898 {
-		t.Errorf("Expected: 898, got %d", gas)
-	}
-	if err != nil {
-		t.Error("not expected error")
-	}*/
 }
 
 func TestGasStaticCall(t *testing.T) {
