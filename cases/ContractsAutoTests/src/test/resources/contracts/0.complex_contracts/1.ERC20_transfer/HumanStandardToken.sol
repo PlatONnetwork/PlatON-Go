f755bc1e3678673e4642819a18809be8a1cc7e93
xiaoqichuan@juzix.net <xiaoqichuan@juzix.net>
2020-01-02 18:33:32 +0800

    fix compile error of path

--- cases/ContractsAutoTests/src/test/resources/contracts/0.complex_contracts/1.ERC20_transfer/HumanStandardToken.sol
+++ cases/ContractsAutoTests/src/test/resources/contracts/0.complex_contracts/1.ERC20_transfer/HumanStandardToken.sol
@@ -13,7 +13,7 @@ Machine-based, rapid creation of many tokens would not necessarily need these ex
 
 .*/
 
-import "StandardToken.sol";
+import "./StandardToken.sol";
 
 contract HumanStandardToken is StandardToken {
 
