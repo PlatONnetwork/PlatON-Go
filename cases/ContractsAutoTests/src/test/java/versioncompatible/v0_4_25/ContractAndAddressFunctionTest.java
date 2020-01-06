73bd5bee9efd350d48dce4455ac707d65ab16ee1
wangzxjiejie <18720061456@139.com>
2020-01-06 17:56:59 +0800

    合约和地址异常日志完善

--- cases/ContractsAutoTests/src/test/java/versioncompatible/v0_4_25/ContractAndAddressFunctionTest.java
+++ cases/ContractsAutoTests/src/test/java/versioncompatible/v0_4_25/ContractAndAddressFunctionTest.java
@@ -44,10 +44,11 @@ public class ContractAndAddressFunctionTest extends ContractPrepareTest {
 
             Transfer transfer = new Transfer(web3j, transactionManager);
             TransactionReceipt receipt = transfer.sendFunds(contractAddress, BigDecimal.valueOf(1.00), Convert.Unit.LAT, GAS_PRICE, GAS_LIMIT).send();
-            if (StringUtils.equals(receipt.getStatus(), "0x0")) {
+            if (StringUtils.equals(receipt.getStatus(), "0x1")) {
                 PlatonGetBalance balance = web3j.platonGetBalance(contractAddress, DefaultBlockParameterName.LATEST).send();
-                collector.logStepPass("transfer contract account is successfully.contractAddress:" + contractAddress + ", amount:" + balance.getResult());
-
+                collector.assertEqual(balance.getBalance(), new BigInteger("1000000000000000000"), "checkout to contract account transfer");
+            }else {
+                collector.logStepFail("transfer contract account is failure.contractAddress:" , contractAddress);
             }
             Tuple3<String, BigInteger, BigInteger> result = contractAndAddress.addressCheck().send();
             Tuple3<String, BigInteger, BigInteger> expert = new Tuple3<>(receipt.getFrom(), new BigInteger("999999999999999980"), new BigInteger("20"));
