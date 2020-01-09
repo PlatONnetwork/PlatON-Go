ef8a3708174cef022c8a2fdf0742bae01840d96f
hudenian <hudenian@163.com>
2020-01-09 16:50:06 +0800

    结算复杂合约代码优化

--- cases/ContractsAutoTests/src/test/java/csdccontracts/NewSecPledgeApplyManagerTest.java
+++ cases/ContractsAutoTests/src/test/java/csdccontracts/NewSecPledgeApplyManagerTest.java
@@ -4,7 +4,6 @@ import beforetest.ContractPrepareTest;
 import network.platon.autotest.junit.annotations.DataSource;
 import network.platon.autotest.junit.enums.DataSourceType;
 import network.platon.contracts.NewSecPledgeApplyManager;
-import network.platon.contracts.OrderDao;
 import org.junit.Before;
 import org.junit.Test;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -20,12 +19,11 @@ import java.util.Date;
  */
 public class NewSecPledgeApplyManagerTest extends ContractPrepareTest {
 
-//    //需要进行double的值
-//    private String doubleValue = "10";
-//
+
 //    //模拟简单的业务数据
     private String secApply = "2-businessNo1-bizId1-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24";
 
+    //对应 secApply以“-”分隔的第一个值
     private String bizId = "2";
 
     @Before
@@ -53,20 +51,28 @@ public class NewSecPledgeApplyManagerTest extends ContractPrepareTest {
             collector.logStepPass("NewSecPledgeApplyManager add successfully hash:" + tx.getTransactionHash());
 
             //查询质押申请信息
-           String business_id =  newSecPledgeApplyManager.select_SecPledgeApply_byId(bizId).send().toString();
-           collector.logStepPass("bizId:"+bizId+"对应的business_no为："+business_id);
+           String businessNo =  newSecPledgeApplyManager.select_SecPledgeApply_byId(bizId).send().toString();
+           collector.logStepPass("bizId:"+bizId+"对应的business_no为："+businessNo);
+            //对应 secApply以“-”分隔的第二个值
+           collector.assertEqual("businessNo1",businessNo);
 
             //查询交易用户
             String tradeUser =  newSecPledgeApplyManager.select_tradeUser_byId(bizId).send().toString();
             collector.logStepPass("bizId:"+bizId+"对应的tradeUser为："+tradeUser);
+            //对应 secApply以“-”分隔的第三个值
+            collector.assertEqual("bizId1",tradeUser);
 
             //查询操作用户
             String tradeOperator =  newSecPledgeApplyManager.select_tradeOperator_bytId(bizId).send().toString();
             collector.logStepPass("bizId:"+bizId+"对应的tradeOperator为："+tradeOperator);
+            //对应 secApply以“-”分隔的第七个值
+            collector.assertEqual("7",tradeOperator);
 
-            //查询SecPledgeApply
-            String secPledgeApply =  newSecPledgeApplyManager.select_SecPledgeApply_byId(bizId).send().toString();
-            collector.logStepPass("bizId:"+bizId+"对应的secPledgeApply为："+secPledgeApply);
+            //查询pledgeSecurity
+            String pledgeSecurity =  newSecPledgeApplyManager.select_pledgeSecurity_bytId(bizId).send().toString();
+            collector.logStepPass("bizId:"+bizId+"对应的pledgeSecurity为："+pledgeSecurity);
+            //对应 secApply以“-”分隔的第十四个值
+            collector.assertEqual("14",pledgeSecurity);
 
 
             Date end = new Date();
