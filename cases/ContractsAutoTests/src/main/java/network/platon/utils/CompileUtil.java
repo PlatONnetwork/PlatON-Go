7887c76402b9d0d69d44d43fcba1e380a58ac5e7
xiaoqichuan@juzix.net <xiaoqichuan@juzix.net>
2020-02-06 10:27:31 +0800

    debug java wrapper class 1

--- cases/ContractsAutoTests/src/main/java/network/platon/utils/CompileUtil.java
+++ cases/ContractsAutoTests/src/main/java/network/platon/utils/CompileUtil.java
@@ -32,7 +32,7 @@ public class CompileUtil {
         try {
             permit.acquire();
             // /usr/local/bin/platon-cpp
-            String[] args = new String[]{"/bin/bash", "/usr/local/bin/platon-cpp", "-o", file, buildPath, "-abigen"};
+            String[] args = new String[]{"/bin/bash", "-c", "/usr/local/bin/platon-cpp" + " " + "-o" + " " + file + " " + buildPath + " " + "-abigen"};
             Process ps = Runtime.getRuntime().exec(args);
             ps.waitFor(2, TimeUnit.SECONDS);
             BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
@@ -44,6 +44,7 @@ public class CompileUtil {
             }
 
             String result = sb.toString();
+            System.out.println(result);
         } catch (Exception e) {
             e.printStackTrace();
             throw new Exception(e);
