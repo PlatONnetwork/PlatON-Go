7a7a4b0778c620e1dd27361ea3352af951f32fca
Liweicheng <237162922@qq.com>
2020-02-09 18:34:01 +0800

    submit function contracts

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/Functions/CoinbaseFunction.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/Functions/CoinbaseFunction.cpp
@@ -4,21 +4,21 @@ using namespace platon;
 
 /**
 * 验证内置的一些与链交互的函数
-* 函数platon_caller
+* 函数platon_origin
 */
 
 
-CONTRACT CallerFunction : public platon::Contract{
+CONTRACT CoinbaseFunction : public platon::Contract{
 	public:
 
     ACTION void init() {}
 
-    CONST std::string get_platon_caller() {
+    CONST std::string get_platon_coinbase() {
         bytes addr;
       	addr.resize(20);
-        ::platon_caller(addr.data());
+        ::platon_coinbase(addr.data());
         return Address(addr).toString();
     }
 };
 
-PLATON_DISPATCH(CallerFunction, (init)(get_platon_caller))
+PLATON_DISPATCH(CoinbaseFunction, (init)(get_platon_coinbase))
