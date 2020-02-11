60462315884db602c2136857fec5fc46d5ca90e6
zjsunzone <zjsunzone@gmail.com>
2020-02-11 15:19:15 +0800

    test type of int32

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/date_type/BasicDataType/IntegerDataTypeContract.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/date_type/BasicDataType/IntegerDataTypeContract.cpp
@@ -102,6 +102,19 @@ CONTRACT IntegerDataTypeContract: public platon::Contract
 			return tInt8.self();
 		}
 		
+		/// to set value for int32.
+		ACTION void setInt32(int32_t input)
+		{
+			tInt32.self() = input;
+			DEBUG("Invoke setInt32", "input", input);
+		}
+		
+		/// get the value from int32.
+		CONST int32_t getInt32()
+		{
+			return tInt32.self();
+		}
+
 		
 
 	
