905dc9aca890a5cf47756959f0aa3b085fb29c66
zjsunzone <zjsunzone@gmail.com>
2020-02-11 19:57:48 +0800

    update IntegerDataTypeContract

--- cases/ContractsAutoTests/src/test/java/wasm/data_type/IntegerDataTypeContractTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/data_type/IntegerDataTypeContractTest.java
@@ -3,6 +3,7 @@ package wasm.data_type;
 import network.platon.autotest.junit.annotations.DataSource;
 import network.platon.autotest.junit.enums.DataSourceType;
 import network.platon.contracts.wasm.InnerFunction;
+import network.platon.contracts.wasm.IntegerDataTypeContract;
 import org.junit.Test;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.DefaultBlockParameterNumber;
@@ -22,17 +23,33 @@ public class IntegerDataTypeContractTest extends WASMContractPrepareTest {
 
     @Test
     @DataSource(type = DataSourceType.EXCEL, file = "test.xls", sheetName = "Sheet1",
-            author = "zjsunzone", showName = "wasm.data_type",sourcePrefix = "wasm")
-    public void testFunctionContract() {
+            author = "zjsunzone", showName = "wasm.integer_data_type",sourcePrefix = "wasm")
+    public void testIntegerTypeContract() {
 
         try {
             prepare();
 
             // deploy contract.
-            InnerFunction innerFunction = InnerFunction.deploy(web3j, transactionManager, provider).send();
-            String contractAddress = innerFunction.getContractAddress();
-            String transactionHash = innerFunction.getTransactionReceipt().get().getTransactionHash();
-            collector.logStepPass("InnerFunction issued successfully.contractAddress:" + contractAddress + ", hash:" + transactionHash);
+            IntegerDataTypeContract contract = IntegerDataTypeContract.deploy(web3j, transactionManager, provider).send();
+            String contractAddress = contract.getContractAddress();
+            String transactionHash = contract.getTransactionReceipt().get().getTransactionHash();
+            collector.logStepPass("IntegerDataTypeContract issued successfully.contractAddress:" + contractAddress + ", hash:" + transactionHash);
+
+            // test: int8
+            int int8 = contract.int8().send();
+            collector.logStepPass("To invoke int8 success, int8: " + int8);
+
+            // test: int32
+            int int32 = contract.int32().send();
+            collector.logStepPass("To invoke int32 success, int8: " + int32);
+
+            // test: int64
+            int int64 = contract.int64().send();
+            collector.logStepPass("To invoke int64 success, int64: " + int8);
+
+            // test: uint8
+            byte uint8 = contract.uint8t(Byte.valueOf("1")).send();
+            collector.logStepPass("To invoke uint8t success, uint8: " + uint8);
 
 
         } catch (Exception e) {
