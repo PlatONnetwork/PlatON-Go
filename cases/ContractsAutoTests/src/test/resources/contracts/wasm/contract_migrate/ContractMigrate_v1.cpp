3fa157935300feaa5c09f86add4a0ea84652c738
hudenian <hudenian@163.com>
2020-02-11 10:19:33 +0800

    wasm event contract interface class

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_migrate/ContractMigrate_v1.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_migrate/ContractMigrate_v1.cpp
@@ -26,6 +26,7 @@ CONTRACT ContractMigrate : public platon::Contract{
             Address return_address;
             platon_migrate_contract(return_address, init_arg, transfer_value, gas_value);
             PLATON_EMIT_EVENT1(transfer,return_address.toString(),return_address.toString());
+            DEBUG("new contract address:", return_address.toString());
             stringstorage.self() = return_address.toString();
             return return_address.toString();
       }
