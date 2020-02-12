6f193213022ed0809d59eebab24c509ea21e935f
GavinXu <meiyan532484710@163.com>
2020-02-12 13:37:36 +0800

    finished cross call contract test case Signed-off-by: GavinXu <meiyan532484710@163.com>

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_cross_call/ContractHello.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_cross_call/ContractHello.cpp
@@ -1,6 +1,8 @@
 #include <platon/platon.hpp>
 #include <string>
+//#include <Contract_hello.hpp>
 using namespace platon;
+
 
 class message {
    public:
@@ -21,31 +23,32 @@ class my_message : public message {
       PLATON_SERIALIZE_DERIVED(my_message, message, (body)(end))
 };
 
-extern char const contract_info[] = "info";
-
 CONTRACT hello : public platon::Contract{
    public:
       PLATON_EVENT1(hello, std::string, std::string, uint32_t)
-      ACTION void init(const my_message &one_message){
-         info.self().push_back(one_message);
-      }
+
+     ACTION void init(){}
       
       ACTION std::vector<my_message> add_message(const my_message &one_message){
           PLATON_EMIT_EVENT1(hello, "add_message", "event1", 1);
-          info.self().push_back(one_message);
-          return info.self();
+          DEBUG("hello add_message");
+          arr.self().push_back(one_message);
+          return arr.self();
       }
       CONST std::vector<my_message> get_message(const std::string &name){
           PLATON_EMIT_EVENT1(hello, "get_message", "event2", 2);
-          return info.self();
+          DEBUG("hello get_message");
+          return arr.self();
       }
 
-      ACTION void hello_abort(){platon_assert(0, "hello abort");}
+      CONST uint64_t get_vector_size(){
+          DEBUG("hello get_vector_size");
+          return arr.self().size();
+      }
 
-      ACTION void hello_panic(){platon_panic();}
 
    private:
-      platon::StorageType<contract_info, std::vector<my_message>> info;
+      platon::StorageType<"info_arr"_n, std::vector<my_message>> arr;
 };
 
-PLATON_DISPATCH(hello, (init)(add_message)(get_message)(hello_abort)(hello_panic))
+PLATON_DISPATCH(hello, (init)(add_message)(get_message)(get_vector_size))
