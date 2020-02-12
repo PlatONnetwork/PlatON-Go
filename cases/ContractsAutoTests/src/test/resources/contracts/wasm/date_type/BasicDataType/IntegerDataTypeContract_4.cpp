66b46e88d28e459a42e7a32332ed53cf6d80f127
zjsunzone <zjsunzone@gmail.com>
2020-02-12 15:04:14 +0800

    new contrace file for base data type

---
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/date_type/BasicDataType/IntegerDataTypeContract_4.cpp
@@ -0,0 +1,65 @@
+// Author: zjsunzone
+// Desc: 验证所有基础数据类型的入参、返回值等是否合规
+#include <platon/platon.hpp>
+#include <string>
+using namespace platon;
+
+
+CONTRACT IntegerDataTypeContract_4: public platon::Contract
+{
+
+	/// common
+	public:
+		ACTION void init()
+		{
+			// do something to init.
+		}
+
+		/// set value for address.
+		ACTION void setAddress(const std::string& input)
+		{
+			tAddress.self() = Address(input);
+		}
+		
+		CONST std::string getAddress()
+		{
+			return tAddress.self().toString();
+		}
+
+		/// set value for u256.
+		ACTION void setU256(uint64_t input)
+		{
+			tU256.self() = u256(input);
+		}
+		
+		CONST std::string getU256()
+		{
+			return to_string(tU256.self());
+		}
+
+		/// set value for h256.
+		ACTION void setH256(const std::string& input)
+		{
+			tH256.self() = h256(input);
+		}
+		
+		CONST std::string getH256()
+		{
+			return tH256.self().toString();
+		}
+			
+	private:
+		platon::StorageType<"saddress"_n, Address> tAddress;
+		platon::StorageType<"su256"_n, u256> tU256;
+		platon::StorageType<"sh256"_n, h256> tH256;
+
+};
+
+// (int8)(int64)(uint8t)(uint32t)(uint64t)(u128t)(u256t)
+//(setInt8)(getInt8)(setInt32)(getInt32)(setInt64)(getInt64)
+// (setUint8)(getUint8)(setUint32)(getUint32)(setUint64)(getUint64)
+PLATON_DISPATCH(IntegerDataTypeContract_4,(init)
+(setAddress)(getAddress)(setU256)(getU256)(setH256)(getH256))
+
+
+
