2f05a74543373d572e3b2f33367d0938b22faeae
zjsunzone <zjsunzone@gmail.com>
2020-02-12 10:20:34 +0800

    test set/get athe type of address

--- cases/ContractsAutoTests/src/test/java/wasm/data_type/IntegerDataTypeContractTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/data_type/IntegerDataTypeContractTest.java
@@ -5,6 +5,8 @@ import network.platon.autotest.junit.enums.DataSourceType;
 import network.platon.contracts.wasm.InnerFunction;
 import network.platon.contracts.wasm.IntegerDataTypeContract;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.web3j.abi.datatypes.Address;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.DefaultBlockParameterNumber;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -47,6 +49,22 @@ public class IntegerDataTypeContractTest extends WASMContractPrepareTest {
             boolean getBool = contract.getBool().send();
             collector.logStepPass("To invoke setBool and getBool, bool: " + getBool);
             collector.assertEqual(getBool, true);
+
+            // test: store char
+            Byte expectByte = (byte)1;
+            TransactionReceipt charTr = contract.setChar(expectByte).send();
+            collector.logStepPass("To invoke setChar success, txHash: " + charTr.getTransactionHash());
+            Byte getChar = contract.getChar().send();
+            collector.logStepPass("To invoke getChar success, getChar: " + getChar.byteValue());
+            collector.assertEqual(getChar.byteValue(), expectByte.byteValue());
+
+            // test: store address
+            Address expectAddr = new Address("0x5b05e7a3e2a688c5e5cc491545a84a1efc66c1b1");
+            TransactionReceipt addrTr = contract.setAddress(expectAddr.getValue()).send();
+            collector.logStepPass("To invoke setAddress success, txHash: " + addrTr.getTransactionHash());
+            String getAddress = contract.getAddress().send();
+            collector.logStepPass("To invoke getAddress success, getAddress: " + getAddress);
+            collector.assertEqual(expectAddr, getAddress);
 
         } catch (Exception e) {
             if(e instanceof ArrayIndexOutOfBoundsException){
