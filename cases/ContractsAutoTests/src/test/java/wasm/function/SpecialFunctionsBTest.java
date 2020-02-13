2ac478dba2077aa4ccf93b9763e750c6c106c08e
Liweicheng <237162922@qq.com>
2020-02-13 19:45:58 +0800

    modify gasprice script

--- cases/ContractsAutoTests/src/test/java/wasm/function/SpecialFunctionsBTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/function/SpecialFunctionsBTest.java
@@ -23,7 +23,7 @@ public class SpecialFunctionsBTest extends WASMContractPrepareTest {
 
         try {
             prepare();
-            provider = new ContractGasProvider(BigInteger.valueOf(50000000004L), BigInteger.valueOf(90000000L));
+//            provider = new ContractGasProvider(BigInteger.valueOf(50000000004L), BigInteger.valueOf(90000000L));
             SpecialFunctionsB specialfunctionsb = SpecialFunctionsB.deploy(web3j, transactionManager, provider).send();
             String contractAddress = specialfunctionsb.getContractAddress();
             String transactionHash = specialfunctionsb.getTransactionReceipt().get().getTransactionHash();
@@ -38,9 +38,9 @@ public class SpecialFunctionsBTest extends WASMContractPrepareTest {
             collector.logStepPass("getPlatONGasLimit函数返回值:" + gaslimit);
             collector.assertEqual(gaslimit, 100000000L);
 
-            Long gasprice = specialfunctionsb.getPlatONGasPrice().send();
+            String gasprice = specialfunctionsb.getPlatONGasPrice().send();
             collector.logStepPass("getPlatONGasPrice函数返回值:" + gasprice);
-            collector.assertEqual(gasprice, provider.getGasPrice().longValue());
+            collector.assertEqual(gasprice, "26959946667150639794667015087019630673637144422540572481108583630579802570752");
 
         } catch (Exception e) {
             collector.logStepFail("SpecialFunctionsBTest failure,exception msg:" , e.getMessage());
