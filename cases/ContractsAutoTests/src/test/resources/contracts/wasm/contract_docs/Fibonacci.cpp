2643f4fdf8bab5f0358213b536ec1fef124fea8b
zjsunzone <zjsunzone@gmail.com>
2020-02-14 15:48:26 +0800

    new contract for docs

---
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_docs/Fibonacci.cpp
@@ -0,0 +1,43 @@
+// Author: zjsunzone
+// 斐波拉契合约验证
+#include <platon/platon.hpp>
+#include <string>
+using namespace platon;
+
+CONTRACT Fibonacci: public platon::Contract
+{
+
+	public:
+		PLATON_EVENT1(Notify, uint64_t, uint64_t)
+
+	public:
+		ACTION void init()
+		{
+			// do something to init.
+		}
+		
+		ACTION void fibonacciNotify(uint64_t number)
+		{
+			uint64_t result = fibonacci(number);
+			PLATON_EMIT_EVENT1(Notify, number, result); 
+		}
+		
+		CONST uint64_t fibonacci(uint64_t number)
+		{
+			if(number == 0){
+				return 0;			
+			} else if(number == 1){
+				return 1;			
+			} else {
+				return fibonacci(number-1) + fibonacci(number-2);		
+			}
+		}
+
+};
+
+
+PLATON_DISPATCH(Fibonacci,(init)
+(fibonacciNotify)(fibonacci))
+
+
+
