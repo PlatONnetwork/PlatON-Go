92e7df8ac43b55458569448aee5bf2ce8a37a14c
xiaoqichuan@juzix.net <xiaoqichuan@juzix.net>
2020-02-21 16:47:19 +0800

    add recursion call test class

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/exec_efficiency/RecursionCall.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/exec_efficiency/RecursionCall.cpp
@@ -9,16 +9,19 @@ using namespace platon;
 CONTRACT RecursionCall : public platon::Contract {
 
     private:
-	    platon::StorageType<"test"_n, uint64_t> sum;
+	    platon::StorageType<"test"_n, uint64_t> sum = 0;
 	public:
         ACTION void init(){}
 
         ACTION void call(uint64_t n) {
             if (sum < n) {
                 ++sum;
-                call(sum);
+                call(n);
             }
         }
 
+        CONST uint64_t get_sum() {
+            return sum.self();
+        }
 };
-PLATON_DISPATCH(RecursionCall,(init)(call))
+PLATON_DISPATCH(RecursionCall,(init)(call)(get_sum))
