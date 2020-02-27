0cd0dd2eb891718766150635da07e4490abc3cbe
hudenian <hudenian@163.com>
2020-02-27 11:24:53 +0800

    contract event delete no use note

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_event/ContractEmitEvent1ComplexParam.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_event/ContractEmitEvent1ComplexParam.cpp
@@ -2,15 +2,6 @@
 #include <string>
 using namespace platon;
 
-//extern char const string_storage[] = "stringstorage";
- /**
-   * PLATON_EVENT 测试一个主题
-   * 编译：./platon-cpp ContractEmitEvent1.cpp -std=c++17
-   * 部署：deploy --wasm ContractEmitEvent1.wasm
-   * 调用：invoke --addr 0xd002CD0427bE17B0671B84A2221834116431aC29 --func one_emit_event1 --params {"name":"5","value":4}
-   * 调用：invoke --addr 0x21cC984a2dbD9431F7b2ebfd564Ff6034b5887c2 --func one_emit_event1_args2 --params {"name":"5","value":4}
-   * 查询：call --addr 0xd002CD0427bE17B0671B84A2221834116431aC29 --func get_string 
-   */
 CONTRACT ContractEmitEvent1ComplexParam : public platon::Contract{
    public:
       PLATON_EVENT1(transfer,std::string,uint32_t,std::list<std::string>)
