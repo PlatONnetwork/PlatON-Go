732b0623aaa05969c840b5666f6344a1aadd23a4
qcxiao <xiaoqichuan@juzix.net>
2020-02-28 21:18:08 +0800

    regenerator java wrapper class

--- cases/ContractsAutoTests/src/main/java/network/platon/contracts/RecursionCall.java
+++ cases/ContractsAutoTests/src/main/java/network/platon/contracts/RecursionCall.java
@@ -21,10 +21,10 @@ import org.web3j.tx.gas.GasProvider;
  * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
  * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
  *
- * <p>Generated with web3j version 0.7.5.8-SNAPSHOT.
+ * <p>Generated with web3j version 0.9.1.0-SNAPSHOT.
  */
 public class RecursionCall extends Contract {
-    private static final String BINARY = "608060405234801561001057600080fd5b5060b08061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806357e9813914602d575b600080fd5b605660048036036020811015604157600080fd5b81019080803590602001909291905050506058565b005b8060005410156078576000808154600101919050819055506077816058565b5b5056fea265627a7a72315820ae93c27e1ebf99f5f996d28c6d3789196f6a19742cd002d58e469942b079359164736f6c634300050d0032";
+    private static final String BINARY = "608060405234801561001057600080fd5b5060c28061001f6000396000f3fe608060405260043610601c5760003560e01c806357e98139146021575b600080fd5b604a60048036036020811015603557600080fd5b81019080803590602001909291905050506060565b6040518082815260200191505060405180910390f35b60008160005410156083576000808154600101919050819055506081826060565b505b600054905091905056fea265627a7a72315820b0ab0185b7531690b114598e2e5d5ccb80d31015347c84abe9508dfbd4dc468f64736f6c634300050d0032";
 
     public static final String FUNC_RECURSIONCALLTEST = "recursionCallTest";
 
@@ -46,12 +46,12 @@ public class RecursionCall extends Contract {
         super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
     }
 
-    public RemoteCall<TransactionReceipt> recursionCallTest(BigInteger n) {
+    public RemoteCall<TransactionReceipt> recursionCallTest(BigInteger n, BigInteger weiValue) {
         final Function function = new Function(
                 FUNC_RECURSIONCALLTEST, 
                 Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(n)), 
                 Collections.<TypeReference<?>>emptyList());
-        return executeRemoteCallTransaction(function);
+        return executeRemoteCallTransaction(function, weiValue);
     }
 
     public static RemoteCall<RecursionCall> deploy(Web3j web3j, Credentials credentials, GasProvider contractGasProvider) {
