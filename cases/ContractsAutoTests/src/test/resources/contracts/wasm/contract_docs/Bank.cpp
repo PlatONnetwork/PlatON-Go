f306aa70a3e8038b370b60f395af4a439599955a
zjsunzone <zjsunzone@gmail.com>
2020-03-09 17:52:01 +0800

    update Bank.cpp

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_docs/Bank.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_docs/Bank.cpp
@@ -84,7 +84,7 @@ CONTRACT Bank: public platon::Contract, public Ownable
 			u128 DailyInt1 = callValue/ u128(100) * IntFee_.self();
 			u128 DailyIntFinal = DailyInt1 / u128(10);
 			InterestPool_.self() += DailyIntFinal;
-			//purchaseTokens(callValue, _referredBy);
+			purchaseTokens(callValue, _referredBy);
 		}
 
 		ACTION void IDD(){
@@ -98,23 +98,38 @@ CONTRACT Bank: public platon::Contract, public Ownable
 			u128 DailyInterestFinal = DailyInterest1 / u128(10);
 			InterestPool_.self() -= DailyInterestFinal;
 			//
-			//DividendsDistribution(DailyInterestFinal, Address("0x0"));
+			DividendsDistribution(DailyInterestFinal, Address("0x0"));
 		}
 
 		ACTION void DivsAddon(){
 			u128 callValue = platon_call_value();
-			//DividendsDistribution(callValue, Address("0x0"));
+			DividendsDistribution(callValue, Address("0x0"));
 		}
 		
 		ACTION void reinvest() {
-						
+			u128 _dividends = myDividends(false);
+			Address _customerAddress = platon_caller();
+			payoutsTo_.self()[_customerAddress] += _dividends*magnitude.self();
+			_dividends += referralBalance_.self()[_customerAddress];
+			referralBalance_.self()[_customerAddress] = 0;
+			u128 _tokens = purchaseTokens(_dividends, Address("0x0"));
+			PLATON_EMIT_EVENT1(onReinvestment, _customerAddress, _dividends, _tokens);
 		}
 
 		ACTION void exit(){
+			Address _customerAddress = platon_caller();
+			u128 _tokens = tokenBalanceLedger_.self()[_customerAddress];
+			if (_tokens > 0){
+				sell(_tokens);			
+			}
+			withdraw();
 		}
 
 		ACTION void withdraw(){
 			onlyStronghands();		
+			Address _customerAddress = platon_caller();
+			u128 _dividends = myDividends(false);
+			//payoutsTo_.self()[_customerAddress] += 
 		}
 		
 		ACTION void sell(u128 _amountOfTokens){
@@ -175,7 +190,26 @@ CONTRACT Bank: public platon::Contract, public Ownable
 		}
 		
 		u128 DividendsDistribution(u128 _incomingEthereum, Address _referredBy){
-			return u128(0);		
+			Address _customerAddress = platon_caller();
+			u128 _undividedDividends = (_incomingEthereum * u128(100)) / u128(100);
+			u128 _referralBonus = (_undividedDividends * refferalFee_.self()) / u128(100);
+			u128 _dividends = _undividedDividends - _referralBonus;
+			u128 _taxedEthereum = _incomingEthereum - _undividedDividends;
+			u128 _amountOfTokens = ethereumToTokens_(_taxedEthereum);
+			u128 _fee = _dividends * magnitude.self();
+			
+			if(_amountOfTokens >= 0 && (_amountOfTokens + tokenSupply_.self()) >= tokenSupply_.self()){
+				platon_revert();			
+			}
+
+			//
+			if(_referredBy != Address("0x0000000000000000000000000000000000000000") &&
+				_referredBy != _customerAddress &&
+				tokenBalanceLedger_.self()[_referredBy] >= stakingRequirement.self()			
+			){
+				referralBalance_.self()[_referredBy] = referralBalance_.self()[_referredBy] + _referralBonus;
+			}
+			return u128(0);
 		}
 		
 		u128 ethereumToTokens_(u128 _ethereum) {
@@ -183,7 +217,10 @@ CONTRACT Bank: public platon::Contract, public Ownable
 		}
 		
 		u128 tokensToEthereum(u128 _tokens) {
-			return _tokens;		
+			u128 tokens_ = _tokens + u128("1000000000000000000");
+			u128 _tokenSupply = tokenSupply_.self() + u128("1000000000000000000");
+			u128 _etherReceived = ((tokenPriceInitial_.self() + (tokenPriceIncremental_.self() * (_tokenSupply / u128("1000000000000000000")))) * (tokens_ - u128("1000000000000000000")) - (tokenPriceIncremental_.self() * ((tokens_*u128(4) - tokens_) / u128("1000000000000000000")) / u128(2))) / u128("1000000000000000000");
+			return _etherReceived;		
 		}
 		
 		u128 sqrt(u128 x) {
