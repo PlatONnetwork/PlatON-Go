cdee7bd885d3eceb830d13c4aa6f7d806d482a47
hudenian <hudenian@163.com>
2020-03-10 20:50:19 +0800

    guess contract gaslimit modify

--- cases/ContractsAutoTests/src/test/resources/contracts/evm/guessing/Guessing.sol
+++ cases/ContractsAutoTests/src/test/resources/contracts/evm/guessing/Guessing.sol
@@ -1,36 +1,34 @@
 pragma solidity ^0.5.13;
 /**
  * 竞猜合约
- * 每次下注合约接收用户转账，满足条件下(单笔>=5LAT，超过5LAT也只分配一个彩票)，
- * 每笔交易给对应地址分配一个彩票（一个地址多笔交易可分配多个彩票），并记录地址转入数；
+ * 当用户往竞猜合约发起转账操作，满足(单笔>=5LAT)，
+ * 每笔交易给对应地址分配一个抽奖号码（一个地址多笔交易可分配多个抽奖号码），并记录地址转入数；
  *
  */
 contract Guessing {
 
     uint256 public endBlock; //竞猜截止块高
+    bytes32 public block_hash;//竞猜截止块高的块hash
     bool public guessingClosed = false; //竞猜是否已开奖
-    uint256 public baseUnit = 5 lat; //最小转金额（为了公平起见，保证只5个lat可以取到一个选票。）
+    uint256 public baseUnit = 5 lat; //最小转金额
 
-    uint256 public balance; //竞猜总金额
-    uint256 public averageAmount; //每个人获奖金额
-    mapping(address => uint256) public gussingerLat; //每个竞猜者对应的金额
-    mapping(uint256 =>address payable ) public IndexOfgussinger; //每个竞猜者对应的下标（5个lat就给他分配一个随机数）
+    uint256 public balance; //竞猜奖池总金额
+    uint256 public averageAmount; //每个中奖号码对应获奖金额
+    mapping(address => uint256) public gussingerLat; //每个竞猜者发起转账的金额
+    mapping(uint256 =>address payable ) public indexOfgussinger; //每个抽奖号码对应的竞猜者地址
+    mapping(address => uint[]) public gussingerCodes; //每个竞猜者所有的抽奖号码集合
     mapping(address => uint256 ) public winnerMap; //中奖者对应中奖号码个数
     uint public indexKey = 0;
-    address[] public winnerAddresses; //中奖者地址
+    address[] public winnerAddresses; //中奖者地址数组
     address public createAddress;//合约创建者
+    uint public postfix = 0; //中奖尾号
+
 
     //竞猜成功通知
     event FundTransfer(address _backer, uint _amount, bool _isSuccess);
 
     //记录已接收的LAT通知
     event CurrentBalance(address _msgSenderAddress, uint _balance);
-
-    //校验地址是否为空
-    modifier validAddress(address _address) {
-        require(_address != address(0x0));
-        _;
-    }
 
     /**
      * 初始化构造函数
@@ -43,57 +41,44 @@ contract Guessing {
     }
 
     /**
-     * 默认函数
      *
      * 默认函数，可以向合约直接打款
      */
     function () payable external {
-        require(msg.value >= baseUnit);
-
-        uint amount = msg.value;
-        IndexOfgussinger[indexKey] = msg.sender;
-        indexKey++;
-
-        //竞猜人的金额累加(可以投多次)
-        gussingerLat[msg.sender] += amount;
-
-        //竞猜总额累加
-        balance += amount;
-
-        //竞猜成功通知
-        emit FundTransfer(msg.sender, amount, true);
+        guessing(msg.value,msg.sender);
     }
-
 
     /**
      * 判断是否已经过了竞猜截止限期
      */
     modifier beforeDeadline() { if (endBlock >= block.number) _; }
 
-    /**
-     * 判断转账金额要大于5lat
-     */
-    modifier checkAmount() { if (msg.value >= baseUnit) _; }
-
 
     /**
-     * 竞猜(带上金额)
+     * 竞猜
      */
-    function guessingWithLat() public beforeDeadline checkAmount payable{
+    function guessingWithLat() public payable{
+        guessing(msg.value,msg.sender);
+    }
 
-        uint amount = msg.value;
+    //竞猜内部实现
+    function guessing(uint amount,address payable msgsender) beforeDeadline internal{
+        //转账金额要大于5lat
+        require(amount >= baseUnit);
 
-        IndexOfgussinger[indexKey] = msg.sender;
+        gussingerCodes[msgsender].push(indexKey);
+
+        indexOfgussinger[indexKey] = msgsender;
         indexKey++;
 
-        //竞猜人的金额累加(可以投多次)
-        gussingerLat[msg.sender] += amount;
+        //竞猜人的金额累加
+        gussingerLat[msgsender] += amount;
 
         //竞猜总额累加
         balance += amount;
 
         //竞猜成功通知
-        emit FundTransfer(msg.sender, amount, true);
+        emit FundTransfer(msgsender, amount, true);
     }
 
     /**
@@ -124,59 +109,18 @@ contract Guessing {
      *   例如余数是1234，取三位数，票数后三位带234的为中奖票
      *
      */
-    function draw() public afterDeadline {
-        //只有合约创建者可以开奖
+    function draw(bytes32 _block_hash) public afterDeadline {
+        //开奖只能操作一次，且只有合约创建者可以开奖
         if(!guessingClosed && createAddress == msg.sender && indexKey > 0){
-
-            uint256 random = uint256(keccak256(abi.encodePacked(blockhash(block.number - 20))));
+            uint256 random = uint256(keccak256(abi.encodePacked(_block_hash)));
             uint drawIndex = random%indexKey;
-            uint postfix;
 
             if(indexKey<100){
-                postfix = drawIndex%10;
-                if(postfix ==0){
-                    for(uint256 i=0;i<indexKey;i++){
-                        if((i-postfix)%10 == 0){
-                            winnerAddresses.push(IndexOfgussinger[i]);
-                        }
-                    }
-                }else{
-                    for(uint256 i=0;i<indexKey;i++){
-                        if(i%10 != 0 && (i-postfix)%10 == 0){
-                            winnerAddresses.push(IndexOfgussinger[i]);
-                        }
-                    }
-                }
+                getwinners(drawIndex,10);
             }else if(indexKey<10000){
-                postfix = drawIndex%100;
-                if(postfix ==0){
-                    for(uint256 i=0;i<indexKey;i++){
-                        if((i-postfix)%100 == 0){
-                            winnerAddresses.push(IndexOfgussinger[i]);
-                        }
-                    }
-                }else{
-                    for(uint256 i=0;i<indexKey;i++){
-                        if(i%100 != 0 && (i-postfix)%100 == 0){
-                            winnerAddresses.push(IndexOfgussinger[i]);
-                        }
-                    }
-                }
+                getwinners(drawIndex,100);
             }else{
-                postfix = drawIndex%1000;
-                if(postfix ==0){
-                    for(uint256 i=0;i<indexKey;i++){
-                        if((i-postfix)%1000 == 0){
-                            winnerAddresses.push(IndexOfgussinger[i]);
-                        }
-                    }
-                }else{
-                    for(uint256 i=0;i<indexKey;i++){
-                        if(i%1000 != 0 && (i-postfix)%1000 == 0){
-                            winnerAddresses.push(IndexOfgussinger[i]);
-                        }
-                    }
-                }
+                getwinners(drawIndex,1000);
             }
 
             //每个中奖者可以分到的金额
@@ -187,14 +131,34 @@ contract Guessing {
                 //中奖者中奖票号统计
                 winnerMap[winnerAddresses[j]] = winnerMap[winnerAddresses[j]]+1;
                 if(winnerAddresses[j] != address(0x0)){
-                   tempAddress = address(uint160(winnerAddresses[j]));
-                   tempAddress.transfer(averageAmount);
+                    tempAddress = address(uint160(winnerAddresses[j]));
+                    tempAddress.transfer(averageAmount);
                 }
             }
 
             guessingClosed = true;
+            block_hash = _block_hash;
         }
     }
+
+    //获取所有的中奖者
+    function getwinners(uint drawIndex,uint times) internal{
+        postfix = drawIndex%times;
+        if(postfix ==0){
+            for(uint256 i=0;i<indexKey;i++){
+                if((i-postfix)%times == 0){
+                    winnerAddresses.push(indexOfgussinger[i]);
+                }
+            }
+        }else{
+            for(uint256 i=0;i<indexKey;i++){
+                if(i%times != 0 && (i-postfix)%times == 0){
+                    winnerAddresses.push(indexOfgussinger[i]);
+                }
+            }
+        }
+    }
+
 
     /**
      * 查看当前合约中的余额
@@ -218,12 +182,27 @@ contract Guessing {
     }
 
     /**
-    * 当前块的时间截
-    */
-    function getBlockTimestamp() public view returns(uint) {
-        uint256 random = uint256(keccak256(abi.encodePacked(block.timestamp)));
-        uint drawIndex = random%3;
-        return drawIndex;
+     * 查询截止区块对应的hash
+     */
+    function getEndBlockHash() view public returns (bytes32){
+        return block_hash;
     }
+
+    /**
+     * 生成指定区块的blockhash
+     */
+    function generateBlockHash(uint256 _blocknumber) view public returns (bytes32){
+        require(_blocknumber > block.number - 257 && _blocknumber < block.number - 1);
+        return blockhash(_blocknumber);
+    }
+
+
+    /**
+     * 获取当前参与者所有幸运号码
+     */
+    function getMyGuessCodes() view public returns (uint[] memory){
+        return gussingerCodes[msg.sender];
+    }
+
 
 }
