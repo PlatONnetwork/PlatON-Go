1d8ec0fd641cd8c1cf07676bb6dd6e21d797860e
zjsunzone <zjsunzone@gmail.com>
2020-03-10 16:17:47 +0800

    update ContractCypherBankTest.java

--- cases/ContractsAutoTests/src/test/java/wasm/contract_docs/ContractCypherBankTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/contract_docs/ContractCypherBankTest.java
@@ -2,10 +2,12 @@ package wasm.contract_docs;
 
 import network.platon.autotest.junit.annotations.DataSource;
 import network.platon.autotest.junit.enums.DataSourceType;
-import network.platon.contracts.wasm.SimpleStorage;
+import network.platon.contracts.wasm.Bank;
 import org.junit.Before;
 import org.junit.Test;
 import wasm.beforetest.WASMContractPrepareTest;
+
+import java.math.BigInteger;
 
 /**
  * @author zjsunzone
@@ -21,23 +23,25 @@ public class ContractCypherBankTest extends WASMContractPrepareTest {
 
     @Test
     @DataSource(type = DataSourceType.EXCEL, file = "test.xls", sheetName = "Sheet1",
-            author = "zjsunzone", showName = "wasm.contract_sypherBank",sourcePrefix = "wasm")
+            author = "zjsunzone", showName = "wasm.contract_CypherBank",sourcePrefix = "wasm")
     public void testSimpleStorageContract() {
         try {
             // deploy contract.
-            SimpleStorage contract = SimpleStorage.deploy(web3j, transactionManager, provider).send();
+            Bank contract = Bank.deploy(web3j, transactionManager, provider).send();
             String contractAddress = contract.getContractAddress();
             String transactionHash = contract.getTransactionReceipt().get().getTransactionHash();
-            collector.logStepPass("SimpleStorage issued successfully.contractAddress:" + contractAddress + ", hash:" + transactionHash);
-            collector.logStepPass("SimpleStorage deploy successfully. gasUsed: " + contract.getTransactionReceipt().get().getGasUsed().toString());
+            collector.logStepPass("contract_CypherBank issued successfully.contractAddress:" + contractAddress + ", hash:" + transactionHash);
+            collector.logStepPass("contract_CypherBank deploy successfully. gasUsed: " + contract.getTransactionReceipt().get().getGasUsed().toString());
 
-
+            // tokenSupply
+            BigInteger tokenSupply = contract.totalSupply().send();
+            collector.logStepPass("Call totalSupply, res: " + tokenSupply);
 
         } catch (Exception e) {
             if(e instanceof ArrayIndexOutOfBoundsException){
-                collector.logStepPass("SimpleStorage and could not call contract function");
+                collector.logStepPass("contract_CypherBank and could not call contract function");
             }else{
-                collector.logStepFail("SimpleStorage failure,exception msg:" , e.getMessage());
+                collector.logStepFail("contract_CypherBank failure,exception msg:" , e.getMessage());
                 e.printStackTrace();
             }
         }
