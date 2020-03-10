332760d7dd551198daff714fdde51fdd32f35c23
hudenian <hudenian@163.com>
2020-03-10 21:02:51 +0800

    contract gaslimit modify

--- cases/ContractsAutoTests/src/test/java/wasm/contract_create/InitWithMapParamsTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/contract_create/InitWithMapParamsTest.java
@@ -68,7 +68,6 @@ public class InitWithMapParamsTest extends WASMContractPrepareTest {
 
         try {
             prepare();
-            provider = new ContractGasProvider(BigInteger.valueOf(50000000004L), BigInteger.valueOf(90000000L));
             InitWithMapParams initWithMapParams = InitWithMapParams.deploy(web3j, transactionManager, provider,maps).send();
             String contractAddress = initWithMapParams.getContractAddress();
             String transactionHash = initWithMapParams.getTransactionReceipt().get().getTransactionHash();
