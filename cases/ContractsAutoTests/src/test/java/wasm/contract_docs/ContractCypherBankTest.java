add74665e7f0ffef70cce0c40a22cc75987703f3
zjsunzone <zjsunzone@gmail.com>
2020-03-11 18:00:09 +0800

    update ContractCypherBankTest.java

--- cases/ContractsAutoTests/src/test/java/wasm/contract_docs/ContractCypherBankTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/contract_docs/ContractCypherBankTest.java
@@ -1,12 +1,14 @@
 package wasm.contract_docs;
 
 import com.platon.rlp.datatypes.Uint8;
+import com.platon.rlp.datatypes.WasmAddress;
 import network.platon.autotest.junit.annotations.DataSource;
 import network.platon.autotest.junit.enums.DataSourceType;
 import network.platon.contracts.wasm.Bank;
 import org.junit.Before;
 import org.junit.Test;
 import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.Transfer;
 import org.web3j.utils.Convert;
 import wasm.beforetest.WASMContractPrepareTest;
@@ -40,7 +42,7 @@ public class ContractCypherBankTest extends WASMContractPrepareTest {
 
             // transfer
             Transfer t = new Transfer(web3j, transactionManager);
-            t.sendFunds(contractAddress, new BigDecimal(100000000), Convert.Unit.LAT, provider.getGasPrice(), provider.getGasLimit()).send();
+            t.sendFunds(contractAddress, new BigDecimal("100000000000"), Convert.Unit.LAT, provider.getGasPrice(), provider.getGasLimit()).send();
             BigInteger cbalance = web3j.platonGetBalance(contractAddress, DefaultBlockParameterName.LATEST).send().getBalance();
             collector.logStepPass("Transfer to contract , address: " + contractAddress + " cbalance: " + cbalance);
 
@@ -53,9 +55,59 @@ public class ContractCypherBankTest extends WASMContractPrepareTest {
             collector.logStepPass("Call exitFee, res: " + exitFee.getValue());
 
             // buy
-            //TransactionReceipt buyTr = contract.buy(new WasmAddress(credentials.getAddress()), new BigInteger("100000000000000000000")).send();
-            //collector.logStepPass("Send buy, txHash: " + buyTr.getTransactionHash() + " gasUsed: " + buyTr.getGasUsed());
-            //collector.logStepPass("Send buy ,logs size: " + buyTr.getLogs().size());
+            TransactionReceipt buyTr = contract.buy(new WasmAddress(credentials.getAddress()), new BigInteger("100000000000000000000")).send();
+            collector.logStepPass("Send buy, txHash: " + buyTr.getTransactionHash() + " gasUsed: " + buyTr.getGasUsed());
+            collector.logStepPass("Send buy ,logs size: " + buyTr.getLogs().size());
+            /*Bank.TestDataEventResponse testData = contract.getTestDataEvents(buyTr).get(0);
+            collector.logStepPass("parse logs, args1: " +
+                    testData.arg3.toString() + "" +
+                    " args2: " + testData.arg2 +
+                    " args3: " + testData.arg1);*/
+
+            // IDD
+            TransactionReceipt iddTr = contract.IDD().send();
+            collector.logStepPass("Send IDD, txHash: " + iddTr.getTransactionHash() + " gasUsed: " + iddTr.getGasUsed());
+            collector.logStepPass("Send IDD ,logs size: " + iddTr.getLogs().size());
+
+            // DivsAddon
+            TransactionReceipt divsAddonTr = contract.DivsAddon().send();
+            collector.logStepPass("Send DivsAddon, txHash: " + divsAddonTr.getTransactionHash() + " gasUsed: " + divsAddonTr.getGasUsed());
+            collector.logStepPass("Send DivsAddon ,logs size: " + divsAddonTr.getLogs().size());
+
+            // reinvest
+            TransactionReceipt reinvestTr = contract.reinvest().send();
+            collector.logStepPass("Send reinvest, txHash: " + reinvestTr.getTransactionHash() + " gasUsed: " + reinvestTr.getGasUsed());
+            collector.logStepPass("Send reinvest ,logs size: " + reinvestTr.getLogs().size());
+
+            // withdraw
+            TransactionReceipt withdrawTr = contract.withdraw().send();
+            collector.logStepPass("Send withdraw, txHash: " + withdrawTr.getTransactionHash() +
+                    " gasUsed: " + withdrawTr.getGasUsed());
+            collector.logStepPass("Send withdraw ,logs size: " + withdrawTr.getLogs().size());
+
+            // sell
+            TransactionReceipt sellTr = contract.sell(new BigInteger("1000")).send();
+            collector.logStepPass("Send sell, txHash: " + sellTr.getTransactionHash() +
+                    " gasUsed: " + sellTr.getGasUsed());
+            collector.logStepPass("Send sell ,logs size: " + sellTr.getLogs().size());
+
+            // transfer
+            TransactionReceipt transferTr = contract.transfer(new WasmAddress("0x72ad2b713faa14c2c4cd2d7affe5d8f538968f5a"), new BigInteger("300000000000000000")).send();
+            collector.logStepPass("Send transfer, txHash: " + transferTr.getTransactionHash() +
+                    " gasUsed: " + transferTr.getGasUsed());
+            collector.logStepPass("Send transfer ,logs size: " + transferTr.getLogs().size());
+
+            // balanceOf
+            BigInteger balanceOf = contract.balanceOf(new WasmAddress("0x72ad2b713faa14c2c4cd2d7affe5d8f538968f5a")).send();
+            collector.logStepPass("Call balanceOf, res: " + balanceOf + " expect: " + "300000000000000000"); //
+
+            // myDividends
+            BigInteger myDividends = contract.myDividends(false).send();
+            collector.logStepPass("Call myDividends, res: " + myDividends);
+
+            // dividendsOf
+            BigInteger dividendsOf = contract.dividendsOf(new WasmAddress(credentials.getAddress())).send();
+            collector.logStepPass("Call dividendsOf, res: " + dividendsOf);
 
             // totalEthereumBalance
             BigInteger totalEthereumBalance = contract.totalEthereumBalance().send();
