fd0ce08ac06d914cb5bb583482881f1a1ce6b6d7
GavinXu <meiyan532484710@163.com>
2020-03-11 23:10:58 +0800

    finished changing  state storage Signed-off-by: GavinXu <meiyan532484710@163.com>

--- core/state/statedb_test.go
+++ core/state/statedb_test.go
@@ -105,9 +105,13 @@ func TestNewStateDBAndCopy(t *testing.T) {
 			maps[key] = value
 			storages[addr] = maps
 		}
+		//if bytes.Equal([]byte{}, []byte(value)) || bytes.Equal([]byte(nil), []byte(value)) {
+		//	fmt.Println("xaxaxaxaxaxaxaxa", "addr", addr.String(),  "key", key, "value", value, "[]byte{}",  bytes.Equal([]byte{}, []byte(value)), "[]byte(nil)",  bytes.Equal([]byte(nil), []byte(value)))
+		//	fmt.Println("xaxaxaxaxaxaxaxa", bytes.Equal([]byte{}, []byte(nil)))
+		//}
 		//fmt.Println("addr", addr.String(), "key", key, "value", value)
 	}
-	for i := 0; i < 255; i++ {
+	for i := 1; i < 255; i++ {
 		modify(s1, s1c, common.Address{byte(i)}, i)
 	}
 
@@ -115,6 +119,10 @@ func TestNewStateDBAndCopy(t *testing.T) {
 	for addr, storage := range storages {
 		for k, v := range storage {
 			value := st.GetState(addr, []byte(k))
+			//fmt.Println("storage :::: ", "k", k, "v", v, "state v", string(value))
+			//if bytes.Equal([]byte{}, []byte(v)) || bytes.Equal(value, []byte(nil)) {
+			//	fmt.Println("lalalalala", "k", k, "v", v, "state v", string(value))
+			//}
 			assert.Equal(t, []byte(v), value)
 		}
 	}
@@ -187,7 +195,7 @@ func TestNewStateDBAndCopy(t *testing.T) {
 	assert.Len(t, s1cc.clearReferenceFunc, 0)
 	assert.Nil(t, s1cc.parent)
 
-	for i := 0; i < 255; i++ {
+	for i := 1; i < 255; i++ {
 		modify(s3, s1cc, common.Address{byte(i)}, i)
 	}
 
@@ -333,7 +341,7 @@ func TestStateStorageRevert(t *testing.T) {
 
 	assert.Equal(t, value1, s1.GetState(addr, key1))
 	obj := s1.getStateObject(addr)
-	assert.Equal(t, 0, len(obj.dirtyValueStorage))
+	assert.Equal(t, 0, len(obj.dirtyStorage))
 }
 
 func TestStateStorageValueUpdate(t *testing.T) {
@@ -349,7 +357,7 @@ func TestStateStorageValueUpdate(t *testing.T) {
 	s1.SetState(addr, key1, value1)
 	s1.SetState(addr, key1, value2)
 	obj := s1.getStateObject(addr)
-	assert.Equal(t, 1, len(obj.dirtyValueStorage))
+	assert.Equal(t, 1, len(obj.dirtyStorage))
 }
 
 // Tests that no intermediate state of an object is stored into the database,
@@ -671,11 +679,13 @@ func (test *snapshotTest) checkEqual(state, checkstate *StateDB) error {
 		checkeq("GetCodeSize", state.GetCodeSize(addr), checkstate.GetCodeSize(addr))
 		// Check storage.
 		if obj := state.getStateObject(addr); obj != nil {
-			state.ForEachStorage(addr, func(key, value []byte) bool {
-				return checkeq("GetState("+string(key)+")", checkstate.GetState(addr, key), value)
+			state.ForEachStorage(addr, func(key common.Hash, value []byte) bool {
+				cobj := checkstate.getStateObject(addr)
+				return checkeq("GetState("+ key.String() +")", cobj.GetState(checkstate.db, key), value)
 			})
-			checkstate.ForEachStorage(addr, func(key, value []byte) bool {
-				return checkeq("GetState("+string(key)+")", checkstate.GetState(addr, key), value)
+			checkstate.ForEachStorage(addr, func(key common.Hash, value []byte) bool {
+				cobj := checkstate.getStateObject(addr)
+				return checkeq("GetState("+ key.String() +")", cobj.GetState(checkstate.db, key), value)
 			})
 		}
 		if err != nil {
--- eth/tracers/tracer_test.go
+++ eth/tracers/tracer_test.go
@@ -41,7 +41,7 @@ func (account) Balance() *big.Int                                   { return nil
 func (account) Address() common.Address                             { return common.Address{} }
 func (account) ReturnGas(*big.Int)                                  {}
 func (account) SetCode(common.Hash, []byte)                         {}
-func (account) ForEachStorage(cb func(key, value common.Hash) bool) {}
+func (account) ForEachStorage(cb func(key common.Hash, value []byte) bool) {}
 
 func runTrace(tracer *Tracer) (json.RawMessage, error) {
 	env := vm.NewEVM(vm.Context{Ctx: context.TODO(), BlockNumber: big.NewInt(1)}, nil, params.TestChainConfig, vm.Config{Debug: true, Tracer: tracer})
