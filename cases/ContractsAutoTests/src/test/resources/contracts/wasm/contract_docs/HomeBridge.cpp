0e6330b8f5df6bfa88abcf7933c35798cde7120b
zjsunzone <zjsunzone@gmail.com>
2020-03-16 13:30:07 +0800

    add HomeBridge.cpp

---
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_docs/HomeBridge.cpp
@@ -0,0 +1,103 @@
+#include <platon/platon.hpp>
+#include <string>
+using namespace platon;
+
+#include "src/BridgeDeploymentAddressStorage.hpp"
+#include "src/HomeBridgeGasConsumptionLimitsStorage.hpp"
+
+class HomeBridge: public platon::Contract, public: BridgeDeploymentAddressStorage, public: HomeBridgeGasConsumptionLimitsStorage
+{
+	public:
+		/// Number of authorities signatures required to withdraw the money.
+	    ///
+	    /// Must be lesser than number of authorities.
+		platon::StorageType<"requiredSignatures"_n, u128> requiredSignatures;
+		/// The gas cost of calling `HomeBridge.withdraw`.
+	    ///
+	    /// Is subtracted from `value` on withdraw.
+	    /// recipient pays the relaying authority for withdraw.
+	    /// this shuts down attacks that exhaust authorities funds on home chain.
+	    platon::StorageType<"estimatedGasCostOfWithdraw"_n, u128> estimatedGasCostOfWithdraw;
+
+	    /// Contract authorities.
+	    platon::StorageType<"authorities"_n, std::vector<Address>> authorities;
+
+	    /// Used foreign transaction hashes.
+	    platon::StorageType<"withdraws"_n, std::map<bytes, bool>> withdraws;
+
+	public:
+		/// Event created on money deposit.
+	    PLATON_EVENT1(Deposit, Address, u128);
+
+	    /// Event created on money withdraw.
+	    PLATON_EVENT1(Withdraw, Address, u128);
+
+	public:
+		void init(u128 requiredSignaturesParam, std::vector<Address> authoritiesParam, 
+			u128 estimatedGasCostOfWithdrawParam {
+			if(requiredSignaturesParam == u128(0)){
+				platon_revert();
+			}
+			if(requiredSignaturesParam > u128(authoritiesParam.size())){
+				platon_revert();
+			}
+	
+	        requiredSignatures.self() = requiredSignaturesParam;
+	        authorities.self() = authoritiesParam;
+	        estimatedGasCostOfWithdraw.self() = estimatedGasCostOfWithdrawParam;
+		}
+		
+		 /// final step of a withdraw.
+	    /// checks that `requiredSignatures` `authorities` have signed of on the `message`.
+	    /// then transfers `value` to `recipient` (both extracted from `message`).
+	    /// see message library above for a breakdown of the `message` contents.
+	    /// `vs`, `rs`, `ss` are the components of the signatures.
+
+	    /// anyone can call this, provided they have the message and required signatures!
+	    /// only the `authorities` can create these signatures.
+	    /// `requiredSignatures` authorities can sign arbitrary `message`s
+	    /// transfering any ether `value` out of this contract to `recipient`.
+	    /// bridge users must trust a majority of `requiredSignatures` of the `authorities`.
+	    ACTION void withdraw(std::vector<uint8_t> vs, std::vector<h256> rs, std::vector<h256> ss, bytes message) {
+	        //require(message.length == 116);
+
+	        // check that at least `requiredSignatures` `authorities` have signed `message`
+	        //require(Helpers.hasEnoughValidSignatures(message, vs, rs, ss, authorities, requiredSignatures));
+
+	        address recipient = Message.getRecipient(message);
+	        uint256 value = Message.getValue(message);
+	        bytes32 hash = Message.getTransactionHash(message);
+	        uint256 homeGasPrice = Message.getHomeGasPrice(message);
+
+	        // if the recipient calls `withdraw` they can choose the gas price freely.
+	        // if anyone else calls `withdraw` they have to use the gas price
+	        // `homeGasPrice` specified by the user initiating the withdraw.
+	        // this is a security mechanism designed to shut down
+	        // malicious senders setting extremely high gas prices
+	        // and effectively burning recipients withdrawn value.
+	        // see https://github.com/paritytech/parity-bridge/issues/112
+	        // for further explanation.
+	        require((recipient == msg.sender) || (tx.gasprice == homeGasPrice));
+
+	        // The following two statements guard against reentry into this function.
+	        // Duplicated withdraw or reentry.
+	        require(!withdraws[hash]);
+	        // Order of operations below is critical to avoid TheDAO-like re-entry bug
+	        withdraws[hash] = true;
+
+	        uint256 estimatedWeiCostOfWithdraw = estimatedGasCostOfWithdraw * homeGasPrice;
+
+	        // charge recipient for relay cost
+	        uint256 valueRemainingAfterSubtractingCost = value - estimatedWeiCostOfWithdraw;
+
+	        // pay out recipient
+	        recipient.transfer(valueRemainingAfterSubtractingCost);
+
+	        // refund relay cost to relaying authority
+	        msg.sender.transfer(estimatedWeiCostOfWithdraw);
+
+	        Withdraw(recipient, valueRemainingAfterSubtractingCost);
+	    }
+
+
+};
