a8683835bed4411e200e0c03fb677ad3673d1e38
zjsunzone <zjsunzone@gmail.com>
2020-03-16 13:30:41 +0800

    add ForeignBridgeGasConsumptionLimitsStorage.hpp

---
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/contract_docs/src/ForeignBridgeGasConsumptionLimitsStorage.hpp
@@ -0,0 +1,28 @@
+#include <platon/platon.hpp>
+#include <string>
+using namespace platon;
+
+class ForeignBridgeGasConsumptionLimitsStorage 
+{
+	public:
+		platon::StorageType<"gasLimitDepositRelay"_n, u128> gasLimitDepositRelay;
+		platon::StorageType<"gasLimitWithdrawConfirm"_n, u128> gasLimitWithdrawConfirm;
+
+	public:
+		PLATON_EVENT1(GasConsumptionLimitsUpdated, u128, u128);
+
+	public:
+		void init() {
+		}
+		
+	public:
+	    ACTION setGasLimitDepositRelay(u128 gas) {
+	        gasLimitDepositRelay.self() = gas;
+	        PLATON_EMIT_EVENT1(GasConsumptionLimitsUpdated, gasLimitDepositRelay.self(), gasLimitWithdrawConfirm.self());
+	    }
+
+	    ACTION setGasLimitWithdrawConfirm(u128 gas) {
+	        gasLimitWithdrawConfirm.self() = gas;
+	        PLATON_EMIT_EVENT1(GasConsumptionLimitsUpdated, gasLimitDepositRelay.self(), gasLimitWithdrawConfirm.self());
+	    }	
+};
