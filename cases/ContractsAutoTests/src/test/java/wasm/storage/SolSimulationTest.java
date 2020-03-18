0e965294df54e262ca8edeebde4e7c870679030b
xiaoqichuan@juzix.net <xiaoqichuan@juzix.net>
2020-03-18 17:11:27 +0800

    add VID contract

--- cases/ContractsAutoTests/src/test/java/wasm/storage/SolSimulationTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/storage/SolSimulationTest.java
@@ -1,11 +1,10 @@
 package wasm.storage;
 
 import com.platon.rlp.datatypes.Uint32;
-import com.platon.rlp.datatypes.Uint64;
 import evm.beforetest.ContractPrepareTest;
 import network.platon.autotest.junit.annotations.DataSource;
 import network.platon.autotest.junit.enums.DataSourceType;
-import network.platon.contracts.wasm.Sol_simulation;
+import network.platon.contracts.wasm.SolSimulation;
 import org.junit.Test;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
@@ -23,16 +22,16 @@ public class SolSimulationTest extends ContractPrepareTest {
 
         try {
             prepare();
-            Sol_simulation solSimulation = Sol_simulation.deploy(web3j, transactionManager, provider, Uint32.of(32), Uint32.of(0)).send();
+            SolSimulation solSimulation = SolSimulation.deploy(web3j, transactionManager, provider, Uint32.of(32), Uint32.of(0)).send();
             String contractAddress = solSimulation.getContractAddress();
             String transactionHash = solSimulation.getTransactionReceipt().get().getTransactionHash();
             collector.logStepPass("deploy successfully.contractAddress:" + contractAddress
                     + ", hash:" + transactionHash
                     + ", gasUsed:" + solSimulation.getTransactionReceipt().get().getGasUsed());
 
-            TransactionReceipt transactionReceiptAction = Sol_simulation.load(contractAddress, web3j, transactionManager, provider).action().send();
+            TransactionReceipt transactionReceiptAction = SolSimulation.load(contractAddress, web3j, transactionManager, provider).action().send();
 
-            TransactionReceipt transactionReceipt = Sol_simulation.load(contractAddress, web3j, transactionManager, provider).debug().send();
+            TransactionReceipt transactionReceipt = SolSimulation.load(contractAddress, web3j, transactionManager, provider).debug().send();
 
             collector.logStepPass("action and debug are gas used: " + transactionReceiptAction.getGasUsed() + "," + transactionReceipt.getGasUsed());
 
