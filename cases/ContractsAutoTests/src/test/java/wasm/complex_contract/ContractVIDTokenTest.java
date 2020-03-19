ea12d1a3ccbe608745b0c6b7b6e8b66ae1e1747e
zjsunzone <zjsunzone@gmail.com>
2020-03-19 14:28:49 +0800

    update test script

--- cases/ContractsAutoTests/src/test/java/wasm/complex_contract/ContractVIDTokenTest.java
+++ cases/ContractsAutoTests/src/test/java/wasm/complex_contract/ContractVIDTokenTest.java
@@ -8,8 +8,11 @@ import network.platon.contracts.wasm.HomeBridge;
 import network.platon.contracts.wasm.VIDToken;
 import org.junit.Before;
 import org.junit.Test;
+import org.web3j.crypto.Credentials;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
 import org.web3j.tx.Transfer;
 import org.web3j.utils.Convert;
 import org.web3j.utils.Numeric;
@@ -69,6 +72,24 @@ public class ContractVIDTokenTest extends WASMContractPrepareTest {
             BigInteger allowance = contract.Allowance(credentials.getAddress(), to).send();
             collector.logStepPass("Call allowance, res: " + allowance);
             collector.assertEqual(allowance, value);
+
+            // TransferFrom
+            // spender: a11859ce23effc663a9460e332ca09bd812acc390497f8dc7542b6938e13f8d7
+            // 0x493301712671Ada506ba6Ca7891F436D29185821
+            // to: 0x1E1ae3407377F7897470FEf31a80873B4FD75cA1
+            Credentials spendCredentials = Credentials.create("a11859ce23effc663a9460e332ca09bd812acc390497f8dc7542b6938e13f8d7");
+            TransactionManager spenderTM = transactionManager = new RawTransactionManager(web3j, spendCredentials, chainId);
+            String to2 = "0x1E1ae3407377F7897470FEf31a80873B4FD75cA1";
+            BigInteger valule2 = new BigInteger("10000000000000");
+            VIDToken v = VIDToken.load(contractAddress, web3j, spenderTM, provider);
+            TransactionReceipt transferFromTr = v.TransferFrom(credentials.getAddress(), to2, valule2).send();
+            collector.logStepPass("Send TransferFrom, hash:  " + transferFromTr.getTransactionHash()
+                    + " gasUsed: " + transferFromTr.getGasUsed());
+            BigInteger to2Balance = contract.BalanceOf(to2).send();
+            collector.logStepPass("Call balanceOf 2, res: " + to2Balance);
+            collector.assertEqual(to2Balance, valule2);
+            collector.logStepPass("Check TransferFrom() and Approve() success.");
+
 
         } catch (Exception e) {
             if(e instanceof ArrayIndexOutOfBoundsException){
