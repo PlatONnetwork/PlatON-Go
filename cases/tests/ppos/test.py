828b4884f974553f7201757be7baa65f8008c741
lifapan <lifapan@juzix.net>
2020-03-24 10:49:22 +0800

    add case

--- cases/tests/ppos/test.py
+++ cases/tests/ppos/test.py
@@ -5,8 +5,19 @@ from client_sdk_python.eth import Eth
 from client_sdk_python.middleware import geth_poa_middleware
 from hexbytes import HexBytes
 
-from common.key import mock_duplicate_sign
+# from common.key import mock_duplicate_sign
+#
+# result = mock_duplicate_sign(1,'a71f82ac2b15f6dbffa9f87de2d237d6bc7581d453bcaf973251b079ef9b20db','15338821af5128c21696180b7ea047905803a519c43153880b7da0c210563254',35531)
+# print(result)
+#
 
-result = mock_duplicate_sign(1,'a71f82ac2b15f6dbffa9f87de2d237d6bc7581d453bcaf973251b079ef9b20db','15338821af5128c21696180b7ea047905803a519c43153880b7da0c210563254',35531)
-print(result)
+class ss():
+    def ssd(self):
+        print(1)
 
+def es():
+    a = ss()
+    a.ssd()
+
+if __name__ == '__main__':
+    es()
--- cases/tests/ppos/test_Zero_out_penalty.py
+++ cases/tests/ppos/test_Zero_out_penalty.py
@@ -12,20 +12,17 @@ from tests.lib import EconomicConfig, Genesis, StakingConfig, Staking, check_nod
 from client_sdk_python.debug import Debug
 
 
-def create_pledge_node_information(clients_noconsensus):
-    first_client = clients_noconsensus[0]
-    first_economic = first_client.economic
-    first_node = first_client.node
-    log.info("Current connection node：{}".format(first_client.node.node_mark))
+def create_pledge_node_information(client):
+    log.info("Current connection node：{}".format(client.node.node_mark))
     log.info("Start creating a pledge account Pledge_address")
-    staking_address, _ = first_economic.account.generate_account(first_node.web3, von_amount(first_economic.create_staking_limit, 2))
-    log.info("Created, account address: {} Amount: {}".format(staking_address, von_amount(first_economic.create_staking_limit, 2)))
+    staking_address, _ = client.economic.account.generate_account(client.node.web3, von_amount(client.economic.create_staking_limit, 2))
+    log.info("Created, account address: {} Amount: {}".format(staking_address, von_amount(client.economic.create_staking_limit, 2)))
     log.info("Start applying for a pledge node")
-    result = first_client.staking.create_staking(0, staking_address, staking_address)
+    result = client.staking.create_staking(0, staking_address, staking_address)
     assert_code(result, 0)
-    first_economic.wait_settlement_blocknum(first_node)
-    log.info("Current block height: {}".format(first_node.eth.blockNumber))
-    result = first_node.ppos.getVerifierList()
+    client.economic.wait_settlement_blocknum(client.node)
+    log.info("Current block height: {}".format(client.node.eth.blockNumber))
+    result = client.node.ppos.getVerifierList()
     log.info("current Verifier List：{}".format(result))
 
 
@@ -40,10 +37,12 @@ def test_ZB_NP_01(new_genesis_env, clients_noconsensus):
     genesis.to_file(new_file)
     new_genesis_env.deploy_all(new_file)
     # start execution use case
-    create_pledge_node_information(clients_noconsensus)
-    # 查看零出块记录表
-    result = Debug.getWaitSlashingNodeList()
+    first_client = clients_noconsensus[0]
+    create_pledge_node_information(first_client)
+    # view the zero out block record table
+    result = Debug.getWaitSlashingNodeList(first_client.node)
     log.info("Slashing NodeList: {}".format(result))
+    assert result == []
 
 
 def test_ZB_NP_02(clients_noconsensus):
@@ -51,5 +50,61 @@ def test_ZB_NP_02(clients_noconsensus):
     节点未被选中共识验证人列表查询零出块记录表（不存在零出块记录）
     """
     # start execution use case
-    create_pledge_node_information(clients_noconsensus)
+    first_client = clients_noconsensus[0]
+    first_economic = first_client.economic
+    first_node = first_client.node
+    create_pledge_node_information(first_client)
+    result = check_node_in_list(first_node.node_id, first_client.ppos.getValidatorList)
+    log.info("Current node in consensus list status：{}".format(result))
+    if result is False:
+        # view the zero out block record table
+        result = Debug.getWaitSlashingNodeList(first_client.node)
+        log.info("Slashing NodeList: {}".format(result))
+        assert result == []
+    else:
+        first_economic.wait_consensus_blocknum(first_node)
 
+
+def test_ZB_NP_03(clients_noconsensus):
+    """
+    节点未被选中共识验证人列表查询零出块记录表（存在零出块记录）
+    """
+    first_client = clients_noconsensus[0]
+    second_client = clients_noconsensus[1]
+    first_economic = first_client.economic
+    first_node = first_client.node
+    create_pledge_node_information(first_client)
+    for i in range(4):
+        result = check_node_in_list(first_node.node_id, first_client.ppos.getValidatorList)
+        log.info("Current node in consensus list status：{}".format(result))
+        if result:
+            # stop node
+            first_client.node.stop()
+            second_client.economic.wait_consensus_blocknum(second_client.node, 1)
+            log.info("Current block height: {}".format(second_client.node.eth.blockNumber))
+            result = Debug.getWaitSlashingNodeList(second_client.node)
+            log.info("Slashing NodeList: {}".format(result))
+            break
+        else:
+            first_economic.wait_consensus_blocknum(first_node)
+
+
+def test_ZB_NP_04(clients_noconsensus):
+    """
+    节点被选中共识验证人查询零出块记录表（不存在零出块记录）
+    """
+    first_client = clients_noconsensus[0]
+    first_economic = first_client.economic
+    first_node = first_client.node
+    create_pledge_node_information(first_client)
+    for i in range(4):
+        result = check_node_in_list(first_node.node_id, first_client.ppos.getValidatorList)
+        log.info("Current node in consensus list status：{}".format(result))
+        if result:
+            # view the zero out block record table
+            result = Debug.getWaitSlashingNodeList(first_client.node)
+            log.info("Slashing NodeList: {}".format(result))
+            assert result == []
+            break
+        else:
+            first_economic.wait_consensus_blocknum(first_node)
