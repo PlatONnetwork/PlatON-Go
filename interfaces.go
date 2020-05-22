b6737419f9c0754307abb928e87ef896fa29ebda
chenglin <910372762@qq.com>
2020-05-22 16:04:51 +0800

    Update interfaces.go

--- interfaces.go
+++ interfaces.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package ethereum defines interfaces for interacting with Ethereum.
+// Package ethereum defines interfaces for interacting with Ethereum..
 package ethereum
 
 import (
