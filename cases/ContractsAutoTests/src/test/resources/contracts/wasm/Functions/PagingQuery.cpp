eafe2a24022aeb8816d61e2393c2fa18f1003ea1
liweichengcode <237162922@qq.com>
2020-05-27 15:36:39 +0800

    delete contract debug

--- cases/ContractsAutoTests/src/test/resources/contracts/wasm/Functions/PagingQuery.cpp
+++ cases/ContractsAutoTests/src/test/resources/contracts/wasm/Functions/PagingQuery.cpp
@@ -41,12 +41,10 @@ CONTRACT PagingQuery : public platon::Contract{
 
         if(CurrentPage <= 0)
         {
-        	println("输入的页面数不合法：", CurrentPage);
         	return "";
         }
         else if(CurrentPage > pages)
         {
-        	println("输入的页面数超过总页面数：", pages);
             return "";
         }
 
