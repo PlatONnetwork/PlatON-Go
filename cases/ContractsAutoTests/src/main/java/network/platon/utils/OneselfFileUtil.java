7ed3f3f5e57d247ea63d46945313320845898d6a
xiaoqichuan@juzix.net <xiaoqichuan@juzix.net>
2020-05-29 18:40:47 +0800

    update file util

--- cases/ContractsAutoTests/src/main/java/network/platon/utils/OneselfFileUtil.java
+++ cases/ContractsAutoTests/src/main/java/network/platon/utils/OneselfFileUtil.java
@@ -8,7 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OneselfFileUtil {
-
+    /*
+     *收集所有evm和wasm的源文件
+     */
+    private List<String> evmSourceFileList = new ArrayList<>();
+    private List<String> wasmSourceFileList = new ArrayList<>();
 
     /**
      * @title OneselfFileUtil
@@ -16,8 +20,7 @@ public class OneselfFileUtil {
      * @author qcxiao
      * @updateTime 2019/12/27 14:22
      */
-    public static List<String> getResourcesFile(String path, int deep) {
-        List<String> list = new ArrayList<>();
+    public List<String> getResourcesFile(String path, int deep) {
         // 获得指定文件对象
         File file = new File(path);
         // 获得该文件夹内的所有文件
@@ -25,14 +28,14 @@ public class OneselfFileUtil {
         for (int i = 0; i < files.length; i++) {
             if (files[i].isFile()) {
                 if (files[i].getName().substring(files[i].getName().lastIndexOf(".") + 1).equals("sol")) {
-                    list.add(files[i].getPath());
+                    evmSourceFileList.add(files[i].getPath());
                 }
             } else if (files[i].isDirectory()) {
                 //文件夹需要调用递归 ，深度+1
                 getResourcesFile(files[i].getPath(), deep + 1);
             }
         }
-        return list;
+        return evmSourceFileList;
     }
 
     /**
@@ -60,7 +63,6 @@ public class OneselfFileUtil {
     }
 
     public List<String> getWasmResourcesFile(String path, int deep) {
-        List<String> list = new ArrayList<>();
         // 获得指定文件对象
         File file = new File(path);
         // 获得该文件夹内的所有文件
@@ -68,14 +70,14 @@ public class OneselfFileUtil {
         for (int i = 0; i < files.length; i++) {
             if (files[i].isFile()) {
                 if (files[i].getName().substring(files[i].getName().lastIndexOf(".") + 1).equals("cpp")) {
-                    list.add(files[i].getPath());
+                    wasmSourceFileList.add(files[i].getPath());
                 }
             } else if (files[i].isDirectory()) {
                 //文件夹需要调用递归 ，深度+1
                 getWasmResourcesFile(files[i].getPath(), deep + 1);
             }
         }
-        return list;
+        return wasmSourceFileList;
     }
 
     public List<String> getWasmFileName() throws Exception {
