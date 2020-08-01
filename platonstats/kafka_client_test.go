739ae026c63873ea104eef4bf2b39614c6b3d914
bglmmz <lvxiaoyi@juzix.net>
2020-08-01 15:41:07 +0800

    remove sarama-cluster

--- platonstats/kafka_client_test.go
+++ platonstats/kafka_client_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 	"time"
 
-	cluster "github.com/bsm/sarama-cluster"
-
 	"github.com/Shopify/sarama"
 
 	"github.com/PlatONnetwork/PlatON-Go/log"
@@ -186,107 +184,6 @@ func Test_kafkaMsg_not_work(t *testing.T) {
 			//手工提交offset()
 			partitionOffsetManager.MarkOffset(msg.Offset, "")
 		case err := <-checkingConsumer.Errors():
-			log.Error("Failed to pull account-checking message from Kafka", "err", err)
-			panic(err)
-		}
-	}
-}
-func T2est_sarama_msg_notwork(t *testing.T) {
-	log.Root().SetHandler(log.CallerFileHandler(log.LvlFilterHandler(log.Lvl(4), log.StreamHandler(os.Stderr, log.TerminalFormat(true)))))
-
-	urls := []string{"192.168.112.32:9092"}
-	topic := "platon-account-checking"
-
-	consumer, err := sarama.NewConsumer(urls, consumerSaramaConfig())
-	if err != nil {
-		log.Error("Failed to init msg Kafka account-checking consumer....", "err", err)
-		panic(err)
-	}
-
-	defer func() {
-		if err := consumer.Close(); err != nil {
-			log.Error("Failed to close consumer", "err", err)
-		}
-	}()
-
-	checkingConsumer, err := consumer.ConsumePartition(topic, int32(0), sarama.OffsetNewest)
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := checkingConsumer.Close(); err != nil {
-			log.Error("Failed to close checkingConsumer", "err", err)
-		}
-	}()
-
-	log.Info("Success to init msg Kafka account-checking consumer....")
-	for {
-		select {
-		case msg := <-checkingConsumer.Messages():
-			key := string(msg.Key)
-			value := string(msg.Value)
-			log.Debug("received account-checking message", "offset", msg.Offset, "key", key, "value", value)
-
-			//手工提交offset()
-			/*if err := checkingConsumer.CommitOffsets(); err != nil {
-				log.Error("Failed to commit checking consumer offset", "err", err)
-				panic(err)
-			}*/
-		case err := <-checkingConsumer.Errors():
-			log.Error("Failed to pull account-checking message from Kafka", "err", err)
-			panic(err)
-		}
-	}
-}
-
-func clusterConfig() *cluster.Config {
-	config := cluster.NewConfig()
-	config.Consumer.Return.Errors = true
-	config.Version = sarama.V2_5_0_0
-	//手工提交offset
-	config.Consumer.Offsets.AutoCommit.Enable = false
-	config.Consumer.Offsets.AutoCommit.Interval = 1 * time.Second
-	//初始从最新的offset开始
-	config.Consumer.Offsets.Initial = sarama.OffsetNewest
-	config.Consumer.Offsets.CommitInterval = 1 * time.Second
-	return config
-}
-
-func T2est_sarama_cluster_msg_notwork(t *testing.T) {
-	log.Root().SetHandler(log.CallerFileHandler(log.LvlFilterHandler(log.Lvl(4), log.StreamHandler(os.Stderr, log.TerminalFormat(true)))))
-
-	urls := []string{"192.168.112.32:9092"}
-	topic := "platon-account-checking"
-	if len(topic) == 0 {
-		topic = defaultKafkaAccountCheckingTopic
-	}
-
-	consumer, err := cluster.NewConsumer(urls, defaultKafkaAccountCheckingConsumerGroup, []string{topic}, clusterConfig())
-	if err != nil {
-		log.Error("Failed to init msg Kafka account-checking consumer....", "err", err)
-		panic(err)
-	}
-
-	defer func() {
-		if err := consumer.Close(); err != nil {
-			log.Error("Failed to close consumer", "err", err)
-		}
-	}()
-
-	log.Info("Success to init msg Kafka account-checking consumer....")
-	for {
-		select {
-		case msg := <-consumer.Messages():
-			key := string(msg.Key)
-			value := string(msg.Value)
-			log.Debug("received account-checking message", "offset", msg.Offset, "key", key, "value", value)
-
-			//手工提交offset()
-			if err := consumer.CommitOffsets(); err != nil {
-				log.Error("Failed to commit checking consumer offset", "err", err)
-				panic(err)
-			}
-		case err := <-consumer.Errors():
 			log.Error("Failed to pull account-checking message from Kafka", "err", err)
 			panic(err)
 		}
