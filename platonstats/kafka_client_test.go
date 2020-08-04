e048864a1b74dc61145d1781bd18433923242bbe
bglmmz <lvxiaoyi@juzix.net>
2020-08-04 20:07:27 +0800

    add producer test

--- platonstats/kafka_client_test.go
+++ platonstats/kafka_client_test.go
@@ -6,13 +6,37 @@ import (
 	"testing"
 	"time"
 
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+
 	"github.com/PlatONnetwork/PlatON-Go/log"
 )
+
+func Test_kafkaClient_producer(t *testing.T) {
+	log.Root().SetHandler(log.CallerFileHandler(log.LvlFilterHandler(log.Lvl(4), log.StreamHandler(os.Stderr, log.TerminalFormat(true)))))
+	topic := "testTopic1"
+
+	kafkaClient := NewConfluentKafkaClient("192.168.9.201:9092", topic, "platon-account-checking", "platon-account-checking-group")
+
+	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
+		err := kafkaClient.producer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: 0},
+			Value:          []byte(word),
+		}, nil)
+		if err != nil {
+			log.Error("cannot queue msg", "err", err)
+			fmt.Printf("cannot queue msg, error: %v\n", err)
+		}
+	}
+
+	// Wait for message deliveries before shutting down
+	kafkaClient.producer.Flush(15 * 1000)
+
+}
 
 func Test_kafkaClient_consumer(t *testing.T) {
 	log.Root().SetHandler(log.CallerFileHandler(log.LvlFilterHandler(log.Lvl(4), log.StreamHandler(os.Stderr, log.TerminalFormat(true)))))
 
-	kafkaClient := NewConfluentKafkaClient("192.168.9.201:9092", "", "platon-account-checking", "platon-account-checking-group")
+	kafkaClient := NewConfluentKafkaClient("192.168.9.201:9092", "block-topic", "platon-account-checking", "platon-account-checking-group")
 
 	for {
 		msg, err := kafkaClient.consumer.ReadMessage(-1)
