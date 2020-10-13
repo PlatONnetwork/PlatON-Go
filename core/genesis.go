13c1ebe336fea93c0fb42b38e51832325033b002
WeiLoy <wei_loy@163.com>
2020-10-13 20:30:53 +0800

    Need to specify the network to start

--- core/genesis.go
+++ core/genesis.go
@@ -153,8 +153,7 @@ func SetupGenesisBlock(db ethdb.Database, snapshotBaseDB snapshotdb.BaseDB, gene
 
 	if (stored == common.Hash{}) {
 		if genesis == nil {
-			log.Info("Writing default main-net genesis block")
-			genesis = DefaultAlayaGenesisBlock()
+			panic("Please specify network")
 		} else {
 			log.Info("Writing custom genesis block", "chainID", genesis.Config.ChainID)
 		}
