4f83f681531c7dd8e564e05bde684324b480b52f
alliswell <151259511@qq.com>
2020-12-23 14:46:03 +0800

    update cbft util root

--- consensus/cbft/cbft_common_util.go
+++ consensus/cbft/cbft_common_util.go
@@ -68,7 +68,7 @@ func NewBlock(parent common.Hash, number uint64) *types.Block {
 		Time:        uint64(time.Now().UnixNano() / 1e6),
 		Extra:       make([]byte, 97),
 		ReceiptHash: common.BytesToHash(hexutil.MustDecode("0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")),
-		Root:        common.BytesToHash(hexutil.MustDecode("0x0353e2f9065dd8d09ae0eb42a2e0776b07cd6915f53583d6ce0ad7b203e0be3c")),
+		Root:        common.BytesToHash(hexutil.MustDecode("0x4a63bf66d9bd3565fcf6cc2859c9ea6d7cc277cd71aa793e1822e3f097281738")),
 		Coinbase:    common.Address{},
 		GasLimit:    10000000000,
 	}
@@ -85,7 +85,7 @@ func NewBlockWithSign(parent common.Hash, number uint64, node *TestCBFT) *types.
 		Time:        uint64(time.Now().UnixNano() / 1e6),
 		Extra:       make([]byte, 97),
 		ReceiptHash: common.BytesToHash(hexutil.MustDecode("0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")),
-		Root:        common.BytesToHash(hexutil.MustDecode("0x0353e2f9065dd8d09ae0eb42a2e0776b07cd6915f53583d6ce0ad7b203e0be3c")),
+		Root:        common.BytesToHash(hexutil.MustDecode("0x4a63bf66d9bd3565fcf6cc2859c9ea6d7cc277cd71aa793e1822e3f097281738")),
 		Coinbase:    common.Address{},
 		GasLimit:    10000000000,
 	}
