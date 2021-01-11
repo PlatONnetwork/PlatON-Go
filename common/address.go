6af24315ed960e047aafde8e1325a118a4da51fe
clearly <910372762@qq.com>
2021-01-11 16:20:40 +0800

    fix

--- common/address.go
+++ common/address.go
@@ -35,9 +35,9 @@ func SetAddressHRP(s string) error {
 		s = DefaultAddressHRP
 	}
 	if len(s) != 3 {
-		return errors.New("the length of  addressHRP  must be 3")
+		return errors.New("the length of addressHRP must be 3")
 	}
-	log.Info("address hrp  has set", "hrp", s)
+	log.Info("the address hrp has been set", "hrp", s)
 	currentAddressHRP = s
 	return nil
 }
