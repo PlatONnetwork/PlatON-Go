d2d3925c7541ea7c637fde7f3599aac8a5d3ecc3
WeiLoy <wei_loy@163.com>
2021-02-23 11:44:27 +0800

    Modified version number

--- params/version.go
+++ params/version.go
@@ -22,13 +22,13 @@ import (
 
 const (
 	//These versions are meaning the current code version.
-	VersionMajor = 0          // Major version component of the current release
-	VersionMinor = 15         // Minor version component of the current release
+	VersionMajor = 1          // Major version component of the current release
+	VersionMinor = 0          // Minor version component of the current release
 	VersionPatch = 0          // Patch version component of the current release
 	VersionMeta  = "unstable" // Version metadata to append to the version string
 
 	//CAUTION: DO NOT MODIFY THIS ONCE THE CHAIN HAS BEEN INITIALIZED!!!
-	GenesisVersion = uint32(0<<16 | 15<<8 | 0)
+	GenesisVersion = uint32(1<<16 | 0<<8 | 0)
 )
 
 func CodeVersion() uint32 {
