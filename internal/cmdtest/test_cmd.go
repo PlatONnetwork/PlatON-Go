868d617b07b403f1df8ac7aa38dae76af66460d2
clearly <910372762@qq.com>
2021-03-22 18:20:30 +0800

    update test wait time

--- internal/cmdtest/test_cmd.go
+++ internal/cmdtest/test_cmd.go
@@ -217,7 +217,7 @@ func (tt *TestCmd) Kill() {
 }
 
 func (tt *TestCmd) withKillTimeout(fn func()) {
-	timeout := time.AfterFunc(5*time.Second, func() {
+	timeout := time.AfterFunc(8*time.Second, func() {
 		tt.Log("killing the child process (timeout)")
 		tt.Kill()
 	})
