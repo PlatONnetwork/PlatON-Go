c7a86f7de94d0b870e88d8bb6867ab1a78f03967
WeiLoy <wei_loy@163.com>
2021-03-31 10:24:29 +0800

    Add unit test for verifying economic model parameters

--- x/xcom/common_config_test.go
+++ x/xcom/common_config_test.go
@@ -16,7 +16,12 @@
 
 package xcom
 
-import "testing"
+import (
+	"github.com/PlatONnetwork/PlatON-Go/common"
+	"github.com/PlatONnetwork/PlatON-Go/rlp"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestDefaultEMConfig(t *testing.T) {
 	t.Run("DefaultMainNet", func(t *testing.T) {
@@ -46,4 +51,13 @@ func TestDefaultEMConfig(t *testing.T) {
 	if getDefaultEMConfig(10) != nil {
 		t.Error("the chain config not support")
 	}
+}
+
+func TestMainNetHash(t *testing.T) {
+	tempEc := getDefaultEMConfig(DefaultMainNet)
+	bytes, err := rlp.EncodeToBytes(tempEc)
+	if err != nil {
+		t.Error(err)
+	}
+	assert.True(t, common.RlpHash(bytes).Hex() == MainNetECHash)
 }
