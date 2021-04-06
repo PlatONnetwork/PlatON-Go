a1f8c4b81e7621e27ee389fab1f9be20baa85abe
WeiLoy <wei_loy@163.com>
2021-04-06 20:12:09 +0800

    Modify the description of ZeroProduceCumulativeTime governance parameter

--- x/xcom/common_config.go
+++ x/xcom/common_config.go
@@ -52,7 +52,7 @@ const (
 	CeilUnStakeFreezeDuration = 168 * 2
 	CeilMaxEvidenceAge        = CeilUnStakeFreezeDuration - 1
 	// The maximum time range for the cumulative number of zero blocks (No more than 64)
-	maxZeroProduceCumulativeTime uint16 = 50
+	MaxZeroProduceCumulativeTime uint16 = 50
 
 	RewardPerMaxChangeRangeUpperLimit = 2000
 	RewardPerMaxChangeRangeLowerLimit = 1
@@ -432,8 +432,8 @@ func CheckSlashBlocksReward(rewards int) error {
 }
 
 func CheckZeroProduceCumulativeTime(zeroProduceCumulativeTime uint16, zeroProduceNumberThreshold uint16) error {
-	if zeroProduceCumulativeTime < zeroProduceNumberThreshold || zeroProduceCumulativeTime > maxZeroProduceCumulativeTime {
-		return common.InvalidParameter.Wrap(fmt.Sprintf("The ZeroProduceCumulativeTime must be [%d, %d]", zeroProduceNumberThreshold, maxZeroProduceCumulativeTime))
+	if zeroProduceCumulativeTime < zeroProduceNumberThreshold || zeroProduceCumulativeTime > MaxZeroProduceCumulativeTime {
+		return common.InvalidParameter.Wrap(fmt.Sprintf("The ZeroProduceCumulativeTime must be [%d, %d]", zeroProduceNumberThreshold, MaxZeroProduceCumulativeTime))
 	}
 	return nil
 }
