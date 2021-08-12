6ce4769e962f2064cb42e6c24a0c85ba4971d0e7
lishuguang <li_shu_guang@yeah.net>
2021-08-12 10:05:17 +0800

    二进制添加commit的日期gitcommit下面的增加git的date

--- cmd/platon/main.go
+++ cmd/platon/main.go
@@ -53,8 +53,9 @@ const (
 var (
 	// Git SHA1 commit hash of the release (set via linker flags)
 	gitCommit = ""
+	gitDate   = ""
 	// The app that holds all commands and flags.
-	app = utils.NewApp(gitCommit, "the platon-go command line interface")
+	app = utils.NewApp(gitCommit, gitDate, "the platon-go command line interface")
 	// flags that configure the node
 	nodeFlags = []cli.Flag{
 		utils.IdentityFlag,
