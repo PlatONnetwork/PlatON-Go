32e138b2d000081d31dca9423deebf5ef0d9e515
niuxiaojie81 <85773309@qq.com>
2022-08-10 10:41:23 +0800

    Each block in the fetch block function is processed serially

--- consensus/cbft/sync_process_test.go
+++ consensus/cbft/sync_process_test.go
@@ -138,6 +138,104 @@ SYNC:
 	assert.Equal(t, uint64(3), nodes[1].engine.state.HighestQCBlock().NumberU64())
 }
 
+func TestFetch_Serial(t *testing.T) {
+	pk, sk, cbftnodes := GenerateCbftNode(4)
+	nodes := make([]*TestCBFT, 0)
+	for i := 0; i < 4; i++ {
+		node := MockNode(pk[i], sk[i], cbftnodes, 200000, 10)
+		assert.Nil(t, node.Start())
+
+		nodes = append(nodes, node)
+	}
+
+	result := make(chan *types.Block, 1)
+	complete := make(chan struct{}, 1)
+	var fetchBlock *types.Block
+	qcBlocks := &protocols.QCBlockList{}
+	parent := nodes[0].chain.Genesis()
+	for i := 0; i < 3; i++ {
+		block := NewBlockWithSign(parent.Hash(), parent.NumberU64()+1, nodes[0])
+		assert.True(t, nodes[0].engine.state.HighestExecutedBlock().Hash() == block.ParentHash())
+		nodes[0].engine.OnSeal(block, result, nil, complete)
+		<-complete
+
+		fetchBlock = block
+		qcBlocks.Blocks = append(qcBlocks.Blocks, block)
+		_, qc := nodes[0].engine.blockTree.FindBlockAndQC(parent.Hash(), parent.NumberU64())
+		select {
+		case b := <-result:
+			assert.NotNil(t, b)
+			assert.Equal(t, uint32(i-1), nodes[0].engine.state.MaxQCIndex())
+			for j := 1; j < 3; j++ {
+				msg := &protocols.PrepareVote{
+					Epoch:          nodes[0].engine.state.Epoch(),
+					ViewNumber:     nodes[0].engine.state.ViewNumber(),
+					BlockIndex:     uint32(i),
+					BlockHash:      b.Hash(),
+					BlockNumber:    b.NumberU64(),
+					ValidatorIndex: uint32(j),
+					ParentQC:       qc,
+				}
+				pb := nodes[0].engine.state.PrepareBlockByIndex(uint32(i))
+				assert.NotNil(t, pb)
+				execute := make(chan uint32, 1)
+				nodes[j].engine.executeFinishHook = func(index uint32) {
+					execute <- index
+				}
+				assert.Nil(t, nodes[j].engine.OnPrepareBlock("id", pb))
+
+				select {
+				case <-execute:
+
+				}
+				index, finish := nodes[j].engine.state.Executing()
+				assert.True(t, index == uint32(i) && finish, fmt.Sprintf("i:%d,index:%d,finish:%v", i, index, finish))
+				assert.Nil(t, nodes[j].engine.signMsgByBls(msg))
+				assert.Nil(t, nodes[0].engine.OnPrepareVote("id", msg), fmt.Sprintf("number:%d", b.NumberU64()))
+			}
+			_, qc := nodes[0].engine.blockTree.FindBlockAndQC(block.Hash(), block.NumberU64())
+			assert.NotNil(t, qc)
+			if i == 2 {
+				// Deliberately mistake the qc of the third block
+				qc = qcBlocks.QC[len(qcBlocks.QC)-1]
+			}
+			qcBlocks.QC = append(qcBlocks.QC, qc)
+
+			parent = b
+		}
+	}
+	assert.Equal(t, uint64(3), nodes[0].engine.state.HighestQCBlock().NumberU64())
+	assert.Equal(t, uint64(0), nodes[1].engine.state.HighestQCBlock().NumberU64())
+
+	finish := make(chan struct{}, 1)
+	nodes[1].engine.insertBlockQCHook = func(block *types.Block, qc *types2.QuorumCert) {
+		if block.NumberU64() == 3 {
+			finish <- struct{}{}
+		}
+	}
+	_, fetchBlockQC := nodes[0].engine.blockTree.FindBlockAndQC(fetchBlock.Hash(), fetchBlock.NumberU64())
+	nodes[1].engine.fetchBlock("id", fetchBlock.Hash(), fetchBlock.NumberU64(), fetchBlockQC)
+	timer := time.NewTimer(20 * time.Millisecond)
+
+	for {
+		select {
+		case <-timer.C:
+			if nodes[1].engine.fetcher.Len() == 1 {
+				goto SYNC
+			}
+			timer.Reset(20 * time.Millisecond)
+		}
+	}
+SYNC:
+	nodes[1].engine.ReceiveSyncMsg(&types2.MsgInfo{PeerID: "id", Msg: qcBlocks})
+	select {
+	case <-time.NewTimer(10 * time.Second).C:
+	case <-finish:
+
+	}
+	assert.Equal(t, uint64(2), nodes[1].engine.state.HighestQCBlock().NumberU64())
+}
+
 func TestSyncBlock(t *testing.T) {
 	nodes := Mock4NodePipe(false)
 	result := make(chan *types.Block, 1)
