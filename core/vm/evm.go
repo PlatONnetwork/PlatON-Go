54090c4005154506bcbf6a5cf299438d14097718
benbaley <alliswell.jiang@gmail.com>
2022-08-18 16:14:26 +0800

    EVM switch branch update version to 0.13.0

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -215,7 +215,7 @@ func NewEVM(ctx Context, snapshotDB snapshotdb.DB, statedb StateDB, chainConfig 
 		chainConfig:  chainConfig,
 		interpreters: make([]Interpreter, 0, 1),
 	}
-	//第一阶段EVM中CHAINID指令返回值沿用旧值
+	//第3阶段EVM中CHAINID指令返回值使用新链ID
 
 	if statedb != nil && gov.Gte130VersionState(statedb) {
 		cpyChainCfg := &params.ChainConfig{
