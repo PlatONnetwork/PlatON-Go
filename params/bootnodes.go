e4a6f7b973ddaabaf7170bdfdde8e8b27d169a81
benbaley <alliswell.jiang@gmail.com>
2023-04-24 18:27:58 +0800

    merge eth# 20758

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -16,6 +16,8 @@
 
 package params
 
+import "github.com/PlatONnetwork/PlatON-Go/common"
+
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main PlatON network.
 var MainnetBootnodes = []string{
@@ -36,3 +38,19 @@ var TestnetBootnodes = []string{
 // DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
 // experimental RLPx v5 topic-discovery network.
 var DiscoveryV5Bootnodes = []string{}
+
+const dnsPrefix = "enrtree://AKA3AM6LPBYEUDMVNU3BSVQJ5AD45Y7YPOHJLEF6W26QOE4VTUDPE@"
+
+// KnownDNSNetwork returns the address of a public DNS-based node list for the given
+// genesis hash and protocol. See https://github.com/ethereum/discv4-dns-lists for more
+// information.
+func KnownDNSNetwork(genesis common.Hash, protocol string) string {
+	var net string
+	switch genesis {
+	case MainnetGenesisHash:
+		net = "mainnet"
+	default:
+		return ""
+	}
+	return dnsPrefix + protocol + "." + net + ".platondisco.net"
+}
