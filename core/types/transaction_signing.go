7480bb9f12bb5039f7ffd0e8a2913ba4aa9e217a
niuxiaojie81 <85773309@qq.com>
2023-04-28 11:10:05 +0800

    merge #22633

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -306,7 +306,7 @@ func (s PIP7Signer) SignatureValues(tx *Transaction, sig []byte) (R, S, V *big.I
 		return nil, nil, nil, ErrTxTypeNotSupported
 	}
 	R, S, V = decodeSignature(sig)
-	if s.chainId.Sign() != 0 {
+	if s.PIP7ChainId.Sign() != 0 {
 		V = big.NewInt(int64(sig[64] + 35))
 		V.Add(V, s.PIP7ChainIdMul)
 	}
@@ -349,6 +349,9 @@ func (s PIP11Signer) Equal(s2 Signer) bool {
 }
 
 func (s PIP11Signer) Sender(tx *Transaction) (common.Address, error) {
+	if tx.Type() != LegacyTxType {
+		return common.Address{}, ErrTxTypeNotSupported
+	}
 	if !tx.Protected() {
 		return HomesteadSigner{}.Sender(tx)
 	}
