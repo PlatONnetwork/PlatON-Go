b4f1abec9fd3a24fa0d3994973142d53104674f1
benbaley <alliswell.jiang@gmail.com>
2023-05-06 14:11:23 +0800

    merge eth#22762

--- trie/proof_test.go
+++ trie/proof_test.go
@@ -914,7 +914,7 @@ func benchmarkVerifyRangeProof(b *testing.B, size int) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _, _, _, err := VerifyRangeProof(trie.Hash(), keys[0], keys[len(keys)-1], keys, values, proof)
+		_, err := VerifyRangeProof(trie.Hash(), keys[0], keys[len(keys)-1], keys, values, proof)
 		if err != nil {
 			b.Fatalf("Case %d(%d->%d) expect no error, got %v", i, start, end-1, err)
 		}
