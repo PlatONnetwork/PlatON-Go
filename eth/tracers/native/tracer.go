349f8e714fd234fedacd9ebd3d477d02d43727a8
benbaley <alliswell.jiang@gmail.com>
2023-08-16 17:25:48 +0800

    fix compile err

--- eth/tracers/native/tracer.go
+++ eth/tracers/native/tracer.go
@@ -27,9 +27,11 @@ Aside from implementing the tracer, it also needs to register itself, using the
 Example:
 
 ```golang
-func init() {
-	register("noopTracerNative", newNoopTracer)
-}
+
+	func init() {
+		register("noopTracerNative", newNoopTracer)
+	}
+
 ```
 */
 package native
@@ -37,7 +39,7 @@ package native
 import (
 	"errors"
 
-	"github.com/ethereum/go-ethereum/eth/tracers"
+	"github.com/PlatONnetwork/PlatON-Go/eth/tracers"
 )
 
 // init registers itself this packages as a lookup for tracers.
