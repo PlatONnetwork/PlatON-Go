66a6d4f9f2f222230b2f1ec75d35c3d167cedbb8
WeiLoy <wei_loy@163.com>
2023-08-21 17:06:25 +0800

    core/vm: fill gaps in jump table with opUndefined

--- core/vm/jump_table.go
+++ core/vm/jump_table.go
@@ -204,7 +204,7 @@ func newHomesteadInstructionSet() JumpTable {
 // NewFrontierInstructionSet returns the frontier instructions
 // that can be executed during the frontier phase.
 func newFrontierInstructionSet() JumpTable {
-	return JumpTable{
+	tbl := JumpTable{
 		STOP: {
 			execute:     opStop,
 			constantGas: 0,
@@ -1006,4 +1006,13 @@ func newFrontierInstructionSet() JumpTable {
 			maxStack:   maxStack(1, 0),
 		},
 	}
+
+	// Fill all unassigned slots with opUndefined.
+	for i, entry := range tbl {
+		if entry == nil {
+			tbl[i] = &operation{execute: opUndefined, maxStack: maxStack(0, 0)}
+		}
+	}
+
+	return tbl
 }
