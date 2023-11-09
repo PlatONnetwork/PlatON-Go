9bb71ea1c6874e30da4aceee791938ebe29fe2d2
WeiLoy <wei_loy@163.com>
2023-11-09 15:40:37 +0800

    internal/ethapi: fix typo in comment

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -510,7 +510,7 @@ func signHash(data []byte) []byte {
 }
 
 // Sign calculates an Ethereum ECDSA signature for:
-// keccack256("\x19Ethereum Signed Message:\n" + len(message) + message))
+// keccak256("\x19Ethereum Signed Message:\n" + len(message) + message))
 //
 // Note, the produced signature conforms to the secp256k1 curve R, S and V values,
 // where the V value will be 27 or 28 for legacy reasons.
