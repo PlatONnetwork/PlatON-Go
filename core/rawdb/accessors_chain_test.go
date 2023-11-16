76cf46102be751798d610e1c1ce90761e69012c7
clearly <910372762@qq.com>
2023-11-16 16:03:59 +0800

    fix

--- core/rawdb/accessors_chain_test.go
+++ core/rawdb/accessors_chain_test.go
@@ -35,7 +35,6 @@ import (
 	"github.com/PlatONnetwork/PlatON-Go/core/types"
 	"github.com/PlatONnetwork/PlatON-Go/crypto"
 	"github.com/PlatONnetwork/PlatON-Go/rlp"
-	"os"
 )
 
 // Tests block header storage and retrieval operations.
