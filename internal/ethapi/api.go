334ec8ec8c70e7aac88c9ee8bd77835eab257385
niuxiaojie81 <85773309@qq.com>
2023-11-23 10:51:22 +0800

    internal/ethapi: add basefee to block overrides #25219

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -852,6 +852,7 @@ type BlockOverrides struct {
 	GasLimit   *hexutil.Uint64
 	Coinbase   *common.Address
 	Random     *common.Hash
+	BaseFee    *hexutil.Big
 }
 
 // Apply overrides the given header fields into the given block context.
@@ -876,6 +877,9 @@ func (diff *BlockOverrides) Apply(blockCtx *vm.BlockContext) {
 	}
 	if diff.Random != nil {
 		blockCtx.Random = diff.Random
+	}
+	if diff.BaseFee != nil {
+		blockCtx.BaseFee = diff.BaseFee.ToInt()
 	}
 }
 
