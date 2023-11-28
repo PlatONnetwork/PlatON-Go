dd78b8bd8c9631b63c02e05fc117797eab77e2a9
benbaley <alliswell.jiang@gmail.com>
2023-11-28 11:59:15 +0800

    make lint

--- core/snapshotdb/heap.go
+++ core/snapshotdb/heap.go
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the PlatON-Go library. If not, see <http://www.gnu.org/licenses/>.
 
-
 package snapshotdb
 
 import (
@@ -39,12 +38,9 @@ type rankingHeap struct {
 	hepMaxNum int
 }
 
-//   the key is gt than or eq than   heap top
+// the key is gt than or eq than   heap top
 func (r *rankingHeap) geMaxHeap(k []byte) bool {
-	if bytes.Compare(k, r.heap[0].key) > 0 {
-		return true
-	}
-	return false
+	return bytes.Compare(k, r.heap[0].key) > 0
 }
 
 func (r *rankingHeap) addHandledKey(key []byte) {
