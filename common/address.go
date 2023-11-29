b7aea5601391a1b8a510dec81eecd07fb797619b
benbaley <alliswell.jiang@gmail.com>
2023-11-29 10:45:09 +0800

    lint

--- common/address.go
+++ common/address.go
@@ -152,27 +152,6 @@ func (a Address) Hex() string {
 	return "0x" + a.HexWithNoPrefix()
 }
 
-func (a *Address) checksumHex() []byte {
-	buf := a.hex()
-
-	// compute checksum
-	sha := sha3.NewLegacyKeccak256()
-	sha.Write(buf[2:])
-	hash := sha.Sum(nil)
-	for i := 2; i < len(buf); i++ {
-		hashByte := hash[(i-2)/2]
-		if i%2 == 0 {
-			hashByte = hashByte >> 4
-		} else {
-			hashByte &= 0xf
-		}
-		if buf[i] > '9' && hashByte > 7 {
-			buf[i] -= 32
-		}
-	}
-	return buf[:]
-}
-
 func (a Address) hex() []byte {
 	var buf [len(a)*2 + 2]byte
 	copy(buf[:2], "0x")
