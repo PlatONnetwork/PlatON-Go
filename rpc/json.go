40d91f7842e418d8389d31db87e3ef3042fb44b9
clearly <910372762@qq.com>
2024-04-24 11:06:47 +0800

    rpc: check that "version" is "2.0" in request objects (#25570)

--- rpc/json.go
+++ rpc/json.go
@@ -47,9 +47,7 @@ const (
 	defaultWriteTimeout = 10 * time.Second // used if context has no deadline
 )
 
-var (
-	null = json.RawMessage("null")
-)
+var null = json.RawMessage("null")
 
 type subscriptionResult struct {
 	ID     string          `json:"subscription"`
@@ -81,19 +79,23 @@ func (msg *jsonrpcMessage) isEthMessage() bool {
 }
 
 func (msg *jsonrpcMessage) isNotification() bool {
-	return msg.ID == nil && msg.Method != ""
+	return msg.hasValidVersion() && msg.ID == nil && msg.Method != ""
 }
 
 func (msg *jsonrpcMessage) isCall() bool {
-	return msg.hasValidID() && msg.Method != ""
+	return msg.hasValidVersion() && msg.hasValidID() && msg.Method != ""
 }
 
 func (msg *jsonrpcMessage) isResponse() bool {
-	return msg.hasValidID() && msg.Method == "" && msg.Params == nil && (msg.Result != nil || msg.Error != nil)
+	return msg.hasValidVersion() && msg.hasValidID() && msg.Method == "" && msg.Params == nil && (msg.Result != nil || msg.Error != nil)
 }
 
 func (msg *jsonrpcMessage) hasValidID() bool {
 	return len(msg.ID) > 0 && msg.ID[0] != '{' && msg.ID[0] != '['
+}
+
+func (msg *jsonrpcMessage) hasValidVersion() bool {
+	return msg.Version == vsn
 }
 
 func (msg *jsonrpcMessage) isSubscribe() bool {
