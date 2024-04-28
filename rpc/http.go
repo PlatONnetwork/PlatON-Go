1afeca7337c8a023703b6c7d7b9dce85188600d4
clearly <910372762@qq.com>
2024-04-28 17:29:40 +0800

    rpc: fix setting client in DialHTTPWithClient (#26470)

--- rpc/http.go
+++ rpc/http.go
@@ -135,6 +135,7 @@ func DialHTTPWithClient(endpoint string, client *http.Client) (*Client, error) {
 	}
 
 	var cfg clientConfig
+	cfg.httpClient = client
 	fn := newClientTransportHTTP(endpoint, &cfg)
 	return newClient(context.Background(), fn)
 }
