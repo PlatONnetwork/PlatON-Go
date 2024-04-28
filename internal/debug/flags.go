28b69536c1591403dd18f6559d23edfbfeef7ac1
niuxiaojie81 <85773309@qq.com>
2024-04-28 15:26:18 +0800

    internal/debug: add --log.file option #26149

--- internal/debug/flags.go
+++ internal/debug/flags.go
@@ -55,6 +55,11 @@ var (
 		Usage:    "Format logs with JSON",
 		Category: flags.LoggingCategory,
 	}
+	logFileFlag = &cli.StringFlag{
+		Name:     "log.file",
+		Usage:    "Write logs to a file",
+		Category: flags.LoggingCategory,
+	}
 	backtraceAtFlag = &cli.StringFlag{
 		Name:     "log.backtrace",
 		Usage:    "Request a stack trace at a specific logging statement (e.g. \"block.go:271\")",
@@ -117,6 +122,7 @@ var Flags = []cli.Flag{
 	verbosityFlag,
 	vmoduleFlag,
 	logjsonFlag,
+	logFileFlag,
 	backtraceAtFlag,
 	debugFlag,
 	pprofFlag,
@@ -129,7 +135,10 @@ var Flags = []cli.Flag{
 	wasmLogFileFlag,
 }
 
-var glogger *log.GlogHandler
+var (
+	glogger         *log.GlogHandler
+	logOutputStream log.Handler
+)
 
 func init() {
 	glogger = log.NewGlogHandler(log.StreamHandler(os.Stderr, log.TerminalFormat(false)))
@@ -140,18 +149,30 @@ func init() {
 // Setup initializes profiling and logging based on the CLI flags.
 // It should be called as early as possible in the program.
 func Setup(ctx *cli.Context) error {
-	var ostream log.Handler
-	output := io.Writer(os.Stderr)
+	logFile := ctx.String(logFileFlag.Name)
+	useColor := logFile == "" && os.Getenv("TERM") != "dumb" && (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd()))
+
+	var logfmt log.Format
 	if ctx.Bool(logjsonFlag.Name) {
-		ostream = log.StreamHandler(output, log.JSONFormat())
+		logfmt = log.JSONFormat()
 	} else {
-		usecolor := (isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd())) && os.Getenv("TERM") != "dumb"
-		if usecolor {
+		logfmt = log.TerminalFormat(useColor)
+	}
+
+	if logFile != "" {
+		var err error
+		logOutputStream, err = log.FileHandler(logFile, logfmt)
+		if err != nil {
+			return err
+		}
+	} else {
+		output := io.Writer(os.Stderr)
+		if useColor {
 			output = colorable.NewColorableStderr()
 		}
-		ostream = log.StreamHandler(output, log.TerminalFormat(usecolor))
+		logOutputStream = log.StreamHandler(output, logfmt)
 	}
-	glogger.SetHandler(ostream)
+	glogger.SetHandler(logOutputStream)
 
 	// logging
 	verbosity := ctx.Int(verbosityFlag.Name)
@@ -247,4 +268,7 @@ func StartPProf(address string, withMetrics bool) {
 func Exit() {
 	Handler.StopCPUProfile()
 	Handler.StopGoTrace()
+	if closer, ok := logOutputStream.(io.Closer); ok {
+		closer.Close()
+	}
 }
