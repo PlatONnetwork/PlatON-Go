2b7f71baede55bab4d6289fc8ff8a2f4e4d05a94
niuxiaojie81 <85773309@qq.com>
2024-05-10 10:45:24 +0800

    all: assign zero after resize in implementations of heap.Interface #26296

--- core/txpool/list.go
+++ core/txpool/list.go
@@ -45,6 +45,7 @@ func (h *nonceHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
+	old[n-1] = 0
 	*h = old[0 : n-1]
 	return x
 }
@@ -469,7 +470,7 @@ func (h *priceHeap) Pop() interface{} {
 }
 
 // pricedList is a price-sorted heap to allow operating on transactions pool
-// contents in a price-incrementing way. It's built opon the all transactions
+// contents in a price-incrementing way. It's built upon the all transactions
 // in txpool but only interested in the remote part. It means only remote transactions
 // will be considered for tracking, sorting, eviction, etc.
 //
