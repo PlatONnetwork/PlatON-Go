beb795914dfe5c8773aa79fde438dbf2e49adfe3
clearly <910372762@qq.com>
2024-11-19 15:28:33 +0800

    update gov param  max validators to 225

--- x/plugin/gov_plugin.go
+++ x/plugin/gov_plugin.go
@@ -149,6 +149,12 @@ func (govPlugin *GovPlugin) BeginBlock(blockHash common.Hash, header *types.Head
 				}
 				log.Info("Successfully upgraded the new version 1.3.0", "blockNumber", blockNumber, "blockHash", blockHash, "preActiveProposalID", preActiveVersionProposalID)
 			}
+			if versionProposal.NewVersion == params.FORKVERSION_1_5_0 {
+				if err := gov.UpdateGovernParamValue(gov.ModuleStaking, gov.KeyMaxValidators, "225", blockNumber+1, blockHash); err != nil {
+					return err
+				}
+				log.Info("Successfully upgraded the new version 1.5.0", "blockNumber", blockNumber, "blockHash", blockHash)
+			}
 
 			log.Info("version proposal is active", "blockNumber", blockNumber, "proposalID", versionProposal.ProposalID, "newVersion", versionProposal.NewVersion, "newVersionString", xutil.ProgramVersion2Str(versionProposal.NewVersion))
 		}
