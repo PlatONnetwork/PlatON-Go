186267169207795df7a1407e0cf0f57e27fe0ca1
niuxiaojie81 <85773309@qq.com>
2025-03-21 10:24:01 +0800

    debug

--- core/blockchain.go
+++ core/blockchain.go
@@ -1510,6 +1510,8 @@ func (bc *BlockChain) WriteBlockWithState(block *types.Block, receipts []*types.
 			nodes, _ := triedb.Size()
 			oversize = nodes > limit
 		} else {
+			size, _ := triedb.Size()
+			log.Warn("triedb size", "size", size, "limit", limit)
 			triedb.ReferenceVersion(root)
 			triedb.DereferenceDB(currentBlock.Root())
 
