8fb1d0e5e707d343d9e0b0d71567d57ebc0233d0
niuxiaojie81 <85773309@qq.com>
2025-07-15 15:33:24 +0800

    eth/tracers: small refactor for native tracers #26196

--- eth/tracers/native/call.go
+++ eth/tracers/native/call.go
@@ -182,10 +182,6 @@ func (t *callTracer) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64, sco
 	}
 }
 
-// CaptureFault implements the EVMLogger interface to trace an execution fault.
-func (t *callTracer) CaptureFault(pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, depth int, err error) {
-}
-
 // CaptureEnter is called when EVM enters a new scope (via call, create or selfdestruct).
 func (t *callTracer) CaptureEnter(typ vm.OpCode, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int) {
 	if t.config.OnlyTopCall {
