cd66c1f43792e1ca3f7634218863049f97729f15
hustrust <hustrust@outlook.com>
2025-09-02 16:58:18 +0800

    chore: fix some typos in comment
    Signed-off-by: hustrust <hustrust@outlook.com>

--- build/ci.go
+++ build/ci.go
@@ -1098,7 +1098,7 @@ func newPodMetadata(env build.Environment, archive string) podMetadata {
 func doPurge(cmdline []string) {
 	/*var (
 		store = flag.String("store", "", `Destination from where to purge archives (usually "gethstore/builds")`)
-		limit = flag.Int("days", 30, `Age threshold above which to delete unstalbe archives`)
+		limit = flag.Int("days", 30, `Age threshold above which to delete unstable archives`)
 	)
 	flag.CommandLine.Parse(cmdline)
 
