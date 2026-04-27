243edbb14701c1b53d378af8cf44298a43f35d2a
niuxiaojie81 <85773309@qq.com>
2026-04-27 16:21:15 +0800

    rlp: support for uint256 #26898

--- rlp/rlpgen/gen_test.go
+++ rlp/rlpgen/gen_test.go
@@ -1,3 +1,19 @@
+// Copyright 2022 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -31,7 +47,7 @@ func init() {
 	}
 }
 
-var tests = []string{"uints", "nil", "rawvalue", "optional", "bigint"}
+var tests = []string{"uints", "nil", "rawvalue", "optional", "bigint", "uint256"}
 
 func TestOutput(t *testing.T) {
 	for _, test := range tests {
@@ -58,9 +74,9 @@ func TestOutput(t *testing.T) {
 			if err != nil {
 				t.Fatal("error loading expected test output:", err)
 			}
-			strOut := string(output)
-			strWantOut := string(wantOutput)
-			t.Log("result", "out", strOut, "wantout", strWantOut)
+			//strOut := string(output)
+			//strWantOut := string(wantOutput)
+			//t.Log("result", "out", strOut, "wantout", strWantOut)
 			if !bytes.Equal(output, wantOutput) {
 				t.Fatal("output mismatch:\n", string(output))
 			}
